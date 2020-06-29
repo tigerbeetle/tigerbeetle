96ec6bb402a873949f2c02799b7fc1c38a5661a2
Joran Dirk Greef <joran@ronomon.com>
2020-06-29 20:53:19 +0200

    Add Journal as a singleton

---
+++ journal.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+
+const Crypto = require('@ronomon/crypto-async');
+
+const Journal = {
+  fs: require('fs'),
+  fd: null,
+  path: null,
+  position: 0,
+  writing: false
+};
+
+Journal.open = function(path) {
+  const self = this;
+  self.path = path;
+  self.fd = self.fs.openSync(self.path, 'r+');
+};
+
+Journal.write = function(buffer, end) {
+  const self = this;
+  // TODO: Batch Records
+  // TODO: Checksums
+  // TODO: Snapshot
+  // TODO: Wrap around journal file
+  assert(self.fd >= 0);
+  assert(self.position >= 0);
+  assert(self.writing === false);
+  assert(buffer.length > 0);
+  assert(buffer.length % 64 === 0);
+  self.writing = true;
+  // Simulate CPU cost of checkum:
+  Crypto.hash('sha256', buffer);
+  self.fs.write(self.fd, buffer, 0, buffer.length, self.position,
+    function(error, bytesWritten) {
+      assert(!error);
+      assert(bytesWritten === buffer.length);
+      self.fs.fsync(self.fd,
+        function(error) {
+          assert(!error);
+          self.position += buffer.length;
+          self.writing = false;
+          end();
+        }
+      );
+    }
+  );
+};
+
+module.exports = Journal;
