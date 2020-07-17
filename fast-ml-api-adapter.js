a8a92aca4cde30889e16f8a13bf404b72271c01d
Joran Dirk Greef <joran@ronomon.com>
2020-07-17 13:31:59 +0200

    Batch prepares and fulfills, send prepare and fulfill notifications, and ACK

--- fast-ml-api-adapter.js
+++ fast-ml-api-adapter.js
@@ -55,11 +55,93 @@ Node.process.on('uncaughtException',
   );
 })();
 
+const TigerBeetle = {};
+
+TigerBeetle.CREATE_TRANSFERS = { jobs: [], timestamp: 0, timeout: 0 };
+TigerBeetle.ACCEPT_TRANSFERS = { jobs: [], timestamp: 0, timeout: 0 };
+
+TigerBeetle.create = function(request, callback) {
+  const self = this;
+  self.push(self.CREATE_TRANSFERS, request, callback);
+};
+
+TigerBeetle.accept = function(request, callback) {
+  const self = this;
+  self.push(self.ACCEPT_TRANSFERS, request, callback);
+};
+
+TigerBeetle.push = function(batch, request, callback) {
+  const self = this;
+  batch.jobs.push(new TigerBeetle.Job(request, callback));
+  if (batch.timeout === 0) {
+    batch.timestamp = Date.now();
+    batch.timeout = setTimeout(
+      function() {
+        self.execute(batch);
+      },
+      100
+    );
+  }
+};
+
+TigerBeetle.execute = function(batch) {
+  const ms = Date.now() - batch.timestamp;
+  LEV(`batched ${batch.jobs.length} jobs in ${ms}ms`);
+  batch.jobs.forEach(
+    function(job) {
+      job.callback();
+    }
+  );
+  batch.jobs = [];
+  batch.timestamp = 0;
+  batch.timeout = 0;
+};
+
+TigerBeetle.Job = function(request, callback) {
+  this.request = request;
+  this.callback = callback;
+};
+
 function CreateServer() {
   const server = Node.http.createServer({},
     function(request, response) {
-      response.statusCode = 203; // TODO Just to test we're hitting FMLAA.
-      response.end();
+      const buffers = [];
+      request.on('data', function(buffer) { buffers.push(buffer); });
+      request.on('end',
+        function() {
+          if (request.url === '/transfers') {
+            const buffer = Buffer.concat(buffers);
+            const payload = JSON.parse(buffer.toString('ascii'));
+            TigerBeetle.create(payload, function() {
+              // Send prepare notification:
+              PostNotification(PAYEE_HOST, PAYEE_PORT, '/transfers', buffer,
+                function() {
+                  // Finally, ACK:
+                  response.statusCode = 202;
+                  response.end();
+                }
+              );
+            });
+          } else if (request.url.length > 36) {
+            const buffer = Buffer.concat(buffers);
+            const payload = JSON.parse(buffer.toString('ascii'));
+            TigerBeetle.accept(payload, function() {
+              // Send fulfill notification:
+              const path = request.url;
+              PostNotification(PAYER_HOST, PAYER_PORT, path, Buffer.from(JSON.stringify({ transferState: 'COMMITTED' })),
+                function() {
+                  // Finally, ACK:
+                  response.statusCode = 202;
+                  response.end();
+                }
+              );
+            });
+          } else {
+            console.log(`unknown request.url: ${request.url}`);
+            response.end();
+          }
+        }
+      );
     }
   );
   server.listen(PORT, HOST,
@@ -69,25 +151,30 @@ function CreateServer() {
   );
 }
 
-function PostTransfer(transfer, end) {
-  // var options = {
-  //   method: 'POST',
-  //   host: MOJALOOP_HOST,
-  //   port: MOJALOOP_PORT,
-  //   path: '/transfers',
-  //   headers: transfer.headers
-  // };
-  // var request = Node.http.request(options,
-  //   function(response) {
-  //     var buffers = [];
-  //     // TO DO: Error handling.
-  //     response.on('data', function(buffer) { buffers.push(buffer); });
-  //     response.on('end', function() {});
-  //     Log(`POST ${transfer.uuid}: ${response.statusCode}`);
-  //   }
-  // );
-  // request.write(transfer.body);
-  // request.end();
+function PostNotification(host, port, path, body, end) {
+  const headers = {
+    'Content-Length': body.length
+  };
+  const options = {
+    method: 'POST',
+    host: host,
+    port: port,
+    path: path,
+    headers: headers
+  };
+  const request = Node.http.request(options,
+    function(response) {
+      const buffers = [];
+      response.on('data', function(buffer) { buffers.push(buffer); });
+      response.on('end',
+        function() {
+          end();
+        }
+      );
+    }
+  );
+  request.write(body);
+  request.end();
 }
 
 CreateServer();
