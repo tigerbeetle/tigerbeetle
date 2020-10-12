e3d0f9ea97edd1470c4cf2a879ae2d9634dc108d
Joran Dirk Greef <joran@ronomon.com>
2020-10-12 13:42:17 +0200

    Extract connection management code and simplify the event loop core

---
+++ connections.zig
@@ -0,0 +1,207 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const mem = std.mem;
+const os = std.os;
+const testing = std.testing;
+const Allocator = mem.Allocator;
+
+/// This is large enough for a batch of 32768 transfers, and good for disk write throughput.
+pub const buffer_max = 4 * 1024 * 1024;
+
+pub const Connection = struct {
+    id: u32,
+    fd: os.fd_t,
+    references: usize,
+    recv_size: usize,
+    send_size: usize,
+    recv: [buffer_max]u8 = undefined,
+    send: [buffer_max]u8 = undefined,
+};
+
+/// This abstracts all our server connection management logic and static allocation.
+/// For example, when a new connection comes in, this finds a free pre-allocated connection struct.
+/// The reason why we don't simply map socket fds to an array of fixed buffers is that socket fds
+/// are sparse numbers and we would waste too much memory, especially where we already have many
+/// other fds open for journalling or replication.
+pub const Connections = struct {
+    allocator: *Allocator,
+    accepting: bool,
+    active: usize,
+    array: []Connection,
+
+    pub fn init(allocator: *Allocator, connection_count: usize) !Connections {
+        if (connection_count == 0) return error.ConnectionCountCannotBeZero;
+        if (connection_count > 128) return error.ConnectionCountRequiresExcessiveMemory;
+        var array = try allocator.alloc(Connection, connection_count);
+        for (array) |*connection, i| {
+            connection.* = .{
+                .id = 0,
+                .fd = -1,
+                .references = 0,
+                .recv_size = 0,
+                .send_size = 0,
+            };
+            @memset(connection.recv[0..], 0, connection.recv.len);
+            @memset(connection.send[0..], 0, connection.send.len);
+        }
+        return Connections {
+            .allocator = allocator,
+            .accepting = false,
+            .active = 0,
+            .array = array
+        };
+    }
+
+    pub fn deinit(self: *Connections) void {
+        assert(self.accepting == false);
+        assert(self.active == 0);
+        self.allocator.free(self.array);
+    }
+    
+    pub fn available(self: *Connections) bool {
+        assert(self.active <= self.array.len);
+        return self.active < self.array.len;
+    }
+
+    pub fn get(self: *Connections, id: u32) !*Connection {
+        if (id >= self.array.len) return error.ConnectionOutOfRange;
+        if (self.active == 0) return error.NoActiveConnections;
+        var connection = &self.array[id];
+        if (connection.fd < 0) return error.ConnectionFileDescriptorInvalid;
+        if (connection.references < 1) return error.ConnectionReferencesInvalid;
+        return connection;
+    }
+
+    pub fn set(self: *Connections, fd: os.fd_t) !*Connection {
+        assert(fd >= 0);
+        // We expect the TigerBeetle client to maintain long-lived connections.
+        // We therefore expect a thundering herd of N new connections at most every T seconds.
+        // TODO Return an error if a high reconnection rate indicates the lack of client keep alive.
+        if (!self.available()) return error.ConnectionLimitReached;
+        for (self.array) |*connection, index| {
+            if (connection.references == 0) {
+                assert(connection.id == 0);
+                assert(connection.fd == -1);
+                assert(connection.recv_size == 0);
+                assert(connection.send_size == 0);
+                connection.id = @intCast(u32, index);
+                connection.fd = fd;
+                connection.references += 1;
+                self.active += 1;
+                assert(self.active <= self.array.len);
+                return connection;
+            }
+        }
+        unreachable;
+    }
+
+    pub fn unset(self: *Connections, id: u32) !void {
+        const connection = try self.get(id);
+        if (connection.references > 1) return error.ConnectionHasLiveReferences;
+        connection.* = .{
+            .id = 0,
+            .fd = -1,
+            .references = 0,
+            .recv_size = 0,
+            .send_size = 0
+        };
+        assert(self.active > 0);
+        assert(self.active <= self.array.len);
+        self.active -= 1;
+    }
+};
+
+test "connections" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var allocator = &arena.allocator;
+
+    const count: usize = 4;
+    var connections = try Connections.init(allocator, count);
+    defer connections.deinit();
+
+    testing.expectEqual(false, connections.accepting);
+    testing.expectEqual(@as(usize, 0), connections.active);
+    testing.expectEqual(count, connections.array.len);
+    testing.expectEqual(true, connections.available());
+
+    const zeroes = [_]u8{0} ** buffer_max;
+    for (connections.array) |*connection, index| {
+        testing.expectEqual(@as(u32, 0), connection.id);
+        testing.expectEqual(@as(os.fd_t, -1), connection.fd);
+        testing.expectEqual(@as(usize, 0), connection.references);
+        testing.expectEqual(@as(usize, 0), connection.recv_size);
+        testing.expectEqual(@as(usize, 0), connection.send_size);
+        testing.expectEqualSlices(u8, zeroes[0..], connection.recv[0..]);
+        testing.expectEqualSlices(u8, zeroes[0..], connection.send[0..]);
+    }
+
+    const fd0: os.fd_t = 9;
+    const id0 = (try connections.set(fd0)).id;
+    testing.expectEqual(@as(u32, 0), id0);
+    testing.expectEqual(@as(usize, 1), connections.active);
+    testing.expectEqual(true, connections.available());
+    const co0 = try connections.get(id0);
+    testing.expectEqual(id0, co0.id);
+    testing.expectEqual(fd0, co0.fd);
+    co0.recv_size = 100;
+    co0.send_size = 100;
+
+    const fd1: os.fd_t = 8;
+    const id1 = (try connections.set(fd1)).id;
+    testing.expectEqual(@as(u32, 1), id1);
+    testing.expectEqual(@as(usize, 2), connections.active);
+    testing.expectEqual(true, connections.available());
+    const co1 = try connections.get(id1);
+    testing.expectEqual(id1, co1.id);
+    testing.expectEqual(fd1, co1.fd);
+    
+    const fd2: os.fd_t = 7;
+    const id2 = (try connections.set(fd2)).id;
+    testing.expectEqual(@as(u32, 2), id2);
+    testing.expectEqual(@as(usize, 3), connections.active);
+    testing.expectEqual(true, connections.available());
+    const co2 = try connections.get(id2);
+    testing.expectEqual(id2, co2.id);
+    testing.expectEqual(fd2, co2.fd);
+    
+    const fd3: os.fd_t = 6;
+    const id3 = (try connections.set(fd3)).id;
+    testing.expectEqual(@as(u32, 3), id3);
+    testing.expectEqual(@as(usize, 4), connections.active);
+    testing.expectEqual(false, connections.available());
+    const co3 = try connections.get(id3);
+    testing.expectEqual(id3, co3.id);
+    testing.expectEqual(fd3, co3.fd);
+    
+    testing.expectError(error.ConnectionLimitReached, connections.set(0));
+    
+    try connections.unset(id0);
+    testing.expectEqual(@as(usize, 3), connections.active);
+    testing.expectEqual(true, connections.available());
+
+    try connections.unset(id1);
+    testing.expectEqual(@as(usize, 2), connections.active);
+    testing.expectEqual(true, connections.available());
+
+    try connections.unset(id2);
+    testing.expectEqual(@as(usize, 1), connections.active);
+    testing.expectEqual(true, connections.available());
+
+    try connections.unset(id3);
+    testing.expectEqual(@as(usize, 0), connections.active);
+    testing.expectEqual(true, connections.available());
+
+    const fd4: os.fd_t = 5;
+    const id4 = (try connections.set(fd4)).id;
+    testing.expectEqual(@as(u32, 0), id4);
+    testing.expectEqual(@as(usize, 1), connections.active);
+    testing.expectEqual(true, connections.available());
+    const co4 = try connections.get(id4);
+    testing.expectEqual(id4, co4.id);
+    testing.expectEqual(fd4, co4.fd);
+    testing.expectEqual(@as(usize, 1), co4.references);
+    testing.expectEqual(@as(usize, 0), co4.recv_size);
+    testing.expectEqual(@as(usize, 0), co4.send_size);
+    try connections.unset(id4);
+}
--- server.zig
+++ server.zig
@@ -6,125 +6,110 @@ const net = std.net;
 const os = std.os;
 const linux = os.linux;
 
+usingnamespace @import("connections.zig");
 usingnamespace @import("io_uring.zig");
 
 const port = 3001;
 const connections_max = 32;
-const connection_buffers = @typeInfo(ConnectionBuffer).Enum.fields.len;
-const buffer_max = 4 * 1024 * 1024;
 const tcp_backlog = 64;
 
-const ConnectionBuffer = enum(u8) {
-    Recv,
-    Send
-};
-
-const Connections = struct {
-    accepting: bool = false,
-
-    count: usize = 0,
-    
-    fds: [connections_max]os.fd_t = [_]os.fd_t{ -1 } ** connections_max,
-    
-    // We want send/recv buffers to be next to each other for optimal memory locality.
-    // TODO Add test to assert that distance between send/recv buffers is at most buffer_max.
-    buffers: [connections_max][connection_buffers][buffer_max]u8 = undefined,
-
-    fn buffer(self: *Connections, connection: u32, purpose: ConnectionBuffer) []u8 {
-        return self.buffers[connection][@enumToInt(purpose)][0..];
-    }
-
-    fn available(self: *Connections) bool {
-        assert(self.count <= self.fds.len);
-        return self.count < self.fds.len;
-    }
-
-    fn set(self: *Connections, fd: os.fd_t) !u32 {
-        assert(fd >= 0);
-        // We expect the TigerBeetle client to maintain long-lived connections.
-        // We expect a thundering herd of N new connections at most every T seconds.
-        // TODO Return an error if a high reconnection rate indicates the lack of client keep alive.
-        if (!self.available()) return error.ConnectionLimitReached;
-        var find = true;
-        var slot: usize = undefined;
-        for (self.fds) |value, index| {
-            // Loop through all slots to assert against reuse, do not break for a free slot:
-            if (value == fd) return error.ConnectionFileDescriptorReused;
-            if (find and value == -1) {
-                slot = index;
-                find = false;
-            }
-        }
-        self.fds[slot] = fd;
-        self.count += 1;
-        return @intCast(u32, slot);
-    }
-
-    fn unset(self: *Connections, connection: u32) void {
-        assert(connection < self.fds.len);
-        assert(self.count > 0);
-        assert(self.fds[connection] >= 0);
-        std.debug.print("removing fd {} from connections for connection {}...\n", .{ self.fds[connection], connection });
-        self.fds[connection] = -1;
-        self.count -= 1;
-        std.debug.print("we now have {} connections\n", .{ self.count });
-    }
-};
-
-var connections = Connections {};
-
 const Event = packed struct {
-    op: EventOp,
-    connection: u32
+    op: enum(u32) {
+        Accept,
+        Recv,
+        Send
+    },
+    connection_id: u32
 };
 
-const EventOp = enum(u32) {
-    Accept,
-    Recv,
-    Send
-};
+var connections: Connections = undefined;
 
 fn accept(ring: *IO_Uring, fd: os.fd_t, addr: *os.sockaddr, addr_len: *os.socklen_t) !void {
     assert(connections.accepting == false);
-    const event = Event { .op = .Accept, .connection = @intCast(u32, fd) };
+    // TODO We use a runtime cast of fd to work around a packed struct default value bug here:
+    // The Accept op does not need a connection_id, it's meaningless but we must provide something.
+    const event = Event { .op = .Accept, .connection_id = @intCast(u32, fd) };
     const user_data = @bitCast(u64, event);
     _ = try ring.accept(user_data, fd, addr, addr_len, 0);
     connections.accepting = true;
 }
 
-fn recv(ring: *IO_Uring, connection: u32, offset: u64) !void {
-    const event = Event { .op = .Recv, .connection = connection };
-    const user_data = @bitCast(u64, event);
-    const fd = connections.fds[connection];
-    if (fd < 0) return error.ConnectionFileDescriptorNotFound;
-    var buffer = connections.buffer(connection, .Recv);
-    std.debug.print("receiving into buffer {} from connection {}...\n", .{
-        @enumToInt(ConnectionBuffer.Recv),
-        connection
-    });
-    _ = try ring.recv(user_data, fd, buffer[offset..], os.MSG_NOSIGNAL);
+fn accept_completed(ring: *IO_Uring, cqe: *const io_uring_cqe) !void {
+    // The Accept SQE has been fulfilled, and we are therefore no longer accepting connections:
+    // This may change at the end of the event loop, but for now we reflect this state immediately.
+    assert(connections.accepting == true);
+    connections.accepting = false;
+    // TODO Return detailed errors for accept():
+    // See https://man7.org/linux/man-pages/man2/accept.2.html
+    if (cqe.res < 0) return os.unexpectedErrno(@intCast(usize, -cqe.res));
+    // Create a connection for this socket fd:
+    var connection = try connections.set(cqe.res);
+    // Read from this connection:
+    _ = try recv(ring, connection, 0);
 }
 
-fn send(ring: *IO_Uring, connection: u32, offset: u64, size: usize) !void {
-    const event = Event { .op = .Send, .connection = connection };
+fn recv(ring: *IO_Uring, connection: *Connection, offset: u64) !void {
+    const event = Event { .op = .Recv, .connection_id = connection.id };
     const user_data = @bitCast(u64, event);
-    const fd = connections.fds[connection];
-    if (fd < 0) return error.ConnectionFileDescriptorNotFound;
-    var buffer = connections.buffer(connection, .Send);
-    std.debug.print("sending from buffer {} to connection {}...\n", .{
-        @enumToInt(ConnectionBuffer.Send),
-        connection
-    });
-    _ = try ring.send(user_data, fd, buffer[offset..size], os.MSG_NOSIGNAL);
+    assert(connection.fd >= 0);
+    assert(connection.references == 1);
+    connection.references += 1;
+    _ = try ring.recv(user_data, connection.fd, connection.recv[offset..], os.MSG_NOSIGNAL);
 }
 
-fn tcp_server_init(address: net.Address) !os.fd_t {
-    const fd = try os.socket(address.any.family, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
-    errdefer os.close(fd);
-    try os.setsockopt(fd, os.SOL_SOCKET, os.SO_REUSEADDR, &mem.toBytes(@as(c_int, 1)));
-    try os.bind(fd, &address.any, address.getOsSockLen());
-    try os.listen(fd, tcp_backlog);
-    return fd;
+fn recv_completed(ring: *IO_Uring, cqe: *const io_uring_cqe, connection: *Connection) !void {
+    assert(connection.references == 2);
+    connection.references -= 1;
+    if (cqe.res < 0) {
+        // See https://linux.die.net/man/3/recv
+        switch (-cqe.res) {
+            // The connection was forcibly closed by the peer:
+            os.ECONNRESET => try connections.unset(connection.id),
+            // The connection timed out due to a transmission timeout:
+            os.ETIMEDOUT => try connections.unset(connection.id),
+            // Shut everything down, we didn't expect this:
+            else => |errno| return os.unexpectedErrno(@intCast(usize, errno))
+        }
+    } else if (cqe.res == 0) {
+        // The peer performed an orderly shutdown.
+        try connections.unset(connection.id);
+    } else {
+        // TODO Handle a short read, where we don't have enough.
+        var size = @intCast(usize, cqe.res);
+        mem.copy(u8, connection.send[0..size], connection.recv[0..size]);
+        _ = try send(ring, connection, 0, size);
+    }
+}
+
+fn send(ring: *IO_Uring, connection: *Connection, offset: u64, size: usize) !void {
+    const event = Event { .op = .Send, .connection_id = connection.id };
+    const user_data = @bitCast(u64, event);
+    assert(connection.fd >= 0);
+    assert(connection.references == 1);
+    connection.references += 1;
+    _ = try ring.send(user_data, connection.fd, connection.send[offset..size], os.MSG_NOSIGNAL);
+}
+
+fn send_completed(ring: *IO_Uring, cqe: *const io_uring_cqe, connection: *Connection) !void {
+    assert(connection.references == 2);
+    connection.references -= 1;
+    if (cqe.res < 0) {
+        // See https://linux.die.net/man/3/send
+        switch (-cqe.res) {
+            // The connection was forcibly closed by the peer:
+            os.ECONNRESET => try connections.unset(connection.id),
+            // The socket is shut down for writing, or the socket is no longer connected.
+            os.EPIPE => try connections.unset(connection.id),
+            // Shut everything down, we didn't expect this:
+            else => |errno| return os.unexpectedErrno(@intCast(usize, errno))
+        }
+    } else {
+        // We have now done a request-response for this connection using static buffers.
+        // We don't queue reads before we have acked a batch, because otherwise a client
+        // could run ahead of us and exhaust resources. This gives us safe flow control.
+        // In other words, a connection has a single recv/send SQE pending at any time.
+        _ = try recv(ring, connection, 0);
+    }
 }
 
 fn event_loop_init(ring: *IO_Uring, server: os.fd_t) !void {
@@ -138,58 +123,14 @@ fn event_loop_init(ring: *IO_Uring, server: os.fd_t) !void {
         while (i < count) : (i += 1) {
             const cqe = cqes[i];
             const event = @bitCast(Event, cqe.user_data);
-            if (cqe.res < 0) {
-                switch (-cqe.res) {
-                    os.EPIPE => {
-                        std.debug.print("connection destroyed, event={}\n", .{ cqe.user_data });
-                    },
-                    os.ECONNRESET => {
-                        std.debug.print("connection reset, event={}\n", .{ cqe.user_data });
-                    },
-                    else => {
-                        std.debug.print("ERROR user_data={} res={}\n", .{ cqe.user_data, cqe.res });
-                        os.exit(1);
-                    }
-                }
-                continue;
-            }
             switch (event.op) {
-                .Accept => {
-                    // The Accept SQE has been fulfilled and we are no longer accepting connections:
-                    assert(connections.accepting == true);
-                    connections.accepting = false;
-                    // TODO Handle errors for accept():
-                    assert(cqe.res >= 0);
-                    // Create a connection for this socket fd:
-                    const connection = try connections.set(cqe.res);
-                    // Read from this connection:
-                    _ = try recv(ring, connection, 0);
-                },
-                .Recv => {
-                    if (cqe.res == 0) {
-                        // The peer performed an orderly shutdown.
-                        connections.unset(event.connection);
-                    } else {
-                        var size = @intCast(usize, cqe.res);
-                        
-                        var source = connections.buffer(event.connection, .Recv);
-                        var target = connections.buffer(event.connection, .Send);
-                        mem.copy(u8, target[0..size], source[0..size]);
-
-                        _ = try send(ring, event.connection, 0, size);
-                    }
-                },
-                .Send => {
-                    // We have now done a request-response for this connection using static buffers.
-                    // We don't queue reads before we have acked a batch, because otherwise a client
-                    // could run ahead of us and exhaust resources. This gives us safe flow control.
-                    // In other words, a connection only has a single SQE pending at any time.
-                    _ = try recv(ring, event.connection, 0);
-                }
+                .Accept => try accept_completed(ring, &cqe),
+                .Recv => try recv_completed(ring, &cqe, try connections.get(event.connection_id)),
+                .Send => try send_completed(ring, &cqe, try connections.get(event.connection_id))
             }
         }
         // Decide whether or not to accept another connection here in one place, since there are
-        // otherwise many branches where connections can be unset.
+        // otherwise many branches above where connections can be closed making space available.
         if (!connections.accepting and connections.available()) {
             _ = try accept(ring, server, &accept_addr, &accept_addr_len);
         }
@@ -197,8 +138,24 @@ fn event_loop_init(ring: *IO_Uring, server: os.fd_t) !void {
     }
 }
 
+fn tcp_server_init(address: net.Address) !os.fd_t {
+    const fd = try os.socket(address.any.family, os.SOCK_STREAM | os.SOCK_CLOEXEC, os.IPPROTO_TCP);
+    errdefer os.close(fd);
+    try os.setsockopt(fd, os.SOL_SOCKET, os.SO_REUSEADDR, &mem.toBytes(@as(c_int, 1)));
+    try os.bind(fd, &address.any, address.getOsSockLen());
+    try os.listen(fd, tcp_backlog);
+    return fd;
+}
+
 pub fn main() !void {
     if (builtin.os.tag != .linux) return error.LinuxRequired;
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var allocator = &arena.allocator;
+
+    connections = try Connections.init(allocator, connections_max);
+    defer connections.deinit();
 
     var addr = try net.Address.parseIp4("0.0.0.0", port);
     var server = try tcp_server_init(addr);
