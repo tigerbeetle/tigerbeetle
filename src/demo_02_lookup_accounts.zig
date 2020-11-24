b75df26a709ed41cf438c05e450aec13e2f3326f
Joran Dirk Greef <joran@ronomon.com>
2020-11-24 20:24:46 +0200

    Add api demo executables:
    ./demo_01_create_accounts
    ./demo_02_lookup_accounts
    ./demo_03_auto_commit_transfers
    ./demo_04_create_transfers
    ./demo_05_accept_transfers
    ./demo_06_reject_transfers.zig

---
+++ src/demo_02_lookup_accounts.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+usingnamespace @import("tigerbeetle.zig");
+usingnamespace @import("demo.zig");
+
+pub fn main() !void {
+    const fd = try connect(config.port);
+    defer std.os.close(fd);
+
+    var ids = [_]u128 { 1, 2 };
+    
+    try send(fd, .lookup_accounts, ids, Account);
+}
---
+++ src/demo_04_create_transfers.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+usingnamespace @import("tigerbeetle.zig");
+usingnamespace @import("demo.zig");
+
+pub fn main() !void {
+    const fd = try connect(config.port);
+    defer std.os.close(fd);
+
+    var transfers = [_]Transfer {
+        Transfer {
+            .id = 1001,
+            .debit_account_id = 1,
+            .credit_account_id = 2,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{},
+            .amount = 100_000,
+            .timeout = 0,
+        },
+        Transfer {
+            .id = 1002,
+            .debit_account_id = 1,
+            .credit_account_id = 2,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{},
+            .amount = 1,
+            .timeout = 0,
+        }
+    };
+
+    try send(fd, .create_transfers, transfers, CreateTransferResults);
+}
