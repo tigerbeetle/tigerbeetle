f2df20d191730cce7027477d5fe16cd331eb1528
Joran Dirk Greef <joran@ronomon.com>
2020-11-24 20:24:46 +0200

    Add api demo executables:
    ./demo_01_create_accounts
    ./demo_02_lookup_accounts
    ./demo_03_auto_commit_transfers
    ./demo_04_create_transfers
    ./demo_05_accept_transfers
    ./demo_06_reject_transfers.zig

---
+++ src/demo_01_create_accounts.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+usingnamespace @import("tigerbeetle.zig");
+usingnamespace @import("demo.zig");
+
+pub fn main() !void {
+    const fd = try connect(config.port);
+    defer std.os.close(fd);
+
+    var accounts = [_]Account {
+        Account {
+            .id = 1,
+            .custom = 0,
+            .flags = .{},
+            .unit = 710, // Let's use the ISO-4217 Code Number for ZAR
+            .debit_reserved = 0,
+            .debit_accepted = 0,
+            .credit_reserved = 0,
+            .credit_accepted = 0,
+            .debit_reserved_limit = 100_000,
+            .debit_accepted_limit = 1_000_000,
+            .credit_reserved_limit = 0,
+            .credit_accepted_limit = 0,
+        },
+        Account {
+            .id = 2,
+            .custom = 0,
+            .flags = .{},
+            .unit = 710,
+            .debit_reserved = 0,
+            .debit_accepted = 0,
+            .credit_reserved = 0,
+            .credit_accepted = 900_000,
+            .debit_reserved_limit = 0,
+            .debit_accepted_limit = 0,
+            .credit_reserved_limit = 200_000,
+            .credit_accepted_limit = 2_000_000_000,
+        },
+    };
+
+    try send(fd, .create_accounts, accounts, CreateAccountResults);
+}
---
+++ src/demo_05_accept_transfers.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+usingnamespace @import("tigerbeetle.zig");
+usingnamespace @import("demo.zig");
+
+pub fn main() !void {
+    const fd = try connect(config.port);
+    defer std.os.close(fd);
+
+    var commits = [_]Commit {
+        Commit {
+            .id = 1001,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{ .accept = true },
+        },
+        Commit {
+            .id = 1002,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{ .accept = true },
+        }
+    };
+
+    try send(fd, .commit_transfers, commits, CommitTransferResults);
+}
