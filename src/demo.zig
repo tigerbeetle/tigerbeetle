cb2c120cede2eff23243a47d428412ea7b353af4
Joran Dirk Greef <joran@ronomon.com>
2020-11-24 20:24:46 +0200

    Add API demo executables:
    ./demo_01_create_accounts
    ./demo_02_lookup_accounts
    ./demo_03_auto_commit_transfers
    ./demo_04_create_transfers
    ./demo_05_accept_transfers
    ./demo_06_reject_transfers

---
+++ src/demo.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+usingnamespace @import("tigerbeetle.zig");
+
+pub fn connect(port: u16) !std.os.fd_t {
+    var addr = try std.net.Address.parseIp4("127.0.0.1", port);
+    var connection = try std.net.tcpConnectToAddress(addr);
+    return connection.handle;
+}
+
+pub var request_id: u128 = 0;
+
+pub fn send(fd: std.os.fd_t, command: Command, batch: anytype, Result: anytype) !void {
+    request_id += 1;
+
+    var data = std.mem.asBytes(batch[0..]);
+
+    var request = NetworkHeader {
+        .id = request_id,
+        .command = command,
+        .size = @intCast(u32, @sizeOf(NetworkHeader) + data.len)
+    };
+    request.set_checksum_data(data[0..]);
+    request.set_checksum_meta();
+
+    const meta = std.mem.asBytes(&request);
+    assert((try std.os.sendto(fd, meta[0..], 0, null, 0)) == meta.len);
+    if (data.len > 0) {
+        assert((try std.os.sendto(fd, data[0..], 0, null, 0)) == data.len);
+    }
+
+    var recv: [1024 * 1024]u8 = undefined;
+    const recv_bytes = try std.os.recvfrom(fd, recv[0..], 0, null, null);
+    assert(recv_bytes >= @sizeOf(NetworkHeader));
+    
+    var response = std.mem.bytesAsValue(NetworkHeader, recv[0..@sizeOf(NetworkHeader)]);
+    std.debug.print("{}\n", .{ response });
+    assert(response.magic == Magic);
+    assert(response.valid_checksum_meta());
+    assert(response.valid_size());
+    assert(recv_bytes >= response.size);
+
+    const response_data = recv[@sizeOf(NetworkHeader)..response.size];
+    assert(response.valid_checksum_data(response_data));
+
+    //if (response.size > 64) std.debug.print("\n", .{});
+    for (std.mem.bytesAsSlice(Result, response_data)) |result| {
+        std.debug.print("\n{}\n", .{ result });
+    }
+}
---
+++ src/demo_06_reject_transfers.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+usingnamespace @import("tigerbeetle.zig");
+usingnamespace @import("demo.zig");
+
+pub fn main() !void {
+    const fd = try connect(config.port);
+    defer std.os.close(fd);
+
+    var commits = [_]Commit {
+        Commit {
+            .id = 1001,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{ .reject = true },
+        }
+    };
+
+    try send(fd, .commit_transfers, commits, CommitTransferResults);
+}
