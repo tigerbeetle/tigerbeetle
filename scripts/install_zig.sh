ac5968f385ba0db57b9188afa28225e6fdbe9495
Joran Dirk Greef <joran@ronomon.com>
2020-12-31 11:13:10 +0200

    Add script to install Zig

---
+++ scripts/install_zig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Determine the latest Linux master build, split the JSON line on whitespace and extract the 2nd field, then remove quotes and commas:
+ZIG_URL=`wget --quiet -O - https://ziglang.org/download/index.json | grep zig-linux-x86 | grep builds | awk '{print $2}' | sed 's/[",]//g'`
+
+# Work out the filename from the URL, as well as the directory without the ".tar.xz" file extension:
+ZIG_TARBALL=`basename "$ZIG_URL"`
+ZIG_DIRECTORY=`basename "$ZIG_TARBALL" .tar.xz`
+
+# Download, making sure we download to the same output document, without wget adding "-1" etc. if the file was previously partially downloaded:
+echo "Downloading $ZIG_URL..."
+wget --quiet --show-progress --output-document=$ZIG_TARBALL $ZIG_URL
+
+# Extract and then remove the downloaded tarball:
+echo "Extracting $ZIG_TARBALL..."
+tar -xf $ZIG_TARBALL
+rm $ZIG_TARBALL
+
+# Replace any existing Zig installation so that we can install or upgrade:
+echo "Installing $ZIG_DIRECTORY to /usr/local/lib/zig (requires root)..."
+sudo rm -rf /usr/local/lib/zig
+sudo mv $ZIG_DIRECTORY /usr/local/lib/zig
+
+# Symlink the Zig binary into /usr/local/bin, which should already be in the user's path:
+sudo ln -s --force /usr/local/lib/zig/zig /usr/local/bin/zig
+
+ZIG_VERSION=`zig version`
+echo "Congratulations, you have successfully installed Zig $ZIG_VERSION. Enjoy!"
