1902074444b3b2788d6f9092627723c194d67653
Joran Dirk Greef <joran@ronomon.com>
2021-01-29 09:59:46 +0200

    Improve demo formatting

--- src/demo_06_reject_transfers.zig
+++ src/demo_06_reject_transfers.zig
@@ -7,13 +7,15 @@ pub fn main() !void {
     const fd = try connect(config.port);
     defer std.os.close(fd);
 
-    var commits = [_]Commit{Commit{
-        .id = 1001,
-        .custom_1 = 0,
-        .custom_2 = 0,
-        .custom_3 = 0,
-        .flags = .{ .reject = true },
-    }};
+    var commits = [_]Commit{
+        Commit{
+            .id = 1001,
+            .custom_1 = 0,
+            .custom_2 = 0,
+            .custom_3 = 0,
+            .flags = .{ .reject = true },
+        },
+    };
 
     try send(fd, .commit_transfers, commits, CommitTransferResults);
 }
