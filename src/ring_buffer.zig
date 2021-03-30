b4b0b82a1e90a620b9d4dcc7d80691e6660a1008
Isaac Freund <ifreund@ifreund.xyz>
2021-03-30 18:18:49 +0200

    MessageBus: address review comments
    - Take replica index in init() so that it does not matter if the replica
    passed to MessageBus.init() is already initialized or not.
    - Avoid shutdown() syscall when a connect operation fails.
    - Avoid submitting a futher recv operation when a partial recv completes
    while shutting down.

--- src/ring_buffer.zig
+++ src/ring_buffer.zig
@@ -14,31 +14,31 @@ pub fn RingBuffer(comptime T: type, comptime size: usize) type {
         /// Add an element to the RingBuffer. Returns an error if the buffer
         /// is already full and the element could not be added.
         pub fn push(self: *Self, item: T) error{NoSpaceLeft}!void {
-            if (self.is_full()) return error.NoSpaceLeft;
+            if (self.full()) return error.NoSpaceLeft;
             self.buffer[(self.index + self.count) % self.buffer.len] = item;
             self.count += 1;
         }
 
         /// Return but do not remove the next item, if any.
         pub fn peek(self: *Self) ?T {
-            if (self.is_empty()) return null;
+            if (self.empty()) return null;
             return self.buffer[self.index];
         }
 
         /// Remove and return the next item, if any.
         pub fn pop(self: *Self) ?T {
-            if (self.is_empty()) return null;
+            if (self.empty()) return null;
             const ret = self.buffer[self.index];
             self.index = (self.index + 1) % self.buffer.len;
             self.count -= 1;
             return ret;
         }
 
-        pub fn is_full(self: Self) bool {
+        pub fn full(self: Self) bool {
             return self.count == self.buffer.len;
         }
 
-        pub fn is_empty(self: Self) bool {
+        pub fn empty(self: Self) bool {
             return self.count == 0;
         }
     };
@@ -49,14 +49,14 @@ test "push/peek/pop/full/empty" {
 
     var fifo = RingBuffer(u32, 3){};
 
-    testing.expect(!fifo.is_full());
-    testing.expect(fifo.is_empty());
+    testing.expect(!fifo.full());
+    testing.expect(fifo.empty());
 
     try fifo.push(1);
     testing.expectEqual(@as(?u32, 1), fifo.peek());
 
-    testing.expect(!fifo.is_full());
-    testing.expect(!fifo.is_empty());
+    testing.expect(!fifo.full());
+    testing.expect(!fifo.empty());
 
     try fifo.push(2);
     testing.expectEqual(@as(?u32, 1), fifo.peek());
@@ -64,19 +64,19 @@ test "push/peek/pop/full/empty" {
     try fifo.push(3);
     testing.expectError(error.NoSpaceLeft, fifo.push(4));
 
-    testing.expect(fifo.is_full());
-    testing.expect(!fifo.is_empty());
+    testing.expect(fifo.full());
+    testing.expect(!fifo.empty());
 
     testing.expectEqual(@as(?u32, 1), fifo.peek());
     testing.expectEqual(@as(?u32, 1), fifo.pop());
 
-    testing.expect(!fifo.is_full());
-    testing.expect(!fifo.is_empty());
+    testing.expect(!fifo.full());
+    testing.expect(!fifo.empty());
 
     testing.expectEqual(@as(?u32, 2), fifo.pop());
     testing.expectEqual(@as(?u32, 3), fifo.pop());
     testing.expectEqual(@as(?u32, null), fifo.pop());
 
-    testing.expect(!fifo.is_full());
-    testing.expect(fifo.is_empty());
+    testing.expect(!fifo.full());
+    testing.expect(fifo.empty());
 }
