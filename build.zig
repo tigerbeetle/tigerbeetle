b657e9cc17cb71f554150a761c46a7254457aa8a
Isaac Freund <ifreund@ifreund.xyz>
2021-03-31 13:16:07 +0200

    build: add a build.zig

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("tigerbeetle", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+
+    const run_step = b.step("run", "Run TigerBeetle");
+    run_step.dependOn(&run_cmd.step);
+}
