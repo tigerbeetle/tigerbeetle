19542e17fa99b5731fe11a765fb7de491784f9a0
Isaac Freund <ifreund@ifreund.xyz>
2021-04-06 16:21:42 +0200

    scripts: add lint.zig
    This takes advantage of the parser in the zig std to provide smarter
    lints based on the AST.
    
    To run, simply do `zig build lint`.

--- build.zig
+++ build.zig
@@ -4,15 +4,30 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("tigerbeetle", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+    {
+        const tigerbeetle = b.addExecutable("tigerbeetle", "src/main.zig");
+        tigerbeetle.setTarget(target);
+        tigerbeetle.setBuildMode(mode);
+        tigerbeetle.install();
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| run_cmd.addArgs(args);
+        const run_cmd = tigerbeetle.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| run_cmd.addArgs(args);
 
-    const run_step = b.step("run", "Run TigerBeetle");
-    run_step.dependOn(&run_cmd.step);
+        const run_step = b.step("run", "Run TigerBeetle");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    {
+        const lint = b.addExecutable("lint", "scripts/lint.zig");
+        lint.setTarget(target);
+        lint.setBuildMode(mode);
+
+        const run_cmd = lint.run();
+        run_cmd.step.dependOn(&lint.step);
+        run_cmd.addArg("src");
+
+        const lint_step = b.step("lint", "Run the linter on src/");
+        lint_step.dependOn(&run_cmd.step);
+    }
 }
