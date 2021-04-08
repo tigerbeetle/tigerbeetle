fcdde88208e4e0486e6209efaf4cda51b78fae22
Joran Dirk Greef <joran@ronomon.com>
2021-04-08 14:42:09 +0200

    Storage: Handle some interesting storage faults...
    Detect file system corruption where the size of a file in the inode
    is truncated through corruption, manifesting in a "short" read.
    For example, trying to read the last 4 MB from the end of the journal
    returns only 1 MB read, even where the underlying syscall is retried
    for interrupted reads.
    
    Subdivide multi-sector reads into sector-sized reads on encountering
    a latent sector error (LSE) for one of the sectors, to recover from
    and read around the faulty sector.
    
    Zero a single-sector read on encountering a latent sector error to do
    recovery at a higher layer using end-to-end checksums and redundancy.

--- src/vr.zig
+++ src/vr.zig
@@ -289,6 +289,8 @@ pub const Storage = struct {
         assert(buffer.len > 0);
         assert(offset + buffer.len <= self.size);
 
+        // Ensure that the read or write is aligned correctly for Direct I/O:
+        // If this is not the case, the underlying syscall will return EINVAL.
         assert(@mod(@ptrToInt(buffer.ptr), config.sector_size) == 0);
         assert(@mod(buffer.len, config.sector_size) == 0);
         assert(@mod(offset, config.sector_size) == 0);
@@ -296,20 +298,73 @@ pub const Storage = struct {
 
     fn read(self: *Storage, buffer: []u8, offset: u64) void {
         self.assert_bounds_and_alignment(buffer, offset);
-        // TODO Subdivide read into individual sectors if a we get an EIO (latent sector error).
+
+        if (self.read_all(buffer, offset)) |bytes_read| {
+            if (bytes_read != buffer.len) {
+                assert(bytes_read < buffer.len);
+                log.emerg("short read: bytes_read={} buffer_len={} offset={}", .{
+                    bytes_read,
+                    buffer.len,
+                    offset,
+                });
+                @panic("fs corruption: file inode size truncated");
+            }
+        } else |err| switch (err) {
+            error.InputOutput => {
+                // The disk was unable to read some sectors (an internal CRC or hardware failure):
+                if (buffer.len > config.sector_size) {
+                    log.err("latent sector error: offset={}, subdividing read...", .{offset});
+                    // Subdivide the read into sectors to read around the faulty sector(s):
+                    // This is considerably slower than doing a bulk read.
+                    // By now we might have also experienced the disk's read timeout (in seconds).
+                    // TODO Docs should instruct on why and how to reduce disk firmware timeouts.
+                    var buffer_offset = 0;
+                    while (buffer_offset < buffer.len) : (buffer_offset += config.sector_size) {
+                        self.read(
+                            buffer[buffer_offset..][0..config.sector_size],
+                            offset + buffer_offset,
+                        );
+                    }
+                    assert(buffer_offset == buffer.len);
+                } else {
+                    // Zero any remaining sectors that cannot be read:
+                    // We treat these EIO errors the same as a checksum failure.
+                    log.err("latent sector error: offset={}, zeroing buffer sector...", .{offset});
+                    assert(buffer.len == config.sector_size);
+                    mem.set(u8, buffer, 0);
+                }
+            },
+            else => {
+                log.emerg("impossible read: buffer_len={} offset={} error={}", .{
+                    buffer_len,
+                    offset,
+                    err,
+                });
+                @panic("impossible read");
+            },
+        }
+    }
+
+    fn read_all(self: *Storage, buffer: []u8, offset: u64) !u64 {
         std.mem.copy(u8, buffer, self.memory[offset .. offset + buffer.len]);
+        return buffer.len;
     }
 
     fn write(self: *Storage, buffer: []const u8, offset: u64) void {
         self.assert_bounds_and_alignment(buffer, offset);
+        self.write_all(buffer, offset) catch |err| switch (err) {
+            // We assume that the disk will attempt to reallocate a spare sector for any LSE.
+            // TODO What if we receive an EIO error because of a faulty cable?
+            error.InputOutput => @panic("latent sector error: no spare sectors to reallocate"),
+            else => {
+                log.emerg("write: buffer.len={} offset={} error={}", .{ buffer.len, offset, err });
+                @panic("unrecoverable disk error");
+            },
+        };
+    }
+
+    fn write_all(self: *Storage, buffer: []const u8, offset: u64) !void {
         std.mem.copy(u8, self.memory[offset .. offset + buffer.len], buffer);
-        // pwriteAll(buffer, offset) catch |err| switch (err) {
-        //     error.InputOutput => @panic("latent sector error: no spare sectors to reallocate"),
-        //     else => {
-        //         log.emerg("write: error={} buffer.len={} offset={}", .{err, buffer.len, offset});
-        //         @panic("unrecoverable disk error");
-        //     },
-        // };
     }
 };
 
