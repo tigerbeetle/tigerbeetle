56ba1dfebfc444446d1f2f782a57cd9610dc3a92
Joran Dirk Greef <joran@coil.com>
2021-05-06 13:57:10 +0200

    scripts/benchmark.sh: Output benchmark.log on error

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
+COLOR_RED='\033[1;31m'
+COLOR_END='\033[0m'
+
 zig build -Drelease-safe
 mv zig-out/bin/tigerbeetle .
+
+function onerror {
+    if [ "$?" == "0" ]; then
+        rm benchmark.log
+    else
+        echo -e "${COLOR_RED}"
+        echo "Error running benchmark, here are more details (from benchmark.log):"
+        echo -e "${COLOR_END}"
+        cat benchmark.log
+    fi
+
+    for I in 0 1
+    do
+        echo "Stopping replica $I..."
+    done
+    kill %1 %2
+}
+trap onerror EXIT
 
 CLUSTER_ID="--cluster-id=0a5ca1ab1ebee11e"
 REPLICA_ADDRESSES="--replica-addresses=3001,3002"
@@ -14,17 +35,9 @@ do
 done
 
 # Wait for replicas to start, listen and connect:
-sleep 2
+sleep 1
 
 echo ""
 echo "Benchmarking..."
 zig run -OReleaseSafe src/benchmark.zig
 echo ""
-
-for I in 0 1
-do
-    echo "Stopping replica $I..."
-done
-kill %1 %2
-
-rm benchmark.log
