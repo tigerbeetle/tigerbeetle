a6499979c6f410716af5f60a707336b01d1a04cb
Joran Dirk Greef <joran@coil.com>
2021-05-06 12:18:16 +0200

    scripts/benchmark.sh: Use zig-out binary, log to benchmark.log

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
 zig build -Drelease-safe
+mv zig-out/bin/tigerbeetle .
 
 CLUSTER_ID="--cluster-id=0a5ca1ab1ebee11e"
 REPLICA_ADDRESSES="--replica-addresses=3001,3002"
@@ -8,7 +10,7 @@ REPLICA_ADDRESSES="--replica-addresses=3001,3002"
 for I in 0 1
 do
     echo "Starting replica $I..."
-    ./tigerbeetle $CLUSTER_ID $REPLICA_ADDRESSES --replica-index=$I > /dev/null 2>&1 &
+    ./tigerbeetle $CLUSTER_ID $REPLICA_ADDRESSES --replica-index=$I > benchmark.log 2>&1 &
 done
 
 # Wait for replicas to start, listen and connect:
@@ -24,3 +26,5 @@ do
     echo "Stopping replica $I..."
 done
 kill %1 %2
+
+rm benchmark.log
