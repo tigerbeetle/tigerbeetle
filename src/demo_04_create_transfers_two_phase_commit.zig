910ab0c24e9ed855b72a0a3ea4ea37a0282c2c0a
Joran Dirk Greef <joran@coil.com>
2021-05-20 13:28:17 +0200

    docs: Add linked transfer example into demo_04

--- src/demo_04_create_transfers_two_phase_commit.zig
+++ src/demo_04_create_transfers_two_phase_commit.zig
@@ -31,6 +31,22 @@ pub fn main() !void {
             .code = 0,
             .flags = .{
                 .two_phase_commit = true,
+                .linked = true, // Link this transfer with the next transfer 1003.
+            },
+            .amount = 1,
+        },
+        Transfer{
+            .id = 1003,
+            .debit_account_id = 1,
+            .credit_account_id = 2,
+            .user_data = 0,
+            .reserved = [_]u8{0} ** 32,
+            .timeout = std.time.ns_per_hour,
+            .code = 0,
+            .flags = .{
+                .two_phase_commit = true,
+                // The last transfer in a linked chain has .linked set to false to close the chain.
+                // This transfer will succeed or fail together with transfer 1002 above.
             },
             .amount = 1,
         },
