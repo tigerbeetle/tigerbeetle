db433f679fb3561a620904f8af44f3bbd51aa7a7
Isaac Freund <ifreund@ifreund.xyz>
2021-06-23 21:40:46 +0200

    clients/node: fix: pin zig version to 0.8.0
    Zig 0.8.0 is recent enough to have everything we need and sticking with
    this version for now will cause less time to be wasted updating
    tigerbeetle and tigerbeetle-node's zig code for breaking zig changes.

--- src/clients/node/scripts/postinstall.sh
+++ src/clients/node/scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-./src/tigerbeetle/scripts/install_zig.sh
+./src/tigerbeetle/scripts/install_zig.sh 0.8.0
 ./scripts/download_node_headers.sh
 mkdir -p dist &&  zig/zig build-lib -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -femit-bin=dist/client.node
