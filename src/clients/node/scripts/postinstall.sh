b5005035cd0c19c02b9bd843c1adee145016a455
Joran Dirk Greef <joran@coil.com>
2021-08-30 10:43:49 +0200

    clients/node: Fix postinstall.sh to set -mcpu=baseline and -OReleaseSafe

--- src/clients/node/scripts/postinstall.sh
+++ src/clients/node/scripts/postinstall.sh
@@ -3,4 +3,4 @@ set -e
 
 ./src/tigerbeetle/scripts/install_zig.sh 0.8.0
 ./scripts/download_node_headers.sh
-mkdir -p dist &&  zig/zig build-lib -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -femit-bin=dist/client.node
+mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -femit-bin=dist/client.node
