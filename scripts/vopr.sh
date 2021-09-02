72d4c9374a142730b3983cbc640bab3bb4a8808b
Joran Dirk Greef <joran@coil.com>
2021-09-02 13:40:45 +0200

    VOPR: Toggle seed build mode and install Zig if needed

--- scripts/vopr.sh
+++ scripts/vopr.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 set -e
-if [ "$1" ]; then
-    echo "Running the simulator in debug mode with full debug logging enabled..."
+
+# Install Zig 0.8.0 if it does not already exist:
+if [ ! -d "zig" ]; then
+    scripts/install_zig.sh 0.8.0
     echo ""
-    # TODO Be able to toggle the replay build mode.
-    zig/zig run src/simulator.zig -ODebug -- $1
+    echo "Running the VOPR for the first time..."
+fi
+
+# If a seed is provided as an argument then replay the seed, otherwise test a 1,000 seeds:
+if [ "$1" ]; then
+
+    # Build in fast ReleaseSafe mode if required, useful where you don't need debug logging:
+    if [ "$2" == "-OReleaseSafe" ]; then
+        echo "Replaying seed $1 in ReleaseSafe mode..."
+        BUILD_MODE="-OReleaseSafe"
+    else
+        echo "Replaying seed $1 in Debug mode with full debug logging enabled..."
+        BUILD_MODE="-ODebug"
+    fi
+    echo ""
+
+    zig/zig run src/simulator.zig $BUILD_MODE -- $1
 else
     for I in {1..1000}
     do
