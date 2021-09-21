b155cc1100b9e65d800a8908cac0be6f60212f21
Joran Dirk Greef <joran@coil.com>
2021-09-21 12:02:06 +0200

    scripts/install.sh: Update to Zig 0.8.1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-scripts/install_zig.sh 0.8.0
+scripts/install_zig.sh 0.8.1
 echo "Building TigerBeetle..."
 zig/zig build -Dcpu=baseline -Drelease-safe
 mv zig-out/bin/tigerbeetle .
