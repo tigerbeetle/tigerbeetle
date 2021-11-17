af576bef6ba41579760bef3afcc1871fd28e2536
sentientwaffle <sentientwaffle@gmail.com>
2021-11-17 11:12:06 -0800

    bench: fix build script for benchmark

--- build.zig
+++ build.zig
@@ -43,4 +43,16 @@ pub fn build(b: *std.build.Builder) void {
         const test_step = b.step("test", "Run the unit tests");
         test_step.dependOn(&unit_tests.step);
     }
+
+    {
+        const benchmark = b.addExecutable("benchmark_array_search", "src/benchmark_array_search.zig");
+        benchmark.setTarget(target);
+        benchmark.setBuildMode(mode);
+        //const run_cmd = tigerbeetle.run();
+        const run_cmd = benchmark.run();
+
+        //const test_step = b.step("test", "Run the unit tests");
+        const step = b.step("benchmark_array_search", "Benchmark array search");
+        step.dependOn(&run_cmd.step);
+    }
 }
