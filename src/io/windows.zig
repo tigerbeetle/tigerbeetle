09fb268d77901449df6b3aa8ba710b31bed4ce9d
kprotty <kbutcher6200@gmail.com>
2021-12-06 11:26:20 -0600

    io: fix windows backend + io tests pass

--- src/io/windows.zig
+++ src/io/windows.zig
@@ -18,6 +18,9 @@ pub const IO = struct {
         _ = entries;
         _ = flags;
 
+        _ = try os.windows.WSAStartup(2, 2);
+        errdefer os.windows.WSACleanup() catch unreachable;
+
         const iocp = try os.windows.CreateIoCompletionPort(os.windows.INVALID_HANDLE_VALUE, null, 0, 0);
         errdefer os.windows.CloseHandle(iocp);
 
@@ -37,6 +40,8 @@ pub const IO = struct {
         assert(self.iocp != os.windows.INVALID_HANDLE_VALUE);
         os.windows.CloseHandle(self.iocp);
         self.iocp = os.windows.INVALID_HANDLE_VALUE;
+
+        os.windows.WSACleanup() catch unreachable;
     }
 
     pub fn tick(self: *IO) !void {
@@ -46,7 +51,7 @@ pub const IO = struct {
     pub fn run_for_ns(self: *IO, nanoseconds: u63) !void {
         const Callback = struct {
             fn onTimeout(timed_out: *bool, completion: *Completion, result: TimeoutError!void) void {
-                _ = result;
+                _ = result catch unreachable;
                 _ = completion;
                 timed_out.* = true;
             }
@@ -72,10 +77,14 @@ pub const IO = struct {
             // NOTE: this may push to completed queue
             var timeout_ms: ?os.windows.DWORD = null;
             if (self.flush_timeouts()) |expires_ns| {
-                assert(expires_ns != 0); // 0ns expires should have been completed not returned
-                const expires_ms = @divCeil(expires_ns, std.time.ns_per_ms);
-                assert(expires_ms != 0); // divCeil should round up to 1ms for sub-millis timeouts
-                timeout_ms = std.math.cast(os.windows.DWORD, expires_ms) catch os.windows.INFINITE - 1;
+                // 0ns expires should have been completed not returned
+                assert(expires_ns != 0); 
+                // Round up sub-millisecond expire times to the next millisecond
+                const expires_ms = (expires_ns + (std.time.ns_per_ms / 2)) / std.time.ns_per_ms;
+                // Saturating cast to DWORD milliseconds
+                const expires = std.math.cast(os.windows.DWORD, expires_ms) catch std.math.maxInt(os.windows.DWORD);
+                // max DWORD is reserved for INFINITE so cap the cast at max - 1
+                timeout_ms = if (expires == os.windows.INFINITE) expires - 1 else expires;
             }
             
             // Poll for IO iff theres IO pending and flush_timeouts() found no ready completions
@@ -94,7 +103,7 @@ pub const IO = struct {
                 self.io_pending -= num_events;
 
                 for (events[0..num_events]) |event| {
-                    const afd_completion = event.asCompletion();
+                    const afd_completion = event.as_completion() orelse unreachable;
                     const completion = @fieldParentPtr(Completion, "afd_completion", afd_completion);
                     completion.next = null;
                     self.completed.push(completion);
@@ -113,11 +122,11 @@ pub const IO = struct {
     }
 
     fn flush_timeouts(self: *IO) ?u64 {
-        var min_timeout: ?u64 = null;
+        var min_expires: ?u64 = null;
         var current_time: ?u64 = null;
         var timeouts: ?*Completion = self.timeouts.peek();
 
-        // iterate through the timeouts, returning min_timeout at the end
+        // iterate through the timeouts, returning min_expires at the end
         while (timeouts) |completion| {
             timeouts = completion.next;
 
@@ -134,11 +143,14 @@ pub const IO = struct {
 
             // if it's still waiting, update min_timeout
             const expires = completion.operation.timeout.deadline - now;
-            const min_expires = min_timeout orelse 0;
-            min_timeout = std.math.min(expires, min_expires);
+            if (min_expires) |current_min_expires| {
+                min_expires = std.math.min(expires, current_min_expires);
+            } else {
+                min_expires = expires;
+            }
         }
 
-        return min_timeout;
+        return min_expires;
     }
 
     /// This struct holds the data needed for a single IO operation
@@ -160,6 +172,7 @@ pub const IO = struct {
         connect: struct {
             socket: os.socket_t,
             address: std.net.Address,
+            initiated: bool,
         },
         fsync: struct {
             fd: os.fd_t,
@@ -212,11 +225,19 @@ pub const IO = struct {
                     .accept, .recv => @as(?Afd.Operation, .read),
                     .connect, .send => @as(?Afd.Operation, .write),
                     else => null,
-                }) |afd_op| blk: {
+                }) |afd_op| {
                     _ = result catch |err| switch (err) {
                         error.WouldBlock => {
                             const afd_completion = &_completion.afd_completion;
-                            io.afd.schedule(_op_data.socket, afd_op, afd_completion) catch break :blk;
+                            io.afd.schedule(_op_data.socket, afd_op, afd_completion) catch {
+                                // On AFD schedulue failure try to perform the op again next tick
+                                _completion.next = null;
+                                io.completed.push(_completion);
+                                return;
+                            };
+
+                            // We've scheduled the completion to be received when polling on IO
+                            io.io_pending += 1;
                             return;
                         },
                         else => {},
@@ -225,7 +246,7 @@ pub const IO = struct {
                 
                 // The completion is finally ready to invoke the callback
                 callback(
-                    @ptrCast(Context, @alignCast(@alignOf(Context), _completion.context)),
+                    @intToPtr(Context, @ptrToInt(_completion.context)),
                     _completion,
                     result,
                 );
@@ -303,14 +324,20 @@ pub const IO = struct {
             .{ .fd = fd },
             struct {
                 fn doOperation(op: anytype) CloseError!void {
+                    // Check if the fd is a SOCKET by seeing if getsockopt() returns ENOTSOCK
+                    // https://stackoverflow.com/a/50981652
                     const socket = @ptrCast(os.socket_t, op.fd);
+                    getsockoptError(socket) catch |err| switch (err) {
+                        error.FileDescriptorNotASocket => return os.windows.CloseHandle(op.fd),
+                        else => {},
+                    };
                     os.closeSocket(socket);
                 }
             },
         );
     }
 
-    pub const ConnectError = os.ConnectError;
+    pub const ConnectError = os.ConnectError || error{FileDescriptorNotASocket};
 
     pub fn connect(
         self: *IO,
@@ -333,10 +360,43 @@ pub const IO = struct {
             .{
                 .socket = socket,
                 .address = address,
+                .initiated = false,
             },
             struct {
                 fn doOperation(op: anytype) ConnectError!void {
-                    return os.connect(op.socket, &op.address.any, op.address.getOsSockLen());
+                    // Don't call connect after being rescheduled by io_pending as it gives EISCONN.
+                    // Instead, check the socket error to see if has been connected successfully.
+                    if (op.initiated)
+                        return getsockoptError(op.socket);
+
+                    op.initiated = true;
+                    const rc = os.windows.ws2_32.connect(
+                        op.socket, 
+                        &op.address.any, 
+                        @intCast(i32, op.address.getOsSockLen()),
+                    );
+
+                    // Need to hardcode as os.connect has unreachable on .WSAEWOULDBLOCK
+                    if (rc == 0) return;                    
+                    switch (os.windows.ws2_32.WSAGetLastError()) {
+                        .WSAEADDRINUSE => return error.AddressInUse,
+                        .WSAEADDRNOTAVAIL => return error.AddressNotAvailable,
+                        .WSAECONNREFUSED => return error.ConnectionRefused,
+                        .WSAECONNRESET => return error.ConnectionResetByPeer,
+                        .WSAETIMEDOUT => return error.ConnectionTimedOut,
+                        .WSAEHOSTUNREACH,
+                        .WSAENETUNREACH,
+                        => return error.NetworkUnreachable,
+                        .WSAEFAULT => unreachable,
+                        .WSAEINVAL => unreachable,
+                        .WSAEISCONN => unreachable,
+                        .WSAENOTSOCK => unreachable,
+                        .WSAEWOULDBLOCK => return error.WouldBlock,
+                        .WSAEACCES => unreachable,
+                        .WSAENOBUFS => return error.SystemResources,
+                        .WSAEAFNOSUPPORT => return error.AddressFamilyNotSupported,
+                        else => |err| return os.windows.unexpectedWSAError(err),
+                    }
                 }
             },
         );
@@ -550,13 +610,66 @@ pub const IO = struct {
         );
     }
 
-    pub const INVALID_SOCKET = os.windows.INVALID_SOCKET;
+    pub const INVALID_SOCKET = os.windows.ws2_32.INVALID_SOCKET;
 
     pub fn openSocket(family: u32, sock_type: u32, protocol: u32) !os.socket_t {
         return os.socket(family, sock_type | os.SOCK_NONBLOCK | os.SOCK_CLOEXEC, protocol);
     }
 };
 
+// TODO: use os.getsockoptError when fixed in stdlib
+fn getsockoptError(socket: os.socket_t) IO.ConnectError!void {
+    var err_code: u32 = undefined;
+    var size: i32 = @sizeOf(u32);
+    const rc = os.windows.ws2_32.getsockopt(
+        socket, 
+        os.SOL_SOCKET,
+        os.SO_ERROR, 
+        std.mem.asBytes(&err_code), 
+        &size,
+    );
+    
+    if (rc != 0) {
+        switch (os.windows.ws2_32.WSAGetLastError()) {
+            .WSAENETDOWN => return error.NetworkUnreachable,
+            .WSANOTINITIALISED => unreachable, // WSAStartup() was never called
+            .WSAEFAULT => unreachable, // The address pointed to by optval or optlen is not in a valid part of the process address space.
+            .WSAEINVAL => unreachable, // The level parameter is unknown or invalid
+            .WSAENOPROTOOPT => unreachable, // The option is unknown at the level indicated.
+            .WSAENOTSOCK => return error.FileDescriptorNotASocket,
+            else => |err| return os.unexpectedErrno(@enumToInt(err)),
+        }
+    }
+    
+    assert(size == 4);
+    switch (err_code) {
+        0 => return,
+        os.EACCES => return error.PermissionDenied,
+        os.EPERM => return error.PermissionDenied,
+        os.EADDRINUSE => return error.AddressInUse,
+        os.EADDRNOTAVAIL => return error.AddressNotAvailable,
+        os.EAFNOSUPPORT => return error.AddressFamilyNotSupported,
+        os.EAGAIN => return error.SystemResources,
+        os.EALREADY => return error.ConnectionPending,
+        os.EBADF => unreachable, // sockfd is not a valid open file descriptor.
+        os.ECONNREFUSED => return error.ConnectionRefused,
+        os.EFAULT => unreachable, // The socket structure address is outside the user's address space.
+        os.EISCONN => unreachable, // The socket is already connected.
+        os.ENETUNREACH => return error.NetworkUnreachable,
+        os.ENOTSOCK => unreachable, // The file descriptor sockfd does not refer to a socket.
+        os.EPROTOTYPE => unreachable, // The socket type does not support the requested communications protocol.
+        os.ETIMEDOUT => return error.ConnectionTimedOut,
+        os.ECONNRESET => return error.ConnectionResetByPeer,
+        else => |err| return os.unexpectedErrno(err),
+    }
+}
+
+/// Windows AFD is an internal system not exposed by kernel32
+/// which is used to monitor sockets and pipes for IO readiness.
+/// It's an alternative to stable IOCP calls and is used in other projects:
+/// - https://github.com/piscisaureus/wepoll
+/// - https://github.com/tokio-rs/mio/blob/e55ec59cf287dd4bdc06c70bfadea74f85c8bf09/src/sys/windows/afd.rs
+/// - https://github.com/libuv/libuv/blob/47e0c5c575e92a25e0da10fc25b2732942c929f3/src/win/winsock.c#L461
 const Afd = struct {
     const AFD_POLL_HANDLE_INFO = extern struct {
         Handle: os.windows.HANDLE,
@@ -644,6 +757,9 @@ const Afd = struct {
         io_status_block: os.windows.IO_STATUS_BLOCK,
     };
 
+    // Schedule a Completion event to be reported by poll()
+    // once the Operation is observed to be ready on the socket.
+    // This is similar to EPOLL_CTL_MOD with EPOLLONESHOT.
     fn schedule(
         self: Afd,
         socket: os.socket_t,
@@ -695,7 +811,7 @@ const Afd = struct {
     const Event = extern struct {
         overlapped_entry: os.windows.OVERLAPPED_ENTRY,
 
-        fn asCompletion(self: Event) ?*Completion {
+        fn as_completion(self: Event) ?*Completion {
             const overlapped = self.overlapped_entry.lpOverlapped;
             const io_status_block = @ptrCast(*os.windows.IO_STATUS_BLOCK, overlapped);
             return @fieldParentPtr(Completion, "io_status_block", io_status_block);
@@ -714,10 +830,9 @@ const Afd = struct {
             timeout_ms,
             true, // alertable wait
         ) catch |err| switch (err) {
-            error.Aborted => {},
-            error.Cancelled => {},
-            error.EOF => {},
-            error.Timeout => {},
+            error.Timeout,
+            error.Aborted,
+            error.Cancelled => @as(usize, 0),
             else => |e| return e,
         };
     }
