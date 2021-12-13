afcb8092fb4f309b7d1f7a0fcaeb680cbb1998cd
koekiebox <jason@koekiebox.com>
2021-12-13 10:22:17 +0100

    Issue #63: * Fix Demo.zig * Test transfer lookup

--- src/demo_07_lookup_transfers.zig
+++ src/demo_07_lookup_transfers.zig
@@ -2,7 +2,7 @@ usingnamespace @import("tigerbeetle.zig");
 usingnamespace @import("demo.zig");
 
 pub fn main() !void {
-    const ids = [_]u128{ 1, 2 };
+    const ids = [_]u128{ 1000, 1001, 1002 };
 
     try Demo.request(.lookup_transfers, ids, Demo.on_lookup_transfers);
 }
