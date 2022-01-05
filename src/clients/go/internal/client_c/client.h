eba437ec0d77465e7528fb2e4db76fd99de1feb0
Donovan Changfoot <donovanchangfoot@gmail.com>
2022-01-05 09:36:29 +0200

    clients/go: C header definition for TigerBeetle Context along with implementation.
    This provides a C ABI to `init` and `deinit` a TigerBeetle context. This
    context contains the Zig client, message bus and globally shared IO
    needed to make requests. `tick` is exposed so users can drive the client
    as they see fit.

--- src/clients/go/internal/client_c/client.h
+++ src/clients/go/internal/client_c/client.h
@@ -1,10 +1,17 @@
 #ifndef TB_CONTEXT_H_C
 #define TB_CONTEXT_H_C
 
-typedef struct TB_Context {} TB_Context;
+typedef struct TB_Context {
+	uint8_t client [5120];
+	uint8_t message_bus [144];
+	uint8_t io [208];
+	unsigned __int128 client_id;
+} TB_Context;
 
-int32_t init (const struct TB_Context*);
+int32_t init (struct TB_Context* const, const uint32_t cluster, char* const addresses, const uint32_t addresses_length);
 
-typedef void (*callback_fcn)(uint32_t);
-int32_t request(const struct TB_Context*, callback_fcn);
+typedef void (*results_callback)(void* const user_data, const uint8_t error, const uint8_t operation, uint8_t* const results, const uint32_t results_length);
+int32_t request(struct TB_Context* const, void* const user_data, results_callback, const uint8_t operation, uint8_t* const batch_raw, const uint32_t batch_raw_length);
+int32_t tick (struct TB_Context* const);
+void deinit(struct TB_Context* const);
 #endif
