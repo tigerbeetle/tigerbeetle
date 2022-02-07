9898a23621da5d34b567d6aff1ddac1d4c535328
Joran Dirk Greef <joran@coil.com>
2022-02-07 14:53:03 +0200

    clients/node: Zig: Update to 0.9.0
    * Remove ZIG_SYSTEM_LINKER_HACK no longer needed (thanks Jakub!)
    * Use namespaced N-API constants (can't be literals any more)
    * Set Zig default release only within `scripts/install_zig.sh`
    * Update allocator for Allocgate
    * Rename `c_void` to `anyopaque`
    * Remove unused locals
    * Remove deprecated log levels

--- src/clients/node/scripts/postinstall.sh
+++ src/clients/node/scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-./src/tigerbeetle/scripts/install_zig.sh 0.8.1
+./src/tigerbeetle/scripts/install_zig.sh
 ./scripts/download_node_headers.sh
-mkdir -p dist && ZIG_SYSTEM_LINKER_HACK=1 zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
+mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
