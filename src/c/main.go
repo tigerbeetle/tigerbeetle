4c269f3e4bd6f31d74305d1422639996c6e8c3f2
kprotty <kbutcher6200@gmail.com>
2022-03-01 07:13:12 -0600

    c: current go client which compiles

--- src/c/main.go
+++ src/c/main.go
@@ -4,20 +4,77 @@ package main
 #cgo LDFLAGS: -L../../zig-out -ltb_client
 
 #include <stdlib.h>
+#include <string.h>
 #include "./tb_client.h"
 
 void onGoPacketCompletion(uintptr_t ctx, tb_client_t client, tb_packet_list_t* packets);
 
-static void tb_packet_set_operation(tb_packet_t* packet, TB_OPERATION operation) {
+static void tb_client_request(
+	tb_client_t client,
+	tb_packet_t* packet,
+	TB_OPERATION operation,
+	void* request_data,
+	uintptr_t user_data
+) {
+	size_t req_bytes;
+	switch (operation)
+	{
+	case TB_OP_CREATE_ACCOUNTS:
+		req_bytes = sizeof(tb_account_t);
+		break;
+	case TB_OP_CREATE_TRANSFERS:
+		req_bytes = sizeof(tb_transfer_t);
+		break;
+	case TB_OP_COMMIT_TRANSFERS:
+		req_bytes = sizeof(tb_commit_t);
+		break;
+	case TB_OP_LOOKUP_ACCOUNTS:
+	case TB_OP_LOOKUP_TRANSFERS:
+		req_bytes = sizeof(tb_uint128_t);
+		break;
+	default:
+		__builtin_unreachable();
+	}
+
+	packet->user_data = user_data;
 	packet->operation = operation;
+	memcpy(&packet->data.request, request_data, req_bytes);
+
+	tb_packet_list_t list;
+	list.head = packet;
+	list.tail = packet;
+	packet->next = NULL;
+
+	tb_client_submit(client, &list);
 }
 
-static void tb_packet_set_request(tb_packet_t* packet, tb_event_t event) {
-	packet->data.request = event;
-}
+static void tb_client_response(
+	tb_packet_t* packet,
+	void* response_data
+) {
+	size_t resp_bytes;
+	switch (packet->operation)
+	{
+	case TB_OP_CREATE_ACCOUNTS:
+		resp_bytes = sizeof(tb_create_accounts_result_t);
+		break;
+	case TB_OP_CREATE_TRANSFERS:
+		resp_bytes = sizeof(tb_create_transfers_result_t);
+		break;
+	case TB_OP_COMMIT_TRANSFERS:
+		resp_bytes = sizeof(tb_commit_transfers_result_t);
+		break;
+	case TB_OP_LOOKUP_ACCOUNTS:
+		resp_bytes = sizeof(tb_account_t);
+		break;
+	case TB_OP_LOOKUP_TRANSFERS:
+		resp_bytes = sizeof(tb_transfer_t);
+		break;
+	default:
+		__builtin_unreachable();
+	}
 
-static tb_result_t tb_packet_get_response(tb_packet_t* packet) {
-	return packet->data.response;
+	memcpy(response_data, &packet->data.response, resp_bytes);
 }
 */
 import "C"
@@ -128,28 +185,36 @@ func NewClient(clusterID uint32, addresses []string) (Client, error) {
 	return c, nil
 }
 
-func (c *c_client) Request(op C.TB_OPERATION, event C.tb_event_t) C.tb_result_t {
+func (c *c_client) doRequest(op C.TB_OPERATION, event_data, result_data unsafe.Pointer) {
 	req := <- c.requests
-	req.packet.user_data = C.uintptr_t((uintptr)((unsafe.Pointer)(req)))
-	C.tb_packet_set_operation(req.packet, op)
-	C.tb_packet_set_request(req.packet, event)
 
-	var packets C.tb_packet_list_t
-	packets.head = req.packet
-	packets.tail = req.packet
-	req.packet.next = nil
-
-	C.tb_client_submit(c.tb_client, &packets)
+	user_data := C.uintptr_t((uintptr)((unsafe.Pointer)(req)))
+	C.tb_client_request(c.tb_client, req.packet, op, event_data, user_data)
+	
 	<- req.ready
+	C.tb_client_response(req.packet, result_data)
 
-	resp := C.tb_packet_get_response(req.packet)
 	c.requests <- req
-	return resp
 }
 
 func (c *c_client) CreateAccounts(account Account) (CreateAccountsResult, error) {
-	var r CreateAccountsResult
-	return r, nil
+	var result CreateAccountsResult
+	c.doRequest(C.TB_OP_CREATE_ACCOUNTS, (unsafe.Pointer)(&account), (unsafe.Pointer)(&result))
+
+	if result.index != 0 {
+		panic("invalid CreateAccountsResult index")
+	}
+	
+	var err error
+	switch result.result {
+	case C.TB_CREATE_ACCOUNT_OK:
+		err = nil
+		break
+	default:
+		panic("todo")
+	}
+
+	return result, err
 }
 
 func (c *c_client) CreateTransfers(transfer Transfer) (CreateTransfersResult, error) {
