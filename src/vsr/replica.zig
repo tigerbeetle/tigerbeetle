matklad/tigerbeetle-filtered

RingBuffer: add buffer type option and push_slice()
We plan to use this for our Table and Level iterators in the LSM tree
implementation.

Co-authored-by: Joran Dirk Greef <joran@coil.com>

src/vsr/replica.zig
@@ -133,7 +133,7 @@ pub fn Replica(
 
         /// The leader's pipeline of inflight prepares waiting to commit in FIFO order.
         /// This allows us to pipeline without the complexity of out-of-order commits.
-        pipeline: RingBuffer(Prepare, config.pipelining_max) = .{},
+        pipeline: RingBuffer(Prepare, config.pipelining_max, .array) = .{},
 
         /// In some cases, a replica may send a message to itself. We do not submit these messages
         /// to the message bus but rather queue them here for guaranteed immediate delivery, which

tigerbeetle/tigerbeetle

src/vsr/replica.zig
@@ -136,7 +136,7 @@ pub fn Replica(
 
         /// The leader's pipeline of inflight prepares waiting to commit in FIFO order.
         /// This allows us to pipeline without the complexity of out-of-order commits.
-        pipeline: RingBuffer(Prepare, config.pipeline_max) = .{},
+        pipeline: RingBuffer(Prepare, config.pipeline_max, .array) = .{},
 
         /// In some cases, a replica may send a message to itself. We do not submit these messages
         /// to the message bus but rather queue them here for guaranteed immediate delivery, which