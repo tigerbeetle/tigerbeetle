65b1d091145dd905cdb057f775686bac726bab0c
King Butcher <king@coil.com>
2022-06-27 16:31:08 -0500

    clients/go: remove testify/all dependencies

---
+++ src/clients/go/assert/assert.go
@@ -0,0 +1,108 @@
+package assert
+
+import (
+	"testing"
+	"reflect"
+	"bytes"
+)
+
+func isEmpty(obj interface{}) bool {
+	if obj == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(obj)
+	switch value.Kind() {
+	case reflect.Chan, reflect.Map, reflect.Slice:
+		return value.Len() == 0
+	default:
+		return false
+	}
+}
+
+func Empty(t *testing.T, obj interface{}) {
+	if isEmpty(obj) {
+		t.Errorf("%v is not empty", obj)
+	}
+}
+
+func getLength(obj interface{}) (ok bool, length int) {
+	value := reflect.ValueOf(obj)
+	defer func() {
+		if e := recover(); e != nil {
+			ok = false
+		}
+	}()
+	return true, value.Len()
+}
+
+func Len(t *testing.T, obj interface{}, size int) {
+	ok, length := getLength(obj)
+	if !ok || length != size {
+		t.Errorf("%v doesn't have size %v", obj, size)
+	}
+}
+
+func isObjectEqual(a, b interface{}) bool {
+	if a == nil || b == nil {
+		return a == b
+	}
+
+	binaryA, ok := a.([]byte)
+	if !ok {
+		return reflect.DeepEqual(a, b)
+	}
+
+	binaryB, ok := b.([]byte)
+	if !ok {
+		return false
+	}	
+
+	if binaryA == nil || binaryB == nil {
+		return binaryA == nil && binaryB == nil
+	}
+
+	return bytes.Equal(binaryA, binaryB)
+}
+
+func isEqual(a, b interface{}) bool {
+	if isObjectEqual(a, b) {
+		return true
+	}
+
+	typeB := reflect.TypeOf(b)
+	if typeB == nil {
+		return false
+	}
+
+	valueA := reflect.ValueOf(a)
+	if valueA.IsValid() && valueA.Type().ConvertibleTo(typeB) {
+		return reflect.DeepEqual(valueA.Convert(typeB).Interface(), b)
+	}
+
+	return false
+}
+
+func Equal(t *testing.T, a, b interface{}) {
+	if !isEqual(a, b) {
+		t.Errorf("%v is not equal to %v", a, b)
+	}
+}
+
+func NotEqual(t *testing.T, a, b interface{}) {
+	if isEqual(a, b) {
+		t.Errorf("%v is equal to %v", a, b)
+	}
+}
+
+func isGreater(a, b interface{}) bool {
+	a64, okA := a.(uint64)
+	b64, okB := b.(uint64)
+	return okA && okB && (a64 > b64)
+}
+
+func Greater(t *testing.T, a, b interface{}) {
+	if !isGreater(a, b) {
+		t.Errorf("%v is not greater than %v", a, b)
+	}
+}
