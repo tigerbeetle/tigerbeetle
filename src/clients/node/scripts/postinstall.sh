607f1d1d29e64beba1eac70b51d2adaa80ec659c
Joran Dirk Greef <joran@coil.com>
2022-06-27 12:57:39 +0200

    clients/node: Remove Yarn
    The TigerBeetle Node client has no dependencies, apart from developer
    devepencies, so Yarn provides less value. We also hit a Yarn-specific
    bug [1] that was not backported, and later versions of Yarn have
    become more complex with the additional indirection of Corepack, which
    is still an experimental Node.js API.
    
    Also deduplicate the Zig library build step, so that there is only
    one canonical build command, not another also in postinstall.sh. This
    caused us some debugging trouble in the past, when a different command
    was being used to build the library, instead of that in package.json.
    
    [1] https://github.com/yarnpkg/berry/issues/2232#issuecomment-818514929

--- src/clients/node/scripts/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-./src/tigerbeetle/scripts/install_zig.sh
-./scripts/download_node_headers.sh
-mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
