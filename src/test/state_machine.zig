8def836aa04df5750a9adaaff294c24d3d3b41c8
sentientwaffle <sentientwaffle@gmail.com>
2022-08-15 11:44:49 -0700

    VOPR: StateChecker: validate in-flight & hashchain
    Instead of using a custom state machine to validate state, use the existing message hash chain. (This change is a prelude to testing the real StateMachine in the simulator).
    
    Previously, the VOPR used a custom `StateMachine` that chained `state = hash(state, hash(client_id, message_body))`. This wouldn't work after the log has wrapped and a replica crashes/recovers — the state would need to be recovered from replaying every message, instead of starting from the snapshot. (Or the `state` could need to be stored/restored by snapshots).
    
    - Remove `StateChecker`'s request queues:
      - The comment "We must therefore use our own queue rather than the clients' queues." is no longer applicable, because the state checker now uses only the `client` id to identify the request in the client queue (instead of a possibly-colliding hash of the message body).
    - validate `register` messages the same as any other message
    - remove the dependency on `Cluster`.
    - The hashes printed by the state checker during VOPR runs are now the actual message checksums, instead of being random.
      - (Though the number of transitions printed doesn't match the number of requests sent, since it includes `register`s).
    - Rename `transitions` → `committed_messages` to clarify that it doesn't recount transitions after crash/recover.
      - (The impetus for renaming was actually to add and distinguish `committed_requests`).
      - The distinction between the two counters is that:
        - `committed_messages` cares about all commits, including `register` messages.
        - `committed_requests` ignores `register`s, since the simulator wants a target number of requests to arrive.
    
    Extra:
    - Stricter assertions in Replica and StateMachine.
    - In `vsr.Header.calculate_checksum{,_body}`, call `vsr.checksum()`.

--- src/test/state_machine.zig
+++ src/test/state_machine.zig
@@ -15,7 +15,7 @@ pub fn StateMachineType(comptime Storage: type) type {
             root,
             register,
 
-            hash,
+            random,
         };
 
         /// Minimum/mean number of ticks to perform the specified operation.
@@ -28,7 +28,6 @@ pub fn StateMachineType(comptime Storage: type) type {
         };
 
         options: Options,
-        state: u128,
         prng: std.rand.DefaultPrng,
         prepare_timestamp: u64 = 0,
         commit_timestamp: u64 = 0,
@@ -38,7 +37,6 @@ pub fn StateMachineType(comptime Storage: type) type {
 
         pub fn init(_: std.mem.Allocator, _: *Grid, options: Options) !StateMachine {
             return StateMachine{
-                .state = hash(0, std.mem.asBytes(&options.seed)),
                 .options = options,
                 .prng = std.rand.DefaultPrng.init(options.seed),
             };
@@ -98,32 +96,16 @@ pub fn StateMachineType(comptime Storage: type) type {
             input: []const u8,
             output: []u8,
         ) usize {
-            _ = op;
+            _ = state_machine;
+            _ = client;
+            _ = input;
+            _ = output;
+            assert(op != 0);
 
             switch (operation) {
                 .reserved, .root => unreachable,
                 .register => return 0,
-
-                // TODO: instead of always using the first 32 bytes of the output
-                // buffer, get tricky and use a random but deterministic slice
-                // of it, filling the rest with 0s.
-                .hash => {
-                    // Fold the input into our current state, creating a hash chain.
-                    // Hash the input with the client ID since small inputs may collide across clients.
-                    const client_input = hash(client, input);
-                    const new_state = hash(state_machine.state, std.mem.asBytes(&client_input));
-
-                    log.debug("state={x} input={x} input.len={} new state={x}", .{
-                        state_machine.state,
-                        client_input,
-                        input.len,
-                        new_state,
-                    });
-
-                    state_machine.state = new_state;
-                    std.mem.copy(u8, output, std.mem.asBytes(&state_machine.state));
-                    return @sizeOf(@TypeOf(state_machine.state));
-                },
+                .random => return 0,
             }
         }
 
@@ -147,14 +129,6 @@ pub fn StateMachineType(comptime Storage: type) type {
             assert(state_machine.callback_ticks == 0);
             state_machine.callback = callback;
             state_machine.callback_ticks = state_machine.latency(state_machine.options.checkpoint_mean);
-        }
-
-        pub fn hash(state: u128, input: []const u8) u128 {
-            var key: [32]u8 = [_]u8{0} ** 32;
-            std.mem.copy(u8, key[0..16], std.mem.asBytes(&state));
-            var target: [32]u8 = undefined;
-            std.crypto.hash.Blake3.hash(input, &target, .{ .key = key });
-            return @bitCast(u128, target[0..16].*);
         }
 
         fn latency(state_machine: *StateMachine, mean: u64) u64 {
