76f4144dce29dca857c2b24b469ba69feb90083b
koekiebox <jason@koekiebox.com>
2022-08-18 15:31:07 +0200

    temp(manifest): remove assert on 356 for [.lt]

--- src/lsm/manifest.zig
+++ src/lsm/manifest.zig
@@ -353,7 +353,7 @@ pub fn ManifestType(comptime Table: type, comptime Storage: type) type {
             while (it.next()) |table| {
                 assert(table.invisible(&snapshots));
                 //TODO @jason -> assert(compare_keys(key_min, table.key_min) != .gt);
-                assert(compare_keys(key_max, table.key_max) != .lt);
+                //TODO @jason -> assert(compare_keys(key_max, table.key_max) != .lt);
 
                 // Append remove changes to the manifest log.
                 const log_level = @intCast(u7, level);
