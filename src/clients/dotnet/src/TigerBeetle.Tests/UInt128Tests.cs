2e74334dc8b2948e6681cd61b7d410da34205bda
batiati <rbatiati@gmail.com>
2022-08-27 19:51:51 -0300

    clients/dotnet: UInt128 review

--- src/clients/dotnet/src/TigerBeetle.Tests/UInt128Tests.cs
+++ src/clients/dotnet/src/TigerBeetle.Tests/UInt128Tests.cs
@@ -7,7 +7,7 @@ namespace TigerBeetle.Tests
     public class UInt128Tests
     {
         [TestMethod]
-        public void Convertion()
+        public void GuidConvertion()
         {
             Guid guid = Guid.NewGuid();
             UInt128 value = guid;
@@ -15,5 +15,25 @@ namespace TigerBeetle.Tests
             Assert.AreEqual(value, (UInt128)guid);
             Assert.AreEqual(guid, (Guid)value);
         }
+
+        [TestMethod]
+        public void I64Constructor()
+        {
+            UInt128 value = new UInt128(-1L, -2L);
+            var (i64a, i64b) = value.ToInt64();
+
+            Assert.AreEqual(i64a, -1L);
+            Assert.AreEqual(i64b, -2L);
+        } 
+
+        [TestMethod]
+        public void U64Constructors()
+        {
+            UInt128 value = new UInt128(1LU, 2LU);
+            var (u64a, u64b) = value.ToUInt64();
+
+            Assert.AreEqual(u64a, 1LU);
+            Assert.AreEqual(u64b, 2LU);
+        }                
     }
 }
