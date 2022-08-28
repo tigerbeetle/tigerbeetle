a253595da613fa091fb5cbcc25b8f4f26a241b9b
batiati <rbatiati@gmail.com>
2022-08-28 06:26:02 -0300

    clients/dotnet: Build scripts review
    - Rename "dynamic library" to "shared library"
    - Use "addSharedLibrary" on build.zig

--- src/clients/dotnet/build.zig
+++ src/clients/dotnet/build.zig
@@ -5,7 +5,7 @@ const CrossTarget = std.zig.CrossTarget;
 pub fn build(b: *std.build.Builder) void {
 
     const mode = b.standardReleaseOptions();
-    const tb_client_dyn = b.step("tb_client_dyn", "Build tb_client dynamic libs");
+    const tb_client = b.step("tb_client", "Build tb_client shared libs");
 
     // Zig cross-target x folder names
     const platforms = .{
@@ -17,19 +17,18 @@ pub fn build(b: *std.build.Builder) void {
     inline for (platforms) |platform| {
         const cross_target = CrossTarget.parse(.{ .arch_os_abi = platform[0], .cpu_features = "baseline" }) catch unreachable;
 
-        const dynamic_lib = b.addStaticLibrary("tb_client", "tigerbeetle/src/c/tb_client.zig");
-        dynamic_lib.setMainPkgPath("tigerbeetle/src");
-        dynamic_lib.setTarget(cross_target);
-        dynamic_lib.setBuildMode(mode);
-        dynamic_lib.setOutputDir(b.pathJoin(&.{ "src/TigerBeetle/native/", platform[1] }));
-        dynamic_lib.bundle_compiler_rt = true;
-        dynamic_lib.linkage = .dynamic;            
+        const lib = b.addSharedLibrary("tb_client", "tigerbeetle/src/c/tb_client.zig", .unversioned);
+        lib.setMainPkgPath("tigerbeetle/src");
+        lib.setOutputDir(b.pathJoin(&.{ "src/TigerBeetle/native/", platform[1] }));
+        lib.setTarget(cross_target);
+        lib.setBuildMode(mode);
+        lib.bundle_compiler_rt = true;           
         
         const os_tag = cross_target.os_tag orelse builtin.target.os.tag;
         if (os_tag != .windows) {
-            dynamic_lib.linkLibC();
+            lib.linkLibC();
         }           
 
-        tb_client_dyn.dependOn(&dynamic_lib.step);
+        tb_client.dependOn(&lib.step);
     }
 }
