8d9f6932e251329088fe30a57612ab246e5e933b
batiati <rbatiati@gmail.com>
2022-09-12 21:10:32 -0300

    clients/java: AssertionError now inherits from java.lang.AssertionError
    Rationale:
    - We can filter logs and track this kind of error by our classname com.tigerbeetle.AssertionError
    - Log frameworks and analysis tools can handle this error like any other generic java.lang.AssertionError

--- src/clients/java/src/java/src/com/tigerbeetle/AssertionError.java
+++ src/clients/java/src/java/src/com/tigerbeetle/AssertionError.java
@@ -1,11 +1,6 @@
 package com.tigerbeetle;
 
-/**
- * {@code AssertionError} is a <em>unchecked exception</em>
- * that is can be thrown when any unexpected value is received by TigerBeetle.
- * This exception should be logged with full stack trace.
- */
-public final class AssertionError extends Error {
+public final class AssertionError extends java.lang.AssertionError {
     AssertionError(String format, Object... args) {
         super(String.format(format, args));
     }
