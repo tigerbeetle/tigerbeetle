02b47456615b7d7aae5c6327e6c08c4c6db2340a
batiati <rbatiati@gmail.com>
2022-09-14 19:59:34 -0300

    clients/java: Make constants from TB's protocol more explicit

--- src/clients/java/src/tigerbeetle-java/src/main/java/Benchmark.java
+++ src/clients/java/src/tigerbeetle-java/src/main/java/Benchmark.java
@@ -25,13 +25,18 @@ public class Benchmark {
             if (results.length > 0)
                 throw new Exception("Unexpected createAccount results");
 
-            final int max_batches = 3;
-            final int max_transfers_per_batch = 8191; // config.message_size_max -
-                                                      // @sizeOf(vsr.Header)
-            var batches = new TransfersBatch[max_batches];
-            for (int i = 0; i < max_batches; i++) {
-                var batch = new TransfersBatch(max_transfers_per_batch);
-                for (int j = 0; j < max_transfers_per_batch; j++) {
+            final int HEADER_SIZE = 128; // @sizeOf(vsr.Header)
+            final int TRANSFER_SIZE = 128; // @sizeOf(Transfer)
+            final int MESSAGE_SIZE_MAX = 1024 * 1024; // config.message_size_max
+
+            final int BATCHES_COUNT = 3;
+            final int TRANSFERS_PER_BATCH = (MESSAGE_SIZE_MAX - HEADER_SIZE) / TRANSFER_SIZE;
+            final int MAX_TRANSFERS = BATCHES_COUNT * TRANSFERS_PER_BATCH;
+
+            var batches = new TransfersBatch[BATCHES_COUNT];
+            for (int i = 0; i < BATCHES_COUNT; i++) {
+                var batch = new TransfersBatch(TRANSFERS_PER_BATCH);
+                for (int j = 0; j < TRANSFERS_PER_BATCH; j++) {
                     var transfer = new Transfer();
                     transfer.setId(new UUID(i + 1, j + 1));
                     transfer.setCreditAccountId(account1.getId());
@@ -57,7 +62,7 @@ public class Benchmark {
                 var request = client.createTransfersAsync(batch);
 
                 // Register something on the application's side while tigerbeetle is processing
-                // UPDATE FROM MyCustomer ...
+                // UPDATE MyCustomer ...
 
                 var errors = request.get();
                 if (errors.length > 0)
@@ -71,13 +76,12 @@ public class Benchmark {
 
             System.out.println("============================================");
 
-            var result = (long) (max_batches * max_transfers_per_batch * 1000) / totalTime;
+            var result = (long) (MAX_TRANSFERS * 1000) / totalTime;
 
             System.out.printf("%d transfers per second\n", result);
             System.out.printf("create_transfers max p100 latency per %d transfers = %dms\n",
-                    max_transfers_per_batch, maxTransferLatency);
-            System.out.printf("total %d transfers in %dms\n", max_batches * max_transfers_per_batch,
-                    totalTime);
+                    TRANSFERS_PER_BATCH, maxTransferLatency);
+            System.out.printf("total %d transfers in %dms\n", MAX_TRANSFERS, totalTime);
 
         } catch (Exception e) {
             System.out.println(e);
