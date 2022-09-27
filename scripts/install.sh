d77e48dd7bc034e254a3622cbbddcebd8bca7736
Phil Eaton <phil@eatonphil.com>
2022-09-27 15:54:44 -0700

    Handle debug flag

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+debug="$DEBUG"
+if [[ "$1" == "--debug" ]]; then
+    debug="true"
+fi
+
 set -eEuo pipefail
 
 scripts/install_zig.sh
 echo "Building TigerBeetle..."
-if [[ "$DEBUG" == "true" ]]; then
+if [[ "$debug" == "true" ]]; then
     zig/zig build -Dcpu=baseline -Drelease-safe
 else
     zig/zig build -Dcpu=baseline
