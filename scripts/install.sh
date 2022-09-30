6bbb27d26434afc1b38337f91bd06a02a1012c6c
Phil Eaton <phil@eatonphil.com>
2022-09-30 09:53:41 -0400

    Also publish a :debug docker image (#172)
    * Also build/push debug docker image
    
    * Handle debug flag
    
    * Try a different tags
    
    * Test with push on pr
    
    * arm64 nor arm6
    
    * Always push
    
    * Maybe can stop using the actions library
    
    * Checkout source
    
    * Always login
    
    * Try again
    
    * Try setting build flag again
    
    * Fix debug conditional
    
    * Clarify in echo
    
    * Add docs on the debug tag/debug flag
    
    * stack trace
    
    * Only actual push if not a pull request

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+debug="$DEBUG"
+if [[ "$1" == "--debug" ]]; then
+    debug="true"
+fi
+
 set -eEuo pipefail
 
 scripts/install_zig.sh
-echo "Building TigerBeetle..."
-zig/zig build -Dcpu=baseline -Drelease-safe
+if [[ "$debug" == "true" ]]; then
+    echo "Building Tigerbeetle debug..."
+    zig/zig build -Dcpu=baseline
+else
+    echo "Building TigerBeetle..."
+    zig/zig build -Dcpu=baseline -Drelease-safe
+fi
 mv zig-out/bin/tigerbeetle .
