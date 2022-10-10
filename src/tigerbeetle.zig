cdc59e12d3b3e3f752d7506acaa287ae8e87329e
djg <sentientwaffle@gmail.com>
2022-10-10 10:32:26 -0700

    StateMachine: Forbid new accounts with amount ≠ 0 (#192)
    StateMachine: Forbid new accounts with amount ≠ 0
    
    To create an account, require that `{debits,credits}_{pending,posted}` are all `0`.
    
    Motivation: Creating accounts with nonzero amounts allowed clients to violate double-entry bookkeeping's "∑ debits = ∑ credits" invariant.
    
    The only possible surprise here is that if you create an account with zero balances, and the account already exists with nonzero balances, you get `exists` back. This property is actually very useful! Previously a client implementation would have to handle `ok`, `exists`, and `exists_with_different_*` identically anyway to correctly recover from application-side crashes. But now it is just `ok` and `exists`, which is more intuitive.
    
    Errors removed:
    - `exists_with_different_debits_pending`
    - `exists_with_different_debits_posted`
    - `exists_with_different_credits_pending`
    - `exists_with_different_credits_posted`
    
    Errors added:
    - `debits_pending_must_be_zero`
    - `debits_posted_must_be_zero`
    - `credits_pending_must_be_zero`
    - `credits_posted_must_be_zero`
    
    This commit will require a rebuild of all client implementations, due to the breaking API change.

--- src/tigerbeetle.zig
+++ src/tigerbeetle.zig
@@ -103,6 +103,10 @@ pub const CreateAccountResult = enum(u32) {
     id_must_not_be_int_max,
     ledger_must_not_be_zero,
     code_must_not_be_zero,
+    debits_pending_must_be_zero,
+    debits_posted_must_be_zero,
+    credits_pending_must_be_zero,
+    credits_posted_must_be_zero,
 
     mutually_exclusive_flags,
 
@@ -116,10 +120,6 @@ pub const CreateAccountResult = enum(u32) {
     exists_with_different_user_data,
     exists_with_different_ledger,
     exists_with_different_code,
-    exists_with_different_debits_pending,
-    exists_with_different_debits_posted,
-    exists_with_different_credits_pending,
-    exists_with_different_credits_posted,
     exists,
 };
 
