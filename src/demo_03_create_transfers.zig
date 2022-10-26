b331b8603e5ad6cd9d48c2b7e05354fba726d49c
sentientwaffle <sentientwaffle@gmail.com>
2022-10-26 08:10:27 -0700

    Test: Fix CI; update demos

--- src/demo_03_create_transfers.zig
+++ src/demo_03_create_transfers.zig
@@ -7,6 +7,19 @@ pub fn main() !void {
     const transfers = [_]Transfer{
         Transfer{
             .id = 1,
+            .debit_account_id = 2,
+            .credit_account_id = 1,
+            .user_data = 0,
+            .reserved = 0,
+            .pending_id = 0,
+            .timeout = 0,
+            .ledger = 710, // Let's use the ISO-4217 Code Number for ZAR
+            .code = 1,
+            .flags = .{},
+            .amount = 10000, // Let's start with some liquidity in account 1.
+        },
+        Transfer{
+            .id = 2,
             .debit_account_id = 1,
             .credit_account_id = 2,
             .user_data = 0,
