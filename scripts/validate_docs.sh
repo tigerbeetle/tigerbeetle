1bece72513e6684bec3273b58bda4eaffb9b52a8
Phil Eaton <phil@eatonphil.com>
2022-11-02 18:00:28 -0400

    Add docs build check (#235)
    * Add docs build check
    
    * Typo
    
    * Test bad docs
    
    * Try getting branch a different way
    
    * Different env variable
    
    * Debug env
    
    * Correct env name again
    
    * Undo bad docs change

---
+++ scripts/validate_docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script builds the docs website for the currently checked out
+# branch.
+
+git clone https://github.com/tigerbeetledb/docs docs_website
+# Try to grab branch from Github Actions CI.
+# See also: https://docs.github.com/en/actions/learn-github-actions/environment-variables.
+BRANCH="$GITHUB_HEAD_REF"
+if [[ -z "$BRANCH" ]]; then
+    # Otherwise fall back to git rev-parse
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
+( cd docs_website && npm install && ./scripts/build.sh "$BRANCH" )
+rm -rf docs_website
