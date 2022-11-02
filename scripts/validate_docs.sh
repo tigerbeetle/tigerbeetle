d9999c5d2c2e14911700806bdd38d5e9b7f1c92e
Phil Eaton <phil@eatonphil.com>
2022-11-02 19:00:41 +0000

    Try getting branch a different way

--- scripts/validate_docs.sh
+++ scripts/validate_docs.sh
@@ -6,6 +6,12 @@ set -e
 # branch.
 
 git clone https://github.com/tigerbeetledb/docs docs_website
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+# Try to grab branch from Github Actions CI.
+# See also: https://docs.github.com/en/actions/learn-github-actions/environment-variables.
+BRANCH="$GITHUB_BASE_REF"
+if [[ -z "$BRANCH" ]]; then
+    # Otherwise fall back to git rev-parse
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
 ( cd docs_website && npm install && ./scripts/build.sh "$BRANCH" )
 rm -rf docs_website
