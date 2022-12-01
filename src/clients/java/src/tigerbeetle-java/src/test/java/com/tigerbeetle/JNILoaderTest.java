97bd3b2fc289710ebe3fb1c3d329b06d14bf558f
Rafael Batiati <rbatiati@gmail.com>
2022-12-01 11:53:19 -0300

    Java client: Add support for musl-based Linux distros and fix Mac OS detection (#268)
    * Java client: Add support for musl-based distros
    
    - Cross-compiles for linux using glibc and musl: The JVM can only load the JNI lib that matches its libc, so we must distribute both versions inside the .jar package.
    
    - Simplifies the cross-compilation naming process, changing the lib subdirectory's names to match the zig triple `arch-os-abi`.
    
    - Detects musl-based distros during runtime by inspecting the process's loaded libraries.
    
    - Minor adjustments for impacted test cases.
    
    - Also, closes https://github.com/tigerbeetledb/tigerbeetle-java/pull/21
    
    * Add build options workaround
    
    - Added a workaround for the required build options introduced by the tracing API #291
    This should be removed once the client is built using the root build.zig file
    
    * Zig fmt

--- src/clients/java/src/tigerbeetle-java/src/test/java/com/tigerbeetle/JNILoaderTest.java
+++ src/clients/java/src/tigerbeetle-java/src/test/java/com/tigerbeetle/JNILoaderTest.java
@@ -8,25 +8,31 @@ public class JNILoaderTest {
     @Test
     public void testResourcePath() {
 
-        assertEquals("/lib/linux-x86_64/libtb_jniclient.so",
-                JNILoader.getResourcesPath(JNILoader.OS.linux, JNILoader.Arch.x86_64));
+        assertEquals("/lib/x86_64-linux-gnu/libtb_jniclient.so", JNILoader
+                .getResourcesPath(JNILoader.Arch.x86_64, JNILoader.OS.linux, JNILoader.Abi.gnu));
 
-        assertEquals("/lib/linux-aarch64/libtb_jniclient.so",
-                JNILoader.getResourcesPath(JNILoader.OS.linux, JNILoader.Arch.aarch64));
+        assertEquals("/lib/aarch64-linux-gnu/libtb_jniclient.so", JNILoader
+                .getResourcesPath(JNILoader.Arch.aarch64, JNILoader.OS.linux, JNILoader.Abi.gnu));
 
-        assertEquals("/lib/macos-x86_64/libtb_jniclient.dylib",
-                JNILoader.getResourcesPath(JNILoader.OS.macos, JNILoader.Arch.x86_64));
+        assertEquals("/lib/x86_64-linux-musl/libtb_jniclient.so", JNILoader
+                .getResourcesPath(JNILoader.Arch.x86_64, JNILoader.OS.linux, JNILoader.Abi.musl));
 
-        assertEquals("/lib/macos-aarch64/libtb_jniclient.dylib",
-                JNILoader.getResourcesPath(JNILoader.OS.macos, JNILoader.Arch.aarch64));
+        assertEquals("/lib/aarch64-linux-musl/libtb_jniclient.so", JNILoader
+                .getResourcesPath(JNILoader.Arch.aarch64, JNILoader.OS.linux, JNILoader.Abi.musl));
 
-        assertEquals("/lib/win-x86_64/tb_jniclient.dll",
-                JNILoader.getResourcesPath(JNILoader.OS.win, JNILoader.Arch.x86_64));
+        assertEquals("/lib/x86_64-macos/libtb_jniclient.dylib", JNILoader
+                .getResourcesPath(JNILoader.Arch.x86_64, JNILoader.OS.macos, JNILoader.Abi.none));
+
+        assertEquals("/lib/aarch64-macos/libtb_jniclient.dylib", JNILoader
+                .getResourcesPath(JNILoader.Arch.aarch64, JNILoader.OS.macos, JNILoader.Abi.none));
+
+        assertEquals("/lib/x86_64-win/tb_jniclient.dll", JNILoader
+                .getResourcesPath(JNILoader.Arch.x86_64, JNILoader.OS.win, JNILoader.Abi.none));
     }
 
     @Test(expected = AssertionError.class)
     public void testUnsupportedPlatform() {
-        JNILoader.getResourcesPath(JNILoader.OS.win, JNILoader.Arch.aarch64);
+        JNILoader.getResourcesPath(JNILoader.Arch.aarch64, JNILoader.OS.win, JNILoader.Abi.none);
     }
 
 }
--- src/clients/java/src/zig/build.zig
+++ src/clients/java/src/zig/build.zig
@@ -1,26 +1,45 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const CrossTarget = std.zig.CrossTarget;
+const Pkg = std.build.Pkg;
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
-    // Zig cross-target x java os-arch names
+    // Zig cross-targets
     const platforms = .{
-        .{ "x86_64-linux-gnu", "linux-x86_64" },
-        .{ "x86_64-macos", "macos-x86_64" },
-        .{ "aarch64-linux-gnu", "linux-aarch_64" },
-        .{ "aarch64-macos", "macos-aarch64" },
-        .{ "x86_64-windows", "win-x86_64" },
+        "x86_64-linux-gnu",
+        "x86_64-linux-musl",
+        "x86_64-macos",
+        "aarch64-linux-gnu",
+        "aarch64-linux-musl",
+        "aarch64-macos",
+        "x86_64-windows",
     };
 
     inline for (platforms) |platform| {
-        const cross_target = CrossTarget.parse(.{ .arch_os_abi = platform[0], .cpu_features = "baseline" }) catch unreachable;
+        const cross_target = CrossTarget.parse(.{ .arch_os_abi = platform, .cpu_features = "baseline" }) catch unreachable;
 
         const lib = b.addSharedLibrary("tb_jniclient", "src/client.zig", .unversioned);
+
+        // TODO(batiati)
+        // This is a hack used to work around the absence of tigerbeetle_build_options.
+        // This should be removed once the client is built using the root `build.zig`.
+        {
+            const TracerBackend = enum { none, perfetto, tracy };
+            const dummy_options = b.addOptions();
+            dummy_options.addOption(TracerBackend, "tracer_backend", .none);
+            var tigerbeetle_build_options = dummy_options.getPackage("tigerbeetle_build_options");
+
+            lib.addPackage(Pkg{
+                .name = "tigerbeetle",
+                .path = .{ .path = "tb_client.zig" },
+                .dependencies = &[_]Pkg{tigerbeetle_build_options},
+            });
+        }
+
         lib.addPackagePath("jui", "lib/jui/src/jui.zig");
-        lib.addPackagePath("tigerbeetle", "tb_client.zig");
-        lib.setOutputDir("../tigerbeetle-java/src/main/resources/lib/" ++ platform[1]);
+        lib.setOutputDir("../tigerbeetle-java/src/main/resources/lib/" ++ platform);
         lib.setTarget(cross_target);
         lib.setBuildMode(mode);
 
@@ -38,8 +57,8 @@ pub fn build(b: *std.build.Builder) void {
         // - From Windows it fails on libc "invalid object"
         // - From MacOS, similar to https://github.com/ziglang/zig/issues/9711
         // Workarround: Just setting a different cache folder for each platform and an isolated global cache.
-        b.cache_root = "zig-cache/" ++ platform[1];
-        b.global_cache_root = "zig-cache/global-" ++ platform[1];
+        b.cache_root = "zig-cache/" ++ platform;
+        b.global_cache_root = "zig-cache/global-" ++ platform;
 
         lib.install();
     }
