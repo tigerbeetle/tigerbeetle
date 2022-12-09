tigerbeetle/tigerbeetle

commit 4882ea86f750c935bc23dbc9443b023d69d11fa1

Config: s/size_max/storage_size_max
Also s/size_limit/storage_size_limit.

"size" is a too general on its own. This matches the typical schema (e.g. `{message,journal,…}_size_max`).

src/lsm/forest_fuzz.zig
@@ -147,7 +147,7 @@ const Environment = struct {
         env.superblock.format(superblock_format_callback, &env.superblock_context, .{
             .cluster = cluster,
             .replica = replica,
-            .size_max = constants.size_max,
+            .storage_size_max = constants.storage_size_max,
         });
         env.tick_until_state_change(.init, .formatted);
     }
@@ -287,7 +287,7 @@ const Environment = struct {
 
 pub fn run_fuzz_ops(storage_options: Storage.Options, fuzz_ops: []const FuzzOp) !void {
     // Init mocked storage.
-    var storage = try Storage.init(allocator, constants.size_max, storage_options);
+    var storage = try Storage.init(allocator, constants.storage_size_max, storage_options);
     defer storage.deinit(allocator);
 
     try Environment.format(&storage);

src/vsr/replica.zig
@@ -291,7 +291,7 @@ pub fn ReplicaType(
 
         const OpenOptions = struct {
             replica_count: u8,
-            size_limit: u64,
+            storage_size_limit: u64,
             storage: *Storage,
             message_pool: *MessagePool,
             time: Time,
@@ -301,8 +301,8 @@ pub fn ReplicaType(
 
         /// Initializes and opens the provided replica using the options.
         pub fn open(self: *Self, parent_allocator: std.mem.Allocator, options: OpenOptions) !void {
-            assert(options.size_limit <= constants.size_max);
-            assert(options.size_limit % constants.sector_size == 0);
+            assert(options.storage_size_limit <= constants.storage_size_max);
+            assert(options.storage_size_limit % constants.sector_size == 0);
 
             self.static_allocator = StaticAllocator.init(parent_allocator);
             const allocator = self.static_allocator.allocator();
@@ -312,7 +312,7 @@ pub fn ReplicaType(
                 .{
                     .storage = options.storage,
                     .message_pool = options.message_pool,
-                    .size_limit = options.size_limit,
+                    .storage_size_limit = options.storage_size_limit,
                 },
             );
 

src/cli.zig
@@ -84,7 +84,7 @@ pub const Command = union(enum) {
         cache_accounts: u32,
         cache_transfers: u32,
         cache_transfers_posted: u32,
-        size_limit: u64,
+        storage_size_limit: u64,
         path: [:0]const u8,
     };
 
@@ -121,7 +121,7 @@ pub fn parse_args(allocator: std.mem.Allocator) !Command {
     var cache_accounts: ?[]const u8 = null;
     var cache_transfers: ?[]const u8 = null;
     var cache_transfers_posted: ?[]const u8 = null;
-    var size_limit: ?[]const u8 = null;
+    var storage_size_limit: ?[]const u8 = null;
     var verbose: ?bool = null;
 
     var args = try std.process.argsWithAllocator(allocator);
@@ -170,7 +170,7 @@ pub fn parse_args(allocator: std.mem.Allocator) !Command {
             cache_transfers_posted = parse_flag("--cache-transfers-posted", arg);
         } else if (mem.startsWith(u8, arg, "--limit-storage")) {
             if (command != .start) fatal("--limit-storage: supported only by 'start' command", .{});
-            size_limit = parse_flag("--limit-storage", arg);
+            storage_size_limit = parse_flag("--limit-storage", arg);
         } else if (mem.eql(u8, arg, "--verbose")) {
             if (command != .version) fatal("--verbose: supported only by 'version' command", .{});
             verbose = true;
@@ -226,7 +226,7 @@ pub fn parse_args(allocator: std.mem.Allocator) !Command {
                         cache_transfers_posted,
                         constants.cache_transfers_posted_max,
                     ),
-                    .size_limit = parse_storage_size(size_limit, constants.size_max),
+                    .storage_size_limit = parse_storage_size(storage_size_limit),
                     .path = path orelse fatal("required: <path>", .{}),
                 },
             };
@@ -281,11 +281,12 @@ fn parse_addresses(allocator: std.mem.Allocator, raw_addresses: []const u8) []ne
     };
 }
 
-fn parse_storage_size(size_string: ?[]const u8, size_max: u64) u64 {
+fn parse_storage_size(size_string: ?[]const u8) u64 {
     const size_min = data_file_size_min;
+    const size_max = constants.storage_size_max;
     const size = if (size_string) |s| parse_size(s) else size_max;
-    if (size > size_max) fatal("size value {} exceeds maximum: {}", .{ size, size_max });
-    if (size < size_min) fatal("size value {} is below minimum: {}", .{ size, size_min });
+    if (size > size_max) fatal("storage size {} exceeds maximum: {}", .{ size, size_max });
+    if (size < size_min) fatal("storage size {} is below minimum: {}", .{ size, size_min });
     if (size % constants.sector_size != 0) {
         fatal("size value {} must be a multiple of sector size ({})", .{
             size,

src/config.zig
@@ -72,7 +72,7 @@ const ConfigCluster = struct {
     journal_slot_count: usize = 1024,
     message_size_max: usize = 1 * 1024 * 1024,
     superblock_copies: comptime_int = 4,
-    size_max: u64 = 16 * 1024 * 1024 * 1024 * 1024,
+    storage_size_max: u64 = 16 * 1024 * 1024 * 1024 * 1024,
     block_size: comptime_int = 64 * 1024,
     lsm_levels: u7 = 7,
     lsm_growth_factor: u32 = 8,
@@ -169,7 +169,7 @@ pub const configs = struct {
             .clients_max = 4,
             .journal_slot_count = Config.Cluster.journal_slot_count_min,
             .message_size_max = Config.Cluster.message_size_max_min(2),
-            .size_max = 1024 * 1024 * 1024,
+            .storage_size_max = 1024 * 1024 * 1024,
 
             .block_size = sector_size,
             .lsm_growth_factor = 4,

src/constants.zig
@@ -295,7 +295,7 @@ comptime {
 /// * replicated storage overhead, since all data files are mirrored,
 /// * the size of the superblock storage zone, and
 /// * the static memory allocation required for tracking LSM forest metadata in memory.
-pub const size_max = config.cluster.size_max;
+pub const storage_size_max = config.cluster.storage_size_max;
 
 /// The unit of read/write access to LSM manifest and LSM table blocks in the block storage zone.
 ///

src/lsm/manifest_log_fuzz.zig
@@ -64,10 +64,10 @@ fn run_fuzz(
         .write_latency_mean = 1 + random.uintLessThan(u64, 40),
     };
 
-    var storage = try Storage.init(allocator, constants.size_max, storage_options);
+    var storage = try Storage.init(allocator, constants.storage_size_max, storage_options);
     defer storage.deinit(allocator);
 
-    var storage_verify = try Storage.init(allocator, constants.size_max, storage_options);
+    var storage_verify = try Storage.init(allocator, constants.storage_size_max, storage_options);
     defer storage_verify.deinit(allocator);
 
     // The MessagePool is shared by both superblocks because they will not use it.
@@ -76,15 +76,15 @@ fn run_fuzz(
 
     var superblock = try SuperBlock.init(allocator, .{
         .storage = &storage,
+        .storage_size_limit = constants.storage_size_max,
         .message_pool = &message_pool,
-        .size_limit = constants.size_max,
     });
     defer superblock.deinit(allocator);
 
     var superblock_verify = try SuperBlock.init(allocator, .{
         .storage = &storage_verify,
+        .storage_size_limit = constants.storage_size_max,
         .message_pool = &message_pool,
-        .size_limit = constants.size_max,
     });
     defer superblock_verify.deinit(allocator);
 
@@ -330,7 +330,6 @@ const Environment = struct {
         env.manifest_log.superblock.format(format_superblock_callback, &env.superblock_context, .{
             .cluster = 0,
             .replica = 0,
-            .size_max = constants.size_max,
         });
     }
 
@@ -461,8 +460,8 @@ const Environment = struct {
                 env.allocator,
                 .{
                     .storage = test_storage,
+                    .storage_size_limit = constants.storage_size_max,
                     .message_pool = env.manifest_log.superblock.client_table.message_pool,
-                    .size_limit = constants.size_max,
                 },
             );
 

src/lsm/test.zig
@@ -27,7 +27,7 @@ const SuperBlock = vsr.SuperBlockType(Storage);
 const Environment = struct {
     const cluster = 32;
     const replica = 4;
-    const size_max = vsr.Zone.superblock.size().? +
+    const storage_size_max = vsr.Zone.superblock.size().? +
         vsr.Zone.wal_headers.size().? +
         vsr.Zone.wal_prepares.size().? +
         (512 + 64) * 1024 * 1024;
@@ -73,7 +73,7 @@ const Environment = struct {
         env.dir_fd = try IO.open_dir(dir_path);
         errdefer std.os.close(env.dir_fd);
 
-        env.fd = try IO.open_file(env.dir_fd, "test_forest", size_max, must_create);
+        env.fd = try IO.open_file(env.dir_fd, "test_forest", storage_size_max, must_create);
         errdefer std.os.close(env.fd);
 
         env.io = try IO.init(128, 0);
@@ -87,8 +87,8 @@ const Environment = struct {
 
         env.superblock = try SuperBlock.init(allocator, .{
             .storage = &env.storage,
+            .storage_size_limit = constants.storage_size_max,
             .message_pool = &env.message_pool,
-            .size_limit = constants.size_max,
         });
         env.superblock_context = undefined;
         errdefer env.superblock.deinit(allocator);
@@ -136,7 +136,7 @@ const Environment = struct {
         env.superblock.format(superblock_format_callback, &env.superblock_context, .{
             .cluster = cluster,
             .replica = replica,
-            .size_max = size_max,
+            .storage_size_max = storage_size_max,
         });
 
         while (true) {

src/lsm/tree_fuzz.zig
@@ -140,8 +140,8 @@ const Environment = struct {
 
         env.superblock = try SuperBlock.init(allocator, .{
             .storage = env.storage,
+            .storage_size_limit = constants.storage_size_max,
             .message_pool = &env.message_pool,
-            .size_limit = constants.size_max,
         });
         errdefer env.superblock.deinit(allocator);
 
@@ -200,7 +200,6 @@ const Environment = struct {
         env.superblock.format(superblock_format_callback, &env.superblock_context, .{
             .cluster = cluster,
             .replica = replica,
-            .size_max = constants.size_max,
         });
         env.tick_until_state_change(.init, .formatted);
     }
@@ -345,7 +344,7 @@ const Environment = struct {
 
 pub fn run_fuzz_ops(storage_options: Storage.Options, fuzz_ops: []const FuzzOp) !void {
     // Init mocked storage.
-    var storage = try Storage.init(allocator, constants.size_max, storage_options);
+    var storage = try Storage.init(allocator, constants.storage_size_max, storage_options);
     defer storage.deinit(allocator);
 
     try Environment.format(&storage);

commit 73574005969e7ac894abaf278f6714127a01502a

Test: Fix forest fuzzer build error
Broken by https://github.com/tigerbeetledb/tigerbeetle/pull/339

src/lsm/forest_fuzz.zig
@@ -93,7 +93,11 @@ const Environment = struct {
         env.message_pool = try MessagePool.init(allocator, .replica);
         errdefer env.message_pool.deinit(allocator);
 
-        env.superblock = try SuperBlock.init(allocator, env.storage, &env.message_pool);
+        env.superblock = try SuperBlock.init(allocator, .{
+            .storage = env.storage,
+            .storage_size_limit = constants.storage_size_max,
+            .message_pool = &env.message_pool,
+        });
         errdefer env.superblock.deinit(allocator);
 
         env.grid = try Grid.init(allocator, &env.superblock);
@@ -147,7 +151,6 @@ const Environment = struct {
         env.superblock.format(superblock_format_callback, &env.superblock_context, .{
             .cluster = cluster,
             .replica = replica,
-            .storage_size_max = constants.storage_size_max,
         });
         env.tick_until_state_change(.init, .formatted);
     }

src/vsr/replica.zig
@@ -311,8 +311,8 @@ pub fn ReplicaType(
                 allocator,
                 .{
                     .storage = options.storage,
-                    .message_pool = options.message_pool,
                     .storage_size_limit = options.storage_size_limit,
+                    .message_pool = options.message_pool,
                 },
             );
 