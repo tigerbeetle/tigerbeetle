168a49061e15c0696d16c246c31cef1f1ced5605
Jamie Brandon <jamie@scattered-thoughts.net>
2022-12-13 09:13:56 -0800

    Verify table when starting table iterator

--- src/lsm/table.zig
+++ src/lsm/table.zig
@@ -984,6 +984,43 @@ pub fn TableType(
 
             return null;
         }
+
+        pub fn verify(
+            comptime Storage: type,
+            storage: *Storage,
+            index_address: u64,
+            key_min: ?Key,
+            key_max: ?Key,
+        ) void {
+            if (Storage != @import("../test/storage.zig").Storage)
+                // Too complicated to do async verification
+                return;
+
+            const index_block = storage.grid_block(index_address);
+            const addresses = index_data_addresses(index_block);
+            const data_blocks_used = index_data_blocks_used(index_block);
+            var data_block_index: usize = 0;
+            while (data_block_index < data_blocks_used) : (data_block_index += 1) {
+                const address = addresses[data_block_index];
+                const data_block = storage.grid_block(address);
+                const values = data_block_values_used(data_block);
+                if (values.len > 0) {
+                    if (data_block_index == 0) {
+                        assert(key_min == null or
+                            compare_keys(key_min.?, key_from_value(&values[0])) == .eq);
+                    }
+                    if (data_block_index == data_blocks_used - 1) {
+                        assert(key_max == null or
+                            compare_keys(key_from_value(&values[values.len - 1]), key_max.?) == .eq);
+                    }
+                    var a = &values[0];
+                    for (values[1..]) |*b| {
+                        assert(compare_keys(key_from_value(a), key_from_value(b)) == .lt);
+                        a = b;
+                    }
+                }
+            }
+        }
     };
 }
 
