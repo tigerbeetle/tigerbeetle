fbba7f4b884232b18dc4151e8196789af4a8d4cb
Alexander Mozeak <alex@astar.app>
2023-01-11 22:05:20 -0500

    Quote variables in build_lib.sh as a best practice.

--- src/clients/node/scripts/build_lib.sh
+++ src/clients/node/scripts/build_lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ -n $ZIG_BUILD_TARGET ]]; then
     echo "Running zig build-lib, targeting $ZIG_BUILD_TARGET"
-    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node -target $ZIG_BUILD_TARGET
+    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-"$(node --version)"/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node -target "$ZIG_BUILD_TARGET"
 else
-    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-$(node --version)/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
+    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-"$(node --version)"/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
 fi
