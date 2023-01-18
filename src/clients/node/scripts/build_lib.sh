9728154764828224082d8240d31ec389ad2523fa
Phil Eaton <phil@eatonphil.com>
2023-01-18 10:01:44 -0500

    Add build_lib.sh to files

--- src/clients/node/scripts/build_lib.sh
+++ src/clients/node/scripts/build_lib.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -e
+
 if [[ -n $ZIG_BUILD_TARGET ]]; then
     echo "Running zig build-lib, targeting $ZIG_BUILD_TARGET"
     mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-"$(node --version)"/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node -target "$ZIG_BUILD_TARGET"
