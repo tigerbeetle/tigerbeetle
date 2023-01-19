8f46b5d4e69e6a98f52892d3669b64adb5dca6fb
Phil Eaton <phil@eatonphil.com>
2023-01-19 11:44:15 -0500

    Try another way

--- src/clients/node/scripts/build_lib.sh
+++ src/clients/node/scripts/build_lib.sh
@@ -2,9 +2,26 @@
 
 set -e
 
-if [[ -n $ZIG_BUILD_TARGET ]]; then
-    echo "Running zig build-lib, targeting $ZIG_BUILD_TARGET"
-    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-"$(node --version)"/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node -target "$ZIG_BUILD_TARGET"
-else
-    mkdir -p dist && zig/zig build-lib -mcpu=baseline -OReleaseSafe -dynamic -lc -isystem build/node-"$(node --version)"/include/node src/node.zig -fallow-shlib-undefined -femit-bin=dist/client.node
+# macOS 13 Ventura is not supported on Zig 0.9.x.
+# Overriding -target is one workaround Andrew suggests.
+# https://github.com/ziglang/zig/issues/10478#issuecomment-1294313967
+# Cut everything after the second `.` in the target query result
+# because the rest of it doesn't always seem to be valid when passed
+# back in to `-target`.
+target="$(zig targets | jq -r '.native.triple' | cut -d '.' -f 1,2)"
+if [[ "$target" == "aarch64-macos.13" ]]; then
+    target="native-macos.11"
 fi
+
+echo "Building for $target"
+
+zig/zig build-lib \
+	-mcpu=baseline \
+	-OReleaseSafe \
+	-dynamic \
+	-lc \
+	-isystem build/node-"$(node --version)"/include/node \
+	-fallow-shlib-undefined \
+	-femit-bin=dist/client.node \
+	-target "$target" \
+	src/node.zig 
