5bf569a6cefc308713b5dbf8fc6afde8d91a2fcf
Federico Lorenzi <florenzi@gmail.com>
2023-02-24 13:50:53 +0200

    Optimize CI
    * Switch our builds to use Zig's cross compilation rather than Docker's
    * Build Node binaries using cross compilation for supported platforms, embed those in the package and build them using zig build
      * No more client side building!
      * Removes the need for cp / symlink hacks too!
    * Refactor our CI pipelines, to make them easier to follow and visualize
    * Switch our built Docker image to FROM scratch FROM alpine:3.17 - we use Alpine instead of scratch to make basic tools available to help debugging if any strangeness arises
    * Speed up the CI pipelines - barring the dotnet and java clients, it now takes ~6 minutes end to end.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-
+# TODO: In future, it would be great to have `zig build install` do everything,
+# with the user just needing to run ./scripts/install_zig.sh; zig/zig build install
 if [ -z "$DEBUG" ]; then
     debug="$DEBUG"
 fi
@@ -9,13 +10,25 @@ if [ "$1" = "--debug" ]; then
     debug="true"
 fi
 
+target="${TARGET:-}"
+if [ "${TARGETPLATFORM:-}" = "linux/arm64" ]; then
+    target="-Dtarget=aarch64-linux-gnu"
+fi
+
 set -eu
 
-scripts/install_zig.sh
+# Install Zig if it does not already exist:
+if [ ! -d "zig" ]; then
+    scripts/install_zig.sh
+fi
+
+
 if [ "$debug" = "true" ]; then
     echo "Building Tigerbeetle debug..."
-    zig/zig build install -Dcpu=baseline
+    # shellcheck disable=SC2086
+    zig/zig build install -Dcpu=baseline ${target}
 else
     echo "Building TigerBeetle..."
-    zig/zig build install -Dcpu=baseline -Drelease-safe
+    # shellcheck disable=SC2086
+    zig/zig build install -Dcpu=baseline -Drelease-safe ${target}
 fi
