e051bafbd175315ee1b030c8f810f6d0fd469b49
kprotty <king@tigerbeetle.com>
2023-03-02 13:11:38 -0600

    scripts: build.sh for proper `zig build`
    On macOS Ventura (13), building an exe when the OS portion
    of the target is "native" (i.e. `aarch64-native`, NOT `native-macos`)
    hits an abort in zig's compiler on 0.9. This abort was fixed in 0.10,
    but we are waiting until 0.11 to update for now. In the meantime,
    a workaround is required.
    
    `zig build` first generates an exe from `build_runner.zig` which
    imports our `build.zig`. Then, it runs this exe with our build flags.
    Said exe is built with a "native" OS portion of the target and hits
    the abort on 0.9 well before touching our build.zig.
    
    Instead, we have our own copy of `build_runner.zig` and run it
    with `-target native-macos` on Ventura which doesn't hit the abort.
    We also makes sure to pass the correct `-Dtarget` in `benchmark.sh`.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -12,7 +12,14 @@ fi
 COLOR_RED='\033[1;31m'
 COLOR_END='\033[0m'
 
-zig/zig build install -Drelease-safe -Dconfig=production
+# Determine the appropriate target flag (to support macos Ventura)
+if [ "$(uname)" = "Linux" ]; then
+    ZIG_TARGET="-Dtarget=native-linux"
+else
+    ZIG_TARGET="-Dtarget=native-macos"
+fi
+
+./scripts/build.sh install -Drelease-safe -Dconfig=production $ZIG_TARGET
 
 function onerror {
     if [ "$?" == "0" ]; then
@@ -54,7 +61,7 @@ done
 
 echo ""
 echo "Benchmarking..."
-zig/zig build benchmark -Drelease-safe -Dconfig=production -- "$@"
+./scripts/build.sh benchmark -Drelease-safe -Dconfig=production $ZIG_TARGET -- "$@"
 echo ""
 
 for I in $REPLICAS
