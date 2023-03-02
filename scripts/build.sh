f8a7a137cf05c574dfa4754b58eaf85489611b6d
kprotty <king@tigerbeetle.com>
2023-03-02 13:11:38 -0600

    scripts: build.sh for proper `zig build`
    On macOS Ventura (13), building an exe when the OS portion
    of the target is "native" (i.e. `aarch64-native`, NOT `native-macos`)
    hits an abort in zig's compiler on 0.9. This abort was fixed in 0.10,
    but we are waiting until 0.11 to update for now. In the meantime,
    a workaround is required.
    
    `zig build` first generates an exe from `build_runner.zig` which
    imports our `build.zig`. Then, it runs this exe with our build flags.
    Said exe is built with a "native" OS portion of the target and hits
    the abort on 0.9 well before touching our build.zig.
    
    Instead, we have our own copy of `build_runner.zig` and run it
    with `-target native-macos` on Ventura which doesn't hit the abort.
    We also makes sure to pass the correct `-Dtarget` in `benchmark.sh`.

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# Determine the operating system:
+if [ "$(uname)" = "Linux" ]; then
+    ZIG_OS="linux"
+else
+    ZIG_OS="macos"
+fi
+
+ZIG_EXE="./zig/zig"
+BUILD_ROOT="./"
+CACHE_ROOT="./zig-cache"
+GLOBAL_CACHE_ROOT="$HOME/.cache/zig"
+
+# This executes "zig/zig build {args}" using "-target native-{os}" for the build.zig executable.
+# Using the {os} directly instead of "native" avoids hitting an abort on macos Ventura.
+# TODO The abort was fixed in 0.10 while we're on 0.9. This script can be removed once we update zig.
+$ZIG_EXE run ./scripts/build_runner.zig -target native-$ZIG_OS --main-pkg-path $BUILD_ROOT \
+    -- $ZIG_EXE $BUILD_ROOT $CACHE_ROOT $GLOBAL_CACHE_ROOT \
+    $@
