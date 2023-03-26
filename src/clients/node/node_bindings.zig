a584b56e5ec231331762afe6b4cda81678c9cd14
batiati <rbatiati@gmail.com>
2023-03-25 21:14:20 -0300

    [Node Client] Autogenerated bindings
    - Better code organization
    - Doc comments

--- src/clients/node/node_bindings.zig
+++ src/clients/node/node_bindings.zig
@@ -2,23 +2,64 @@ const std = @import("std");
 const tb = @import("../../tigerbeetle.zig");
 const tb_client = @import("../c/tb_client.zig");
 
-const type_mappings = .{
-    .{ tb.AccountFlags, "AccountFlags" },
-    .{ tb.TransferFlags, "TransferFlags" },
-    .{ tb.Account, "Account" },
-    .{ tb.Transfer, "Transfer" },
-    .{ tb.CreateAccountResult, "CreateAccountError" },
-    .{ tb.CreateTransferResult, "CreateTransferError" },
-    .{ tb.CreateAccountsResult, "CreateAccountsError" },
-    .{ tb.CreateTransfersResult, "CreateTransfersError" },
-    .{ tb_client.tb_operation_t, "Operation" },
+const TypeMapping = struct {
+    name: []const u8,
+    hidden_fields: []const []const u8 = &.{},
+    docs_link: ?[]const u8 = null,
+
+    pub fn hidden(comptime self: @This(), name: []const u8) bool {
+        inline for (self.hidden_fields) |field| {
+            if (std.mem.eql(u8, field, name)) {
+                return true;
+            }
+        } else return false;
+    }
 };
 
-fn node_type(comptime Type: type) []const u8 {
+const type_mappings = .{
+    .{ tb.AccountFlags, TypeMapping{
+        .name = "AccountFlags",
+        .hidden_fields = &.{"padding"},
+        .docs_link = "reference/accounts#flags",
+    } },
+    .{ tb.TransferFlags, TypeMapping{
+        .name = "TransferFlags",
+        .hidden_fields = &.{"padding"},
+        .docs_link = "reference/transfers#flags",
+    } },
+    .{ tb.Account, TypeMapping{
+        .name = "Account",
+        .docs_link = "reference/accounts/#",
+    } },
+    .{ tb.Transfer, TypeMapping{
+        .name = "Transfer",
+        .docs_link = "reference/transfers/#",
+    } },
+    .{ tb.CreateAccountResult, TypeMapping{
+        .name = "CreateAccountError",
+        .docs_link = "reference/operations/create_accounts#",
+    } },
+    .{ tb.CreateTransferResult, TypeMapping{
+        .name = "CreateTransferError",
+        .docs_link = "reference/operations/create_transfers#",
+    } },
+    .{ tb.CreateAccountsResult, TypeMapping{
+        .name = "CreateAccountsError",
+    } },
+    .{ tb.CreateTransfersResult, TypeMapping{
+        .name = "CreateTransfersError",
+    } },
+    .{ tb_client.tb_operation_t, TypeMapping{
+        .name = "Operation",
+        .hidden_fields = &.{ "reserved", "root", "register" },
+    } },
+};
+
+fn typescript_type(comptime Type: type) []const u8 {
     switch (@typeInfo(Type)) {
         .Enum => return comptime get_mapped_type_name(Type) orelse @compileError("Type " ++ @typeName(Type) ++ " not mapped."),
         .Struct => |info| switch (info.layout) {
-            .Packed => return comptime node_type(std.meta.Int(.unsigned, @bitSizeOf(Type))),
+            .Packed => return comptime typescript_type(std.meta.Int(.unsigned, @bitSizeOf(Type))),
             else => return comptime get_mapped_type_name(Type) orelse @compileError("Type " ++ @typeName(Type) ++ " not mapped."),
         },
         .Int => |info| {
@@ -28,7 +69,7 @@ fn node_type(comptime Type: type) []const u8 {
                 32 => "number",
                 64 => "bigint",
                 128 => "bigint",
-                else => @compileError("invalid int type"),
+                else => @compileError("invalid int type: " ++ @typeName(Type)),
             };
         },
         else => @compileError("Unhandled type: " ++ @typeName(Type)),
@@ -38,7 +79,7 @@ fn node_type(comptime Type: type) []const u8 {
 fn get_mapped_type_name(comptime Type: type) ?[]const u8 {
     inline for (type_mappings) |type_mapping| {
         if (Type == type_mapping[0]) {
-            return type_mapping[1];
+            return type_mapping[1].name;
         }
     } else return null;
 }
@@ -46,10 +87,17 @@ fn get_mapped_type_name(comptime Type: type) ?[]const u8 {
 fn emit_enum(
     buffer: *std.ArrayList(u8),
     comptime type_info: anytype,
-    comptime name: []const u8,
+    comptime mapping: TypeMapping,
 ) !void {
-    try buffer.writer().print("export enum {s} {{\n", .{name});
+    try emit_docs(buffer, mapping, 0, null);
+
+    try buffer.writer().print("export enum {s} {{\n", .{mapping.name});
+
     inline for (type_info.fields) |field, i| {
+        if (comptime mapping.hidden(field.name)) continue;
+
+        try emit_docs(buffer, mapping, 1, field.name);
+
         try buffer.writer().print("  {s} = {d},\n", .{
             field.name,
             i,
@@ -62,10 +110,21 @@ fn emit_enum(
 fn emit_packed_struct(
     buffer: *std.ArrayList(u8),
     comptime type_info: anytype,
-    comptime name: []const u8,
+    comptime mapping: TypeMapping,
 ) !void {
-    try buffer.writer().print("export enum {s} {{\n", .{name});
+    try emit_docs(buffer, mapping, 0, null);
+
+    try buffer.writer().print(
+        \\export enum {s} {{
+        \\  none = 0,
+        \\
+    , .{mapping.name});
+
     inline for (type_info.fields) |field, i| {
+        if (comptime mapping.hidden(field.name)) continue;
+
+        try emit_docs(buffer, mapping, 1, field.name);
+
         try buffer.writer().print("  {s} = (1 << {d}),\n", .{
             field.name,
             i,
@@ -78,13 +137,19 @@ fn emit_packed_struct(
 fn emit_struct(
     buffer: *std.ArrayList(u8),
     comptime type_info: anytype,
-    comptime name: []const u8,
+    comptime mapping: TypeMapping,
 ) !void {
+    try emit_docs(buffer, mapping, 0, null);
+
     try buffer.writer().print("export type {s} = {{\n", .{
-        name,
+        mapping.name,
     });
 
     inline for (type_info.fields) |field| {
+        if (comptime mapping.hidden(field.name)) continue;
+
+        try emit_docs(buffer, mapping, 1, field.name);
+
         switch (@typeInfo(field.field_type)) {
             .Array => try buffer.writer().print("  {s}: Buffer\n", .{
                 field.name,
@@ -93,7 +158,7 @@ fn emit_struct(
                 "  {s}: {s}\n",
                 .{
                     field.name,
-                    node_type(field.field_type),
+                    typescript_type(field.field_type),
                 },
             ),
         }
@@ -102,12 +167,29 @@ fn emit_struct(
     try buffer.writer().print("}}\n\n", .{});
 }
 
+fn emit_docs(buffer: anytype, comptime mapping: TypeMapping, comptime indent: comptime_int, comptime field: ?[]const u8) !void {
+    if (mapping.docs_link) |docs_link| {
+        try buffer.writer().print(
+            \\
+            \\{[indent]s}/**
+            \\{[indent]s}* See [{[name]s}](https://docs.tigerbeetle.com/{[docs_link]s}{[field]s})
+            \\{[indent]s}*/
+            \\
+        , .{
+            .indent = "  " ** indent,
+            .name = field orelse mapping.name,
+            .docs_link = docs_link,
+            .field = field orelse "",
+        });
+    }
+}
+
 pub fn generate_bindings(buffer: *std.ArrayList(u8)) !void {
     @setEvalBranchQuota(100_000);
 
     try buffer.writer().print(
         \\///////////////////////////////////////////////////////
-        \\// This file was auto-generated by node_bindings.zig   //
+        \\// This file was auto-generated by node_bindings.zig //
         \\//              Do not manually modify.              //
         \\///////////////////////////////////////////////////////
         \\
@@ -117,15 +199,15 @@ pub fn generate_bindings(buffer: *std.ArrayList(u8)) !void {
     // Emit JS declarations.
     inline for (type_mappings) |type_mapping| {
         const ZigType = type_mapping[0];
-        const name = type_mapping[1];
+        const mapping = type_mapping[1];
 
         switch (@typeInfo(ZigType)) {
             .Struct => |info| switch (info.layout) {
                 .Auto => @compileError("Only packed or extern structs are supported: " ++ @typeName(ZigType)),
-                .Packed => try emit_packed_struct(buffer, info, name),
-                .Extern => try emit_struct(buffer, info, name),
+                .Packed => try emit_packed_struct(buffer, info, mapping),
+                .Extern => try emit_struct(buffer, info, mapping),
             },
-            .Enum => |info| try emit_enum(buffer, info, name),
+            .Enum => |info| try emit_enum(buffer, info, mapping),
             else => @compileError("Type cannot be represented: " ++ @typeName(ZigType)),
         }
     }
@@ -138,5 +220,5 @@ pub fn main() !void {
 
     var buffer = std.ArrayList(u8).init(allocator);
     try generate_bindings(&buffer);
-    try std.fs.cwd().writeFile("src/clients/node/src/types.ts", buffer.items);
+    try std.fs.cwd().writeFile("src/clients/node/src/bindings.ts", buffer.items);
 }
