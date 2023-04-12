b6ee8ebf9969210ad5e744fa527cac0e91ec89bc
Federico Lorenzi <florenzi@gmail.com>
2023-04-12 17:19:26 +0200

    Don't stack allocate AOFEntry

--- src/testing/aof.zig
+++ src/testing/aof.zig
@@ -32,6 +32,7 @@ const InMemoryAOF = struct {
 pub const AOF = struct {
     index: usize,
     backing_store: []align(constants.sector_size) u8,
+    validation_target: *AOFEntry,
     last_checksum: ?u128 = null,
     validation_checksums: std.AutoHashMap(u128, void) = undefined,
 
@@ -39,16 +40,21 @@ pub const AOF = struct {
         const memory = try allocator.allocAdvanced(u8, constants.sector_size, backing_size, .exact);
         errdefer allocator.free(memory);
 
+        const target = try allocator.create(AOFEntry);
+        errdefer allocator.free(target);
+
         log.debug("init. allocated {} bytes", .{backing_size});
         return AOF{
             .index = 0,
             .backing_store = memory,
+            .validation_target = target,
             .validation_checksums = std.AutoHashMap(u128, void).init(allocator),
         };
     }
 
     pub fn deinit(self: *AOF, allocator: std.mem.Allocator) void {
         allocator.free(self.backing_store);
+        allocator.destroy(self.validation_target);
         self.validation_checksums.deinit();
     }
 
@@ -61,10 +67,9 @@ pub const AOF = struct {
         // a parent.
         it.validate_chain = false;
 
-        var last_entry: *AOFEntry = undefined;
-        var target: AOFEntry = undefined;
+        var last_entry: ?*AOFEntry = null;
 
-        while (try it.next(&target)) |entry| {
+        while (try it.next(self.validation_target)) |entry| {
             const header = entry.header();
             if (self.validation_checksums.count() != 0) {
                 assert(self.validation_checksums.get(header.parent) != null);
@@ -74,11 +79,11 @@ pub const AOF = struct {
             last_entry = entry;
         }
 
-        if (last_entry.header().checksum != last_checksum) {
+        if (last_entry.?.header().checksum != last_checksum) {
             return error.ChecksumMismatch;
         }
 
-        log.debug("validated all aof entries. last entry checksum {} matches supplied {}", .{ last_entry.header().checksum, last_checksum });
+        log.debug("validated all aof entries. last entry checksum {} matches supplied {}", .{ last_entry.?.header().checksum, last_checksum });
     }
 
     pub fn write(self: *AOF, message: *const Message, options: struct { replica: u8, primary: u8 }) !void {
