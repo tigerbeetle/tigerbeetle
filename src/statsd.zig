17ceef93f4b7345fba683a4307b405d86a261d91
Federico Lorenzi <florenzi@gmail.com>
2023-04-25 16:33:23 +0200

    Fixups for IO

--- src/statsd.zig
+++ src/statsd.zig
@@ -3,21 +3,22 @@ const std = @import("std");
 const IO = @import("io.zig").IO;
 const FIFO = @import("fifo.zig").FIFO;
 
+const BufferCompletion = struct {
+    next: ?*BufferCompletion = null,
+    buffer: [256]u8,
+    completion: IO.Completion = undefined,
+};
+
 pub const StatsD = struct {
-    buffer: []u8,
     socket: std.os.socket_t,
     io: *IO,
-    completions: []IO.Completion,
-    completions_fifo: FIFO(IO.Completion) = .{ .name = "statsd" },
+    buffer_completions: []BufferCompletion,
+    buffer_completions_fifo: FIFO(BufferCompletion) = .{ .name = "statsd" },
 
     /// Creates a statsd instance, which will send UDP packets via the IO instance provided.
     /// Not thread safe, since it uses a single buffer shared between gauge and timing methods
     /// with no locking.
     pub fn init(allocator: std.mem.Allocator, io: *IO, address: std.net.Address) !StatsD {
-        // Limit the max packet size to 1000.
-        const buffer = try allocator.alloc(u8, 1000);
-        errdefer allocator.free(buffer);
-
         const socket = try io.open_socket(
             address.any.family,
             std.os.SOCK.DGRAM,
@@ -25,20 +26,18 @@ pub const StatsD = struct {
         );
         errdefer std.os.closeSocket(socket);
 
-        // Allocate our completions - hardcoded max of 256.
-        var completions = try allocator.alloc(IO.Completion, 256);
-        errdefer allocator.free(completions);
+        const buffer_completions = try allocator.alloc(BufferCompletion, 256);
+        errdefer allocator.free(buffer_completions);
 
         var statsd = StatsD{
-            .buffer = buffer,
             .socket = socket,
             .io = io,
-            .completions = completions,
+            .buffer_completions = buffer_completions,
         };
 
-        for (completions) |*completion| {
-            completion.next = null;
-            statsd.completions_fifo.push(completion);
+        for (buffer_completions) |*buffer_completion| {
+            buffer_completion.next = null;
+            statsd.buffer_completions_fifo.push(buffer_completion);
         }
 
         // 'Connect' the UDP socket, so we can just send() to it normally.
@@ -48,47 +47,41 @@ pub const StatsD = struct {
     }
 
     pub fn deinit(self: *StatsD, allocator: std.mem.Allocator) void {
-        allocator.free(self.buffer);
         std.os.closeSocket(self.socket);
-        allocator.free(self.completions);
+        allocator.free(self.buffer_completions);
     }
 
     pub fn gauge(self: *StatsD, stat: []const u8, value: usize) !void {
-        const statsd_packet = try std.fmt.bufPrint(self.buffer, "{s}:{}|g", .{ stat, value });
-        var completion = self.completions_fifo.pop() orelse return error.NoSpaceLeft;
-        completion.next = null;
+        var buffer_completion = self.buffer_completions_fifo.pop() orelse return error.NoSpaceLeft;
+        const statsd_packet = try std.fmt.bufPrint(buffer_completion.buffer[0..], "{s}:{}|g", .{ stat, value });
 
         self.io.send(
             *StatsD,
             self,
             StatsD.send_callback,
-            completion,
+            &buffer_completion.completion,
             self.socket,
             statsd_packet,
         );
     }
 
     pub fn timing(self: *StatsD, stat: []const u8, ms: usize) !void {
-        const statsd_packet = try std.fmt.bufPrint(self.buffer, "{s}:{}|ms", .{ stat, ms });
-        var completion = self.completions_fifo.pop() orelse return error.NoSpaceLeft;
-        completion.next = null;
+        var buffer_completion = self.buffer_completions_fifo.pop() orelse return error.NoSpaceLeft;
+        const statsd_packet = try std.fmt.bufPrint(buffer_completion.buffer[0..], "{s}:{}|g", .{ stat, ms });
 
         self.io.send(
             *StatsD,
             self,
             StatsD.send_callback,
-            completion,
+            &buffer_completion.completion,
             self.socket,
             statsd_packet,
         );
     }
 
     pub fn send_callback(context: *StatsD, completion: *IO.Completion, result: IO.SendError!usize) void {
-        _ = context;
-        _ = completion;
         _ = result catch {};
-
-        completion.next = null;
-        context.completions_fifo.push(completion);
+        var buffer_completion = @fieldParentPtr(BufferCompletion, "completion", completion);
+        context.buffer_completions_fifo.push(buffer_completion);
     }
 };
