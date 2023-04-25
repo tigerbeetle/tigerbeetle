eee1689d18f017861d8b4c90dc0b7bf77989db00
Federico Lorenzi <florenzi@gmail.com>
2023-04-25 16:16:33 +0200

    Switch to IO

--- src/statsd.zig
+++ src/statsd.zig
@@ -1,13 +1,18 @@
 const std = @import("std");
 
 const IO = @import("io.zig").IO;
+const FIFO = @import("fifo.zig").FIFO;
 
 pub const StatsD = struct {
     buffer: []u8,
     socket: std.os.socket_t,
-    address: std.net.Address,
-    address_size: u32,
+    io: *IO,
+    completions: []IO.Completion,
+    completions_fifo: FIFO(IO.Completion) = .{ .name = "statsd" },
 
+    /// Creates a statsd instance, which will send UDP packets via the IO instance provided.
+    /// Not thread safe, since it uses a single buffer shared between gauge and timing methods
+    /// with no locking.
     pub fn init(allocator: std.mem.Allocator, io: *IO, address: std.net.Address) !StatsD {
         // Limit the max packet size to 1000.
         const buffer = try allocator.alloc(u8, 1000);
@@ -20,58 +25,70 @@ pub const StatsD = struct {
         );
         errdefer std.os.closeSocket(socket);
 
-        return StatsD{
+        // Allocate our completions - hardcoded max of 256.
+        var completions = try allocator.alloc(IO.Completion, 256);
+        errdefer allocator.free(completions);
+
+        var statsd = StatsD{
             .buffer = buffer,
             .socket = socket,
-            .address = address,
-            .address_size = address.getOsSockLen(),
+            .io = io,
+            .completions = completions,
         };
+
+        for (completions) |*completion| {
+            completion.next = null;
+            statsd.completions_fifo.push(completion);
+        }
+
+        // 'Connect' the UDP socket, so we can just send() to it normally.
+        try std.os.connect(socket, &address.any, address.getOsSockLen());
+
+        return statsd;
     }
 
     pub fn deinit(self: *StatsD, allocator: std.mem.Allocator) void {
         allocator.free(self.buffer);
         std.os.closeSocket(self.socket);
+        allocator.free(self.completions);
     }
 
     pub fn gauge(self: *StatsD, stat: []const u8, value: usize) !void {
         const statsd_packet = try std.fmt.bufPrint(self.buffer, "{s}:{}|g", .{ stat, value });
+        var completion = self.completions_fifo.pop() orelse return error.NoSpaceLeft;
+        completion.next = null;
 
-        var iovec = [_]std.os.iovec_const{.{
-            .iov_base = statsd_packet.ptr,
-            .iov_len = statsd_packet.len,
-        }};
-
-        var msg = std.os.msghdr_const{
-            .name = &self.address.any,
-            .namelen = self.address_size,
-            .iov = &iovec,
-            .iovlen = 1,
-            .control = null,
-            .controllen = 0,
-            .flags = 0,
-        };
-
-        _ = try std.os.sendmsg(self.socket, msg, 0);
+        self.io.send(
+            *StatsD,
+            self,
+            StatsD.send_callback,
+            completion,
+            self.socket,
+            statsd_packet,
+        );
     }
 
     pub fn timing(self: *StatsD, stat: []const u8, ms: usize) !void {
         const statsd_packet = try std.fmt.bufPrint(self.buffer, "{s}:{}|ms", .{ stat, ms });
+        var completion = self.completions_fifo.pop() orelse return error.NoSpaceLeft;
+        completion.next = null;
 
-        var iovec = [_]std.os.iovec_const{.{
-            .iov_base = statsd_packet.ptr,
-            .iov_len = statsd_packet.len,
-        }};
+        self.io.send(
+            *StatsD,
+            self,
+            StatsD.send_callback,
+            completion,
+            self.socket,
+            statsd_packet,
+        );
+    }
 
-        var msg = std.os.msghdr_const{
-            .name = &self.address.any,
-            .namelen = self.address_size,
-            .iov = &iovec,
-            .iovlen = 1,
-            .control = null,
-            .controllen = 0,
-            .flags = 0,
-        };
+    pub fn send_callback(context: *StatsD, completion: *IO.Completion, result: IO.SendError!usize) void {
+        _ = context;
+        _ = completion;
+        _ = result catch {};
 
-        _ = try std.os.sendmsg(self.socket, msg, 0);
+        completion.next = null;
+        context.completions_fifo.push(completion);
     }
 };
