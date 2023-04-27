64ab0ba5a22168767aeaffebb7ec23fc17bd5531
Phil Eaton <phil@eatonphil.com>
2023-04-27 16:52:17 +0000

    Fix for shellcheck

--- scripts/next_version.sh
+++ scripts/next_version.sh
@@ -10,7 +10,7 @@ git fetch --tags --all
 version="$MAJOR.$MINOR.0"
 latest="$(git tag | grep $MAJOR.$MINOR | sort -r | head -n1)"
 if [ "$latest" != "" ]; then
-    patch="$(echo $latest | cut -d '.' -f 3)"
+    patch="$(echo "$latest" | cut -d '.' -f 3)"
     version="$MAJOR.$MINOR.$((patch+1))"
 fi
 echo "$version"
