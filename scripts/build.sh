9a0bcec14003fdc10a85a0b5b252849481e426de
Phil Eaton <phil@eatonphil.com>
2023-06-23 13:52:57 +0000

    Trace in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,9 +29,14 @@ BUILD_ROOT="./"
 CACHE_ROOT="./zig-cache"
 GLOBAL_CACHE_ROOT="$HOME/.cache/zig"
 
+TRACE=""
+if [ "$ZIG_OS" ="macos" ]; then
+    TRACE="dtruss"
+fi
+
 # This executes "zig/zig build {args}" using "-target native-{os}" for the build.zig executable.
 # Using the {os} directly instead of "native" avoids hitting an abort on macos Ventura.
 # TODO The abort was fixed in 0.10 while we're on 0.9. This script can be removed once we update zig.
-$ZIG_EXE run ./scripts/build_runner.zig -target native-$ZIG_OS --main-pkg-path $BUILD_ROOT \
+$TRACE $ZIG_EXE run ./scripts/build_runner.zig -target native-$ZIG_OS --main-pkg-path $BUILD_ROOT \
     -- $ZIG_EXE $BUILD_ROOT $CACHE_ROOT "$GLOBAL_CACHE_ROOT" $TARGET \
     "$@"
