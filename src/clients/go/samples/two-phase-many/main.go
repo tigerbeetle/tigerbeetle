c2a13e6f937c9fcc7d49bcafa64b2103be99a1c7
Phil Eaton <phil@eatonphil.com>
2023-07-20 16:15:59 -0400

    Add Go two-phase-many sample project

--- src/clients/go/samples/two-phase-many/main.go
+++ src/clients/go/samples/two-phase-many/main.go
@@ -26,7 +26,7 @@ func assert(a, b interface{}, field string) {
 	}
 }
 
-func assertAccountBalances(client *tb.Client, accounts []tb_types.Account, debugMsg string) {
+func assertAccountBalances(client tb.Client, accounts []tb_types.Account, debugMsg string) {
 	ids := []tb_types.Uint128{}
 	for _, account := range accounts {
 		ids = append(ids, account.ID)
@@ -167,11 +167,167 @@ func main() {
 		}
 	}, "after starting 5 pending transfers")
 
-	// Create a second transfer simply posting the first transfer
+	// Create a 6th transfer posting the 1st transfer
 	transferRes, err = client.CreateTransfers([]tb_types.Transfer{
 		{
-			ID:              uint128("2"),
+			ID:              uint128("6"),
 			PendingID:       uint128("1"),
+			DebitAccountID:  uint128("1"),
+			CreditAccountID: uint128("2"),
+			Ledger:          1,
+			Code:            1,
+			Amount:          100,
+			Flags:           tb_types.TransferFlags{PostPendingTransfer: true}.ToUint16(),
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating transfers: %s", err)
+	}
+
+	for _, err := range transferRes {
+		log.Fatalf("Error creating transfer: %s", err.Result)
+	}
+
+	// Validate account balances after posting 1st pending transfer.
+	assertAccountBalances(client, []tb_types.Account{
+		{
+			ID: uint128("1"),
+			DebitsPosted: uint64(100),
+			CreditsPosted: uint64(0),
+			DebitsPending: uint64(1400),
+			CreditsPending: uint64(0),
+		},
+		{
+			ID: uint128("2"),
+			DebitsPosted: uint64(0),
+			CreditsPosted: uint64(100),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(1400),
+		}
+	}, "after completing 1 pending transfer")
+
+	// Create a 7th transfer voiding the 2nd transfer
+	transferRes, err = client.CreateTransfers([]tb_types.Transfer{
+		{
+			ID:              uint128("7"),
+			PendingID:       uint128("2"),
+			DebitAccountID:  uint128("1"),
+			CreditAccountID: uint128("2"),
+			Ledger:          1,
+			Code:            1,
+			Amount:          200,
+			Flags:           tb_types.TransferFlags{VoidPendingTransfer: true}.ToUint16(),
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating transfers: %s", err)
+	}
+
+	for _, err := range transferRes {
+		log.Fatalf("Error creating transfer: %s", err.Result)
+	}
+
+	// Validate account balances after voiding 2nd pending transfer.
+	assertAccountBalances(client, []tb_types.Account{
+		{
+			ID: uint128("1"),
+			DebitsPosted: uint64(100),
+			CreditsPosted: uint64(0),
+			DebitsPending: uint64(1200),
+			CreditsPending: uint64(0),
+		},
+		{
+			ID: uint128("2"),
+			DebitsPosted: uint64(0),
+			CreditsPosted: uint64(100),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(1200),
+		}
+	}, "after completing 2 pending transfers")
+
+	// Create a 8th transfer posting the 3rd transfer
+	transferRes, err = client.CreateTransfers([]tb_types.Transfer{
+		{
+			ID:              uint128("8"),
+			PendingID:       uint128("3"),
+			DebitAccountID:  uint128("1"),
+			CreditAccountID: uint128("2"),
+			Ledger:          1,
+			Code:            1,
+			Amount:          300,
+			Flags:           tb_types.TransferFlags{PostPendingTransfer: true}.ToUint16(),
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating transfers: %s", err)
+	}
+
+	for _, err := range transferRes {
+		log.Fatalf("Error creating transfer: %s", err.Result)
+	}
+
+	// Validate account balances after voiding 3rd pending transfer.
+	assertAccountBalances(client, []tb_types.Account{
+		{
+			ID: uint128("1"),
+			DebitsPosted: uint64(400),
+			CreditsPosted: uint64(0),
+			DebitsPending: uint64(900),
+			CreditsPending: uint64(0),
+		},
+		{
+			ID: uint128("2"),
+			DebitsPosted: uint64(0),
+			CreditsPosted: uint64(400),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(900),
+		}
+	}, "after completing 3 pending transfers")
+
+	// Create a 9th transfer voiding the 4th transfer
+	transferRes, err = client.CreateTransfers([]tb_types.Transfer{
+		{
+			ID:              uint128("9"),
+			PendingID:       uint128("4"),
+			DebitAccountID:  uint128("1"),
+			CreditAccountID: uint128("2"),
+			Ledger:          1,
+			Code:            1,
+			Amount:          400,
+			Flags:           tb_types.TransferFlags{VoidPendingTransfer: true}.ToUint16(),
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating transfers: %s", err)
+	}
+
+	for _, err := range transferRes {
+		log.Fatalf("Error creating transfer: %s", err.Result)
+	}
+
+	// Validate account balances after voiding 4th pending transfer.
+	assertAccountBalances(client, []tb_types.Account{
+		{
+			ID: uint128("1"),
+			DebitsPosted: uint64(400),
+			CreditsPosted: uint64(0),
+			DebitsPending: uint64(500),
+			CreditsPending: uint64(0),
+		},
+		{
+			ID: uint128("2"),
+			DebitsPosted: uint64(0),
+			CreditsPosted: uint64(400),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(500),
+		}
+	}, "after completing 4 pending transfers")
+
+	// Create a 10th transfer voiding the 5th transfer
+	transferRes, err = client.CreateTransfers([]tb_types.Transfer{
+		{
+			ID:              uint128("10"),
+			PendingID:       uint128("5"),
 			DebitAccountID:  uint128("1"),
 			CreditAccountID: uint128("2"),
 			Ledger:          1,
@@ -188,28 +344,23 @@ func main() {
 		log.Fatalf("Error creating transfer: %s", err.Result)
 	}
 
-	// Validate accounts pending and posted debits/credits after finishing the two-phase transfer
-	accounts, err = client.LookupAccounts([]tb_types.Uint128{uint128("1"), uint128("2")})
-	if err != nil {
-		log.Fatalf("Could not fetch accounts: %s", err)
-	}
-	assert(len(accounts), 2, "accounts")
-
-	for _, account := range accounts {
-		if account.ID == uint128("1") {
-			assert(account.DebitsPosted, uint64(500), "account 1 debits")
-			assert(account.CreditsPosted, uint64(0), "account 1 credits")
-			assert(account.DebitsPending, uint64(0), "account 1 debits pending")
-			assert(account.CreditsPending, uint64(0), "account 1 credits pending")
-		} else if account.ID == uint128("2") {
-			assert(account.DebitsPosted, uint64(0), "account 2 debits")
-			assert(account.CreditsPosted, uint64(500), "account 2 credits")
-			assert(account.DebitsPending, uint64(0), "account 2 debits pending")
-			assert(account.CreditsPending, uint64(0), "account 2 credits pending")
-		} else {
-			log.Fatalf("Unexpected account: %s", account.ID)
+	// Validate account balances after voiding 4th pending transfer.
+	assertAccountBalances(client, []tb_types.Account{
+		{
+			ID: uint128("1"),
+			DebitsPosted: uint64(900),
+			CreditsPosted: uint64(0),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(0),
+		},
+		{
+			ID: uint128("2"),
+			DebitsPosted: uint64(0),
+			CreditsPosted: uint64(900),
+			DebitsPending: uint64(0),
+			CreditsPending: uint64(0),
 		}
-	}
+	}, "after completing 5 pending transfers")
 
 	fmt.Println("ok")
 }
