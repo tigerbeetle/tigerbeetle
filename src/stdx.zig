84fb8098a4fdaa256a337c9d071f800e98a16f9c
Alex Kladov <aleksey.kladov@gmail.com>
2023-07-26 12:28:01 +0100

    stdx: keep hashing functions together
    `update` originally got in-between hashing API and impl because the impl
    incuded huge test vectors, but, now that they are gone, we can go for
    logical layout.

--- src/stdx.zig
+++ src/stdx.zig
@@ -293,21 +293,6 @@ pub inline fn hash_inline(value: anytype) u64 {
     });
 }
 
-/// Returns a copy of `base` with fields changed according to `diff`.
-///
-/// Intended exclusively for table-driven prototype-based tests. Write
-/// updates explicitly in production code.
-pub fn update(base: anytype, diff: anytype) @TypeOf(base) {
-    assert(builtin.is_test);
-    assert(@typeInfo(@TypeOf(base)) == .Struct);
-
-    var updated = base;
-    inline for (std.meta.fields(@TypeOf(diff))) |f| {
-        @field(updated, f.name) = @field(diff, f.name);
-    }
-    return updated;
-}
-
 /// Inline version of Google Abseil "LowLevelHash" (inspired by wyhash).
 /// https://github.com/abseil/abseil-cpp/blob/master/absl/hash/internal/low_level_hash.cc
 inline fn low_level_hash(seed: u64, input: anytype) u64 {
@@ -371,4 +356,19 @@ test "hash_inline" {
         const hash = low_level_hash(case.seed, input);
         try std.testing.expectEqual(case.hash, hash);
     }
+}
+
+/// Returns a copy of `base` with fields changed according to `diff`.
+///
+/// Intended exclusively for table-driven prototype-based tests. Write
+/// updates explicitly in production code.
+pub fn update(base: anytype, diff: anytype) @TypeOf(base) {
+    assert(builtin.is_test);
+    assert(@typeInfo(@TypeOf(base)) == .Struct);
+
+    var updated = base;
+    inline for (std.meta.fields(@TypeOf(diff))) |f| {
+        @field(updated, f.name) = @field(diff, f.name);
+    }
+    return updated;
 }
