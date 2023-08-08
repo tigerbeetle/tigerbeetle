eb6e1f85894c96c14ff0ce4951a7308af7ef66b5
Phil Eaton <phil@eatonphil.com>
2023-08-08 15:02:06 -0400

    Add swap for local dotnet builds in integration tests

--- src/clients/dotnet/docs.zig
+++ src/clients/dotnet/docs.zig
@@ -1,24 +1,137 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-const Docs = @import("../docs_types.zig").Docs;
-const run = @import("../shutil.zig").run;
-const run_shell = @import("../shutil.zig").run_shell;
-const script_filename = @import("../shutil.zig").script_filename;
-const write_shell_newlines_into_single_line = @import("../shutil.zig").write_shell_newlines_into_single_line;
+const assert = std.debug.assert;
 
-fn current_commit_pre_install_hook(
+const Docs = @import("../docs_types.zig").Docs;
+const binary_filename = @import("../shutil.zig").binary_filename;
+const run_shell = @import("../shutil.zig").run_shell;
+const path_separator = @import("../shutil.zig").path_separator;
+const file_or_directory_exists = @import("../shutil.zig").file_or_directory_exists;
+
+fn current_commit_post_install_hook(
     arena: *std.heap.ArenaAllocator,
-    sample_dir: []const u8,
-    _: []const u8,
+    sample_directory: []const u8,
+    root: []const u8,
 ) !void {
-    try std.os.chdir(sample_dir);
-    run_shell(arena, "rm *.csproj") catch {
-        // Ok if it doesn't exist.
+    try std.os.chdir(root);
+    try run_shell(
+        arena,
+        try std.fmt.allocPrint(
+            arena.allocator(),
+            "{s} build dotnet_client",
+            .{try binary_filename(arena, &[_][]const u8{ "zig", "zig" })},
+        ),
+    );
+
+    try std.os.chdir(sample_directory);
+    // Find the .csproj file so we can swap out the public package
+    // with our local build, if the .csproj file exists.
+    var dir = try std.fs.cwd().openIterableDir(".", .{});
+    defer dir.close();
+
+    // When running integration tests, the integration tests already
+    // have a .csproj file. But this will jostle with the .csproj
+    // created during the prepare_directory step when we call `dotnet
+    // new console`. So we'll get rid of the existing .csproj file
+    // before we call `dotnet new`.
+    var path_parts_backwards = std.mem.splitBackwards(
+        u8,
+        sample_directory,
+        path_separator,
+    );
+    const directory_name = path_parts_backwards.next().?;
+    const generated_csproj_filename = try std.fmt.allocPrint(
+        arena.allocator(),
+        "{s}.csproj",
+        .{directory_name},
+    );
+    const generated_csproj_file_exists =
+        file_or_directory_exists(generated_csproj_filename);
+
+    const csproj_filename = blk: {
+        var walker = try dir.walk(arena.allocator());
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            // Need to see if there's another .csproj file. If there
+            // is, we can delete the generated one.
+            if (std.mem.eql(u8, entry.path, generated_csproj_filename)) {
+                continue;
+            }
+
+            if (std.mem.endsWith(u8, entry.path, ".csproj")) {
+                assert(!std.mem.eql(u8, generated_csproj_filename, entry.path));
+                if (generated_csproj_file_exists) {
+                    try run_shell(arena, try std.fmt.allocPrint(
+                        arena.allocator(),
+                        "rm {s}",
+                        .{generated_csproj_filename},
+                    ));
+                }
+
+                break :blk entry.path;
+            }
+        }
+
+        if (file_or_directory_exists(generated_csproj_filename)) {
+            break :blk generated_csproj_filename;
+        }
+
+        return error.CSProjFileNotFound;
     };
-    run_shell(arena, "rm Program.cs") catch {
-        // Ok if it doesn't exist.
-    };
+    assert(file_or_directory_exists(csproj_filename));
+
+    try run_shell(arena, "dotnet remove package tigerbeetle");
+
+    const public_reference =
+        \\</Project>
+    ;
+    const old_csproj_contents = try std.fs.cwd().readFileAlloc(arena.allocator(), csproj_filename, std.math.maxInt(usize));
+    assert(std.mem.containsAtLeast(u8, old_csproj_contents, 1, public_reference));
+
+    const local_reference = try std.fmt.allocPrint(
+        arena.allocator(),
+        \\  <ItemGroup>
+        \\    <ProjectReference Include="{s}/src/clients/dotnet/TigerBeetle/TigerBeetle.csproj" />
+        \\  </ItemGroup>
+        \\  <ItemGroup>
+        \\    <Content Include="{s}/src/clients/dotnet/TigerBeetle/runtimes/$(RuntimeIdentifier)/native/*.*">
+        \\      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+        \\    </Content>
+        \\  </ItemGroup>
+        \\</Project>
+    ,
+        .{ root, root },
+    );
+
+    const property_group = "</PropertyGroup>";
+    assert(std.mem.containsAtLeast(u8, old_csproj_contents, 1, property_group));
+
+    const property_group_with_runtime_info =
+        \\  <UseCurrentRuntimeIdentifier>true</UseCurrentRuntimeIdentifier>
+        \\</PropertyGroup>
+    ;
+
+    const csproj_contents =
+        try std.mem.replaceOwned(
+        u8,
+        arena.allocator(),
+        try std.mem.replaceOwned(
+            u8,
+            arena.allocator(),
+            old_csproj_contents,
+            public_reference,
+            local_reference,
+        ),
+        property_group,
+        property_group_with_runtime_info,
+    );
+
+    try std.fs.cwd().writeFile(
+        csproj_filename,
+        csproj_contents,
+    );
 }
 
 pub const DotnetDocs = Docs{
@@ -61,8 +174,8 @@ pub const DotnetDocs = Docs{
     \\Console.WriteLine("SUCCESS");
     ,
 
-    .current_commit_pre_install_hook = current_commit_pre_install_hook,
-    .current_commit_post_install_hook = null,
+    .current_commit_pre_install_hook = null,
+    .current_commit_post_install_hook = current_commit_post_install_hook,
 
     .install_commands = 
     \\dotnet new console
