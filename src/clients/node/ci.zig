42d274d601ae8a6861a31ff578ee17880c5b17b3
Alex Kladov <aleksey.kladov@gmail.com>
2023-10-18 18:14:30 +0100

    node: move distro testing to ci.zig
    We used to do this directly in .yml, but let's do this in an
    easy-locally-repeatable.
    
    While we are at it, actually change the way we install the node --- what
    we have in scripts is actually deprecated, and inludes a `sleep 60` to
    motivate us to upgarade :)
    
    I've also removed meentions of the scripts from the docs -- we don't
    need anything but nodejs, and I don't think we should explain folks how
    to install node

--- src/clients/node/ci.zig
+++ src/clients/node/ci.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const log = std.log;
 const assert = std.debug.assert;
 
@@ -28,6 +29,73 @@ pub fn tests(shell: *Shell, gpa: std.mem.Allocator) !void {
         try shell.env.put("TB_ADDRESS", tmp_beetle.port_str.slice());
         try shell.exec("npm install", .{});
         try shell.exec("node main.js", .{});
+    }
+
+    // Container smoke tests.
+    if (builtin.target.os.tag == .linux) {
+        var client_dir = try shell.project_root.openDir("src/clients/node/", .{});
+        defer client_dir.close();
+
+        try client_dir.setAsCwd();
+
+        // Installing node through <https://github.com/nodesource/distributions>.
+
+        const deb_install_script =
+            \\apt-get update
+            \\apt-get install -y ca-certificates curl gnupg
+            \\mkdir -p /etc/apt/keyrings
+            \\
+            \\curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
+            \\  | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+            \\
+            \\echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] \
+            \\    https://deb.nodesource.com/node_18.x nodistro main" \
+            \\  | tee /etc/apt/sources.list.d/nodesource.list
+            \\
+            \\apt-get update
+            \\apt-get install nodejs -y
+        ;
+
+        const rpm_install_script =
+            \\yum install \
+            \\  https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+            \\yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
+        ;
+
+        const distributions = .{
+            "alpine", "debian",      "ubuntu",
+            "fedora", "redhat/ubi9", "amazonlinux:2.0.20230307.0",
+        };
+
+        const install_scripts = .{
+            \\apk add --update nodejs npm
+            ,
+            deb_install_script,
+            deb_install_script,
+            rpm_install_script,
+            "update-crypto-policies --set DEFAULT:SHA1\n" ++ rpm_install_script,
+            rpm_install_script,
+        };
+
+        inline for (distributions, install_scripts) |distribution, install_script| {
+            try shell.exec(
+                \\docker run
+                \\--security-opt seccomp=unconfined
+                \\--volume ./:/host
+                \\{image}
+                \\sh
+                \\-c {script}
+            , .{
+                .image = distribution,
+                .script = 
+                \\set -ex
+                \\mkdir test-project && cd test-project
+                ++ "\n" ++ install_script ++ "\n" ++
+                    \\npm install /host/tigerbeetle-node-*.tgz
+                    \\node -e 'require("tigerbeetle-node"); console.log("SUCCESS!")'
+                ,
+            });
+        }
     }
 }
 
