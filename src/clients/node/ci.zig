f85f02e8ba7e3f95da31fe892358c28e90c943d7
Alex Kladov <aleksey.kladov@gmail.com>
2023-10-18 16:56:38 +0100

    CI: disable large instances
    They are two expensive. We need to figure _something_ out, but, for now,
    let's scale down to just ubuntu latest.

--- src/clients/node/ci.zig
+++ src/clients/node/ci.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const log = std.log;
 const assert = std.debug.assert;
 
@@ -28,6 +29,60 @@ pub fn tests(shell: *Shell, gpa: std.mem.Allocator) !void {
         try shell.env.put("TB_ADDRESS", tmp_beetle.port_str.slice());
         try shell.exec("npm install", .{});
         try shell.exec("node main.js", .{});
+    }
+
+    // Container smoke tests.
+    if (builtin.target.os.tag == .linux) {
+        var client_dir = try shell.project_root.openDir("src/clients/dotnet/", .{});
+        defer client_dir.close();
+
+        try client_dir.setAsCwd();
+
+        // Here, we want to check that our package does not break horrible on upstream containers
+        // due to missing runtime dependencies, mismatched glibc ABI and similar issues.
+        //
+        // We don't necessary want to be able to _build_ code inside such a container, we only
+        // need to check that pre-built code runs successfully. So, build a package on host,
+        // mount it inside the container and smoke test.
+        //
+        // We run an sh script inside a container, because it is trivial. If it grows larger,
+        // we should consider running a proper zig program inside.
+        try shell.exec("dotnet pack --configuration Release", .{});
+
+        const image_tags = .{
+            "7.0",        "6.0",        "3.1",
+            "7.0-alpine", "6.0-alpine", "3.1-alpine",
+        };
+
+        inline for (image_tags) |image_tag| {
+            try shell.exec(
+                \\docker run
+                \\--security-opt seccomp=unconfined
+                \\--volume ./TigerBeetle/bin/Release:/nuget
+                \\{image}
+                \\sh
+                \\-c {script}
+            , .{
+                .image = "mcr.microsoft.com/dotnet/sdk:" ++ image_tag,
+                .script =
+                \\set -ex
+                \\mkdir test-project && cd test-project
+                \\dotnet nuget add source /nuget
+                \\dotnet new console
+                \\dotnet add package tigerbeetle --source /nuget > /dev/null
+                \\cat <<EOF > Program.cs
+                \\using System;
+                \\public class Program {
+                \\  public static void Main() {
+                \\    new TigerBeetle.Client(0, new [] {"3001"}).Dispose();
+                \\    Console.WriteLine("SUCCESS");
+                \\  }
+                \\}
+                \\EOF
+                \\dotnet run
+                ,
+            });
+        }
     }
 }
 
