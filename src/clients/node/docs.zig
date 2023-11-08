a22174400a355462d7cca716b720251314883ce6
Alex Kladov <aleksey.kladov@gmail.com>
2023-11-08 21:20:02 +0000

    node: fix sample instructions

--- src/clients/node/docs.zig
+++ src/clients/node/docs.zig
@@ -87,7 +87,10 @@ pub const NodeDocs = Docs{
     \\console.log("Import ok!");
     ,
 
-    .install_commands = "npm install tigerbeetle-node",
+    .install_commands =
+    \\npm install --prefix ../../
+    \\npm install
+    ,
     .build_commands =
     \\npm install typescript @types/node
     \\npx tsc --allowJs --noEmit main.js
