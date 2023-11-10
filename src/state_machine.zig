311e037d72134fc884a00fb16b4f09da05e08573
kprotty <kbutcher6200@gmail.com>
2023-11-09 23:00:32 -0600

    vsr: Demuxer type-safety and unit tests

--- src/state_machine.zig
+++ src/state_machine.zig
@@ -119,18 +119,14 @@ pub fn StateMachineType(
                 results: []CreateResult,
 
                 /// Create a Demuxer which can extract Results out of the reply bytes in-place.
-                pub fn init(reply: []align(@alignOf(CreateResult)) u8) Demuxer {
-                    return Demuxer{ .results = std.mem.bytesAsSlice(CreateResult, reply) };
+                pub fn init(reply: []CreateResult) Demuxer {
+                    return Demuxer{ .results = reply };
                 }
 
                 /// Returns a slice into the the original reply bytes with Results matching the
                 /// Event range (offset and size). Each subsequent call to demux() must have ranges
                 /// that are disjoint and increase monotonically.
-                pub fn decode(
-                    self: *Demuxer,
-                    event_offset: u32,
-                    event_size: u32,
-                ) []align(@alignOf(CreateResult)) u8 {
+                pub fn decode(self: *Demuxer, event_offset: u32, event_size: u32) []CreateResult {
                     // Count all results from the start of our slice which match the Event range,
                     // Updating the result.indexes to be relative to the Event range in the process.
                     const demuxed = for (self.results, 0..) |*result, i| {
@@ -141,7 +137,7 @@ pub fn StateMachineType(
 
                     // Consume the demuxed Results out of our local slice and return them as bytes.
                     defer self.results = self.results[demuxed..];
-                    return std.mem.sliceAsBytes(self.results[0..demuxed]);
+                    return self.results[0..demuxed];
                 }
             };
         }
@@ -2124,6 +2120,50 @@ test "create_transfers: balancing_debit/balancing_credit + pending" {
         \\ lookup_transfer T4 amount  5
         \\ commit lookup_transfers
     );
+}
+
+test "StateMachine: Demuxer" {
+    const StateMachine = StateMachineType(
+        @import("testing/storage.zig").Storage,
+        global_constants.state_machine_config,
+    );
+
+    var prng = std.rand.DefaultPrng.init(42);
+    inline for ([_]StateMachine.Operation{
+        .create_accounts,
+        .create_transfers,
+    }) |operation| {
+        try expect(StateMachine.batch_logical_allowed.get(operation));
+
+        const Result = StateMachine.Result(operation);
+        var results: [@divExact(global_constants.message_body_size_max, @sizeOf(Result))]Result = undefined;
+
+        for (0..100) |_| {
+            // Generate Result errors to Events at random.
+            var reply_len: u32 = 0;
+            for (0..results.len) |i| {
+                if (prng.random().boolean()) {
+                    results[reply_len] = .{ .index = @intCast(i), .result = .ok };
+                    reply_len += 1;
+                }
+            }
+
+            // Demux events of random strides from the generated results.
+            var demuxer = StateMachine.DemuxerType(operation).init(results[0..reply_len]);
+            const event_count: u32 = @intCast(@max(1, prng.random().uintAtMost(usize, results.len)));
+            var event_offset: u32 = 0;
+            while (event_offset < event_count) {
+                const event_size = @max(1, prng.random().uintAtMost(u32, event_count - event_offset));
+                const reply = demuxer.decode(event_offset, event_size);
+                defer event_offset += event_size;
+
+                for (reply) |*result| {
+                    try expectEqual(result.result, .ok);
+                    try expect(result.index < event_offset + event_size);
+                }
+            }
+        }
+    }
 }
 
 test "StateMachine: ref all decls" {
