matklad/tigerbeetle-filter-repo

vsr: store free set in the grid
Goal: get rid of storage_size_max

Currently, free set is stored as a part of superblock in a trailer. As a
result, the worst-case size of the free set affects the layout of the
data file. As the worst-case size is proprtional to the size of the
grid, this requires limiting the size of the data file up-front, using
`storage_size_max` parameter. This is bad operator experience -- it's
hard to predict which size would be enough, and failure to allocate
enough would be very costly to fix down the line.

The solution to make free set elastic and store it in the grid.

 ## Overview

FreeSetEncoded is the persistent component of the free set. It defines
defines the layout of the free set as stored in the grid between
checkpoints.

Free set is stored as a linked list of blocks containing EWAH-encoding
of a bitset of allocated blocks. The length of the linked list is
proportional to the degree of fragmentation, rather that to  size of the
data file. The common case is a single block.

The blocks holding free set itself are marked as free in the on-disk
encoding, because the number of blocks required to store the compressed
bitset becomes known only after encoding.

Linked list is a FIFO. While the blocks are written in the direct order,
they have to be read in the reverse order.

 ## Implementation notes

- Separate structs to hold persistent and in-memory components of the
  free set, as the shape of data is quite different between the two.

- FreeSet Trailer and associated messages are gone.

- `storage_size_max` is _not_ gone. While it's the entire purpose of the
  work, the actual removal of the parameter can happen separately.

- FreeSetEncoded field is stored as a field in the superblock alongside
  the free set. This isn't great, as it needs access to the grid, but
  the superblock is above the grid. As a result, the implementation has
  a bunch of fairly awkward paths like `set.grid.superblock.free_set`.

  I want to move both free se

src/lsm/schema.zig
@@ -56,9 +56,10 @@ pub const BlockType = enum(u8) {
     /// Unused; verifies that no block is written with a default 0 block type.
     reserved = 0,
 
-    manifest = 1,
-    index = 2,
-    data = 3,
+    free_set = 1,
+    manifest = 2,
+    index = 3,
+    data = 4,
 
     pub fn valid(block_type: BlockType) bool {
         _ = std.meta.intToEnum(BlockType, @intFromEnum(block_type)) catch return false;
@@ -340,6 +341,67 @@ pub const TableData = struct {
     }
 };
 
+pub const FreeSetNode = struct {
+    pub const Word = u64;
+
+    pub const Metadata = extern struct {
+        next_free_set_block_checksum: u128,
+        next_free_set_block_checksum_padding: u128 = 0,
+        next_free_set_block_address: u64,
+        reserved: [56]u8 = .{0} ** 56,
+
+        comptime {
+            assert(stdx.no_padding(Metadata));
+            assert(@sizeOf(Metadata) == vsr.Header.Block.metadata_size);
+        }
+    };
+
+    fn metadata(free_set_block: BlockPtrConst) *const Metadata {
+        const header = header_from_block(free_set_block);
+        assert(header.command == .block);
+        assert(header.block_type == .free_set);
+        assert(header.address > 0);
+        assert(header.snapshot == 0);
+
+        const header_metadata = std.mem.bytesAsValue(Metadata, &header.metadata_bytes);
+        assert(header_metadata.next_free_set_block_checksum_padding == 0);
+        assert(stdx.zeroed(&header_metadata.reserved));
+
+        if (header_metadata.next_free_set_block_address == 0) {
+            assert(header_metadata.next_free_set_block_checksum == 0);
+        }
+
+        assert((header.size - @sizeOf(vsr.Header)) % @sizeOf(Word) == 0);
+
+        return header_metadata;
+    }
+
+    pub fn assert_valid_header(free_set_block: BlockPtrConst) void {
+        _ = metadata(free_set_block);
+    }
+
+    pub fn next(free_set_block: BlockPtrConst) ?struct { checksum: u128, address: u64 } {
+        const header_metadata = metadata(free_set_block);
+
+        if (header_metadata.next_free_set_block_address == 0) {
+            assert(header_metadata.next_free_set_block_checksum == 0);
+            return null;
+        } else {
+            return .{
+                .checksum = header_metadata.next_free_set_block_checksum,
+                .address = header_metadata.next_free_set_block_address,
+            };
+        }
+    }
+
+    pub fn encoded_words(block: BlockPtrConst) []align(@alignOf(Word)) const u8 {
+        assert_valid_header(block);
+
+        const header = header_from_block(block);
+        return block[@sizeOf(vsr.Header)..header.size];
+    }
+};
+
 /// A Manifest block's body is an array of TableInfo entries.
 // TODO Store snapshot in header.
 pub const ManifestNode = struct {
@@ -429,6 +491,7 @@ pub const ManifestNode = struct {
         assert(header.command == .block);
         assert(header.block_type == .manifest);
         assert(header.address > 0);
+        assert(header.snapshot == 0);
 
         const header_metadata = std.mem.bytesAsValue(Metadata, &header.metadata_bytes);
         assert(header_metadata.entry_count > 0);

src/testing/cluster/storage_checker.zig
@@ -47,7 +47,6 @@ const Checkpoints = std.AutoHashMap(u64, Checkpoint);
 
 const CheckpointArea = enum {
     superblock_checkpoint,
-    superblock_free_set,
     superblock_client_sessions,
     client_replies,
     grid,
@@ -62,6 +61,7 @@ pub const StorageChecker = struct {
     checkpoints: Checkpoints,
 
     free_set: SuperBlock.FreeSet,
+    free_set_buffer: []align(@alignOf(u64)) u8,
 
     pub fn init(allocator: std.mem.Allocator) !StorageChecker {
         var compactions = Compactions.init(allocator);
@@ -73,14 +73,23 @@ pub const StorageChecker = struct {
         var free_set = try SuperBlock.FreeSet.init(allocator, vsr.superblock.grid_blocks_max);
         errdefer free_set.deinit(allocator);
 
+        var free_set_buffer = try allocator.alignedAlloc(
+            u8,
+            @alignOf(u64),
+            SuperBlock.FreeSet.encode_size_max(vsr.superblock.grid_blocks_max),
+        );
+        errdefer allocator.free(free_set);
+
         return StorageChecker{
             .compactions = compactions,
             .checkpoints = checkpoints,
             .free_set = free_set,
+            .free_set_buffer = free_set_buffer,
         };
     }
 
     pub fn deinit(checker: *StorageChecker, allocator: std.mem.Allocator) void {
+        allocator.free(checker.free_set_buffer);
         checker.free_set.deinit(allocator);
         checker.checkpoints.deinit();
         checker.compactions.deinit();
@@ -94,7 +103,6 @@ pub const StorageChecker = struct {
             superblock,
             std.enums.EnumSet(CheckpointArea).init(.{
                 .superblock_checkpoint = true,
-                .superblock_free_set = true,
                 .superblock_client_sessions = true,
                 .client_replies = !syncing,
                 .grid = !syncing,
@@ -111,7 +119,6 @@ pub const StorageChecker = struct {
             superblock,
             std.enums.EnumSet(CheckpointArea).init(.{
                 .superblock_checkpoint = true,
-                .superblock_free_set = true,
                 .superblock_client_sessions = true,
                 // The replica may have have already committed some addition prepares atop the
                 // checkpoint, so its client-replies zone will have mutated.
@@ -134,9 +141,6 @@ pub const StorageChecker = struct {
                     .superblock_checkpoint,
                     vsr.checksum(std.mem.asBytes(&superblock.working.vsr_state.checkpoint)),
                 );
-            }
-            if (areas.contains(.superblock_free_set)) {
-                checkpoint.put(.superblock_free_set, checksum_trailer(superblock, .free_set));
             }
             if (areas.contains(.superblock_client_sessions)) {
                 checkpoint.put(
@@ -229,13 +233,37 @@ pub const StorageChecker = struct {
     }
 
     fn checksum_grid(checker: *StorageChecker, superblock: *const SuperBlock) u128 {
-        const free_set_zone = vsr.SuperBlockTrailer.free_set.zone();
-        const free_set_offset = vsr.Zone.superblock.offset(free_set_zone.start_for_copy(0));
-        const free_set_size = superblock.working.free_set_size;
-        const free_set_buffer = superblock.storage.memory[free_set_offset..][0..free_set_size];
-        assert(vsr.checksum(free_set_buffer) == superblock.working.free_set_checksum);
+        const free_set_size = superblock.working.vsr_state.checkpoint.free_set_size;
 
-        checker.free_set.decode(@alignCast(free_set_buffer));
+        if (free_set_size > 0) {
+            // Read free set from the grid by manually following the linked list of blocks.
+            // Note that free set is written in direct order, and must be read backwards.
+            var free_set_block_address =
+                superblock.working.vsr_state.checkpoint.free_set_head_address;
+            var free_set_block_checksum =
+                superblock.working.vsr_state.checkpoint.free_set_head_checksum;
+            var free_set_cursor: usize = free_set_size;
+            while (true) {
+                const block = superblock.storage.grid_block(free_set_block_address).?;
+                assert(schema.header_from_block(block).checksum == free_set_block_checksum);
+
+                const encoded_words = schema.FreeSetNode.encoded_words(block);
+                free_set_cursor -= encoded_words.len;
+                stdx.copy_disjoint(
+                    .inexact,
+                    u8,
+                    checker.free_set_buffer[free_set_cursor..],
+                    encoded_words,
+                );
+
+                const next = schema.FreeSetNode.next(block) orelse break;
+                free_set_block_address = next.address;
+                free_set_block_checksum = next.checksum;
+            }
+            assert(free_set_cursor == 0);
+        }
+
+        checker.free_set.decode(checker.free_set_buffer[0..free_set_size]);
         defer checker.free_set.reset();
 
         var stream = vsr.ChecksumStream.init();

tigerbeetle/tigerbeetle

src/lsm/schema.zig
@@ -56,9 +56,10 @@ pub const BlockType = enum(u8) {
     /// Unused; verifies that no block is written with a default 0 block type.
     reserved = 0,
 
-    manifest = 1,
-    index = 2,
-    data = 3,
+    free_set = 1,
+    manifest = 2,
+    index = 3,
+    data = 4,
 
     pub fn valid(block_type: BlockType) bool {
         _ = std.meta.intToEnum(BlockType, @intFromEnum(block_type)) catch return false;
@@ -340,6 +341,68 @@ pub const TableData = struct {
     }
 };
 
+pub const FreeSetNode = struct {
+    pub const Word = u64;
+
+    pub const Metadata = extern struct {
+        previous_free_set_block_checksum: u128,
+        previous_free_set_block_checksum_padding: u128 = 0,
+        previous_free_set_block_address: u64,
+        reserved: [56]u8 = .{0} ** 56,
+
+        comptime {
+            assert(stdx.no_padding(Metadata));
+            assert(@sizeOf(Metadata) == vsr.Header.Block.metadata_size);
+        }
+    };
+
+    fn metadata(free_set_block: BlockPtrConst) *const Metadata {
+        const header = header_from_block(free_set_block);
+        assert(header.command == .block);
+        assert(header.block_type == .free_set);
+        assert(header.address > 0);
+        assert(header.snapshot == 0);
+
+        const header_metadata = std.mem.bytesAsValue(Metadata, &header.metadata_bytes);
+        assert(header_metadata.previous_free_set_block_checksum_padding == 0);
+        assert(stdx.zeroed(&header_metadata.reserved));
+
+        if (header_metadata.previous_free_set_block_address == 0) {
+            assert(header_metadata.previous_free_set_block_checksum == 0);
+        }
+
+        assert(header.size > @sizeOf(vsr.Header));
+        assert((header.size - @sizeOf(vsr.Header)) % @sizeOf(Word) == 0);
+
+        return header_metadata;
+    }
+
+    pub fn assert_valid_header(free_set_block: BlockPtrConst) void {
+        _ = metadata(free_set_block);
+    }
+
+    pub fn previous(free_set_block: BlockPtrConst) ?struct { checksum: u128, address: u64 } {
+        const header_metadata = metadata(free_set_block);
+
+        if (header_metadata.previous_free_set_block_address == 0) {
+            assert(header_metadata.previous_free_set_block_checksum == 0);
+            return null;
+        } else {
+            return .{
+                .checksum = header_metadata.previous_free_set_block_checksum,
+                .address = header_metadata.previous_free_set_block_address,
+            };
+        }
+    }
+
+    pub fn encoded_words(block: BlockPtrConst) []align(@alignOf(Word)) const u8 {
+        assert_valid_header(block);
+
+        const header = header_from_block(block);
+        return block[@sizeOf(vsr.Header)..header.size];
+    }
+};
+
 /// A Manifest block's body is an array of TableInfo entries.
 // TODO Store snapshot in header.
 pub const ManifestNode = struct {
@@ -429,6 +492,7 @@ pub const ManifestNode = struct {
         assert(header.command == .block);
         assert(header.block_type == .manifest);
         assert(header.address > 0);
+        assert(header.snapshot == 0);
 
         const header_metadata = std.mem.bytesAsValue(Metadata, &header.metadata_bytes);
         assert(header_metadata.entry_count > 0);

src/testing/cluster/storage_checker.zig
@@ -47,7 +47,6 @@ const Checkpoints = std.AutoHashMap(u64, Checkpoint);
 
 const CheckpointArea = enum {
     superblock_checkpoint,
-    superblock_free_set,
     superblock_client_sessions,
     client_replies,
     grid,
@@ -62,6 +61,7 @@ pub const StorageChecker = struct {
     checkpoints: Checkpoints,
 
     free_set: SuperBlock.FreeSet,
+    free_set_buffer: []align(@alignOf(u64)) u8,
 
     pub fn init(allocator: std.mem.Allocator) !StorageChecker {
         var compactions = Compactions.init(allocator);
@@ -73,14 +73,23 @@ pub const StorageChecker = struct {
         var free_set = try SuperBlock.FreeSet.init(allocator, vsr.superblock.grid_blocks_max);
         errdefer free_set.deinit(allocator);
 
+        var free_set_buffer = try allocator.alignedAlloc(
+            u8,
+            @alignOf(u64),
+            SuperBlock.FreeSet.encode_size_max(vsr.superblock.grid_blocks_max),
+        );
+        errdefer allocator.free(free_set);
+
         return StorageChecker{
             .compactions = compactions,
             .checkpoints = checkpoints,
             .free_set = free_set,
+            .free_set_buffer = free_set_buffer,
         };
     }
 
     pub fn deinit(checker: *StorageChecker, allocator: std.mem.Allocator) void {
+        allocator.free(checker.free_set_buffer);
         checker.free_set.deinit(allocator);
         checker.checkpoints.deinit();
         checker.compactions.deinit();
@@ -94,7 +103,6 @@ pub const StorageChecker = struct {
             superblock,
             std.enums.EnumSet(CheckpointArea).init(.{
                 .superblock_checkpoint = true,
-                .superblock_free_set = true,
                 .superblock_client_sessions = true,
                 .client_replies = !syncing,
                 .grid = !syncing,
@@ -111,7 +119,6 @@ pub const StorageChecker = struct {
             superblock,
             std.enums.EnumSet(CheckpointArea).init(.{
                 .superblock_checkpoint = true,
-                .superblock_free_set = true,
                 .superblock_client_sessions = true,
                 // The replica may have have already committed some addition prepares atop the
                 // checkpoint, so its client-replies zone will have mutated.
@@ -134,9 +141,6 @@ pub const StorageChecker = struct {
                     .superblock_checkpoint,
                     vsr.checksum(std.mem.asBytes(&superblock.working.vsr_state.checkpoint)),
                 );
-            }
-            if (areas.contains(.superblock_free_set)) {
-                checkpoint.put(.superblock_free_set, checksum_trailer(superblock, .free_set));
             }
             if (areas.contains(.superblock_client_sessions)) {
                 checkpoint.put(
@@ -229,13 +233,37 @@ pub const StorageChecker = struct {
     }
 
     fn checksum_grid(checker: *StorageChecker, superblock: *const SuperBlock) u128 {
-        const free_set_zone = vsr.SuperBlockTrailer.free_set.zone();
-        const free_set_offset = vsr.Zone.superblock.offset(free_set_zone.start_for_copy(0));
-        const free_set_size = superblock.working.free_set_size;
-        const free_set_buffer = superblock.storage.memory[free_set_offset..][0..free_set_size];
-        assert(vsr.checksum(free_set_buffer) == superblock.working.free_set_checksum);
+        const free_set_size = superblock.working.vsr_state.checkpoint.free_set_size;
 
-        checker.free_set.decode(@alignCast(free_set_buffer));
+        if (free_set_size > 0) {
+            // Read free set from the grid by manually following the linked list of blocks.
+            // Note that free set is written in direct order, and must be read backwards.
+            var free_set_block_address =
+                superblock.working.vsr_state.checkpoint.free_set_last_block_address;
+            var free_set_block_checksum =
+                superblock.working.vsr_state.checkpoint.free_set_last_block_checksum;
+            var free_set_cursor: usize = free_set_size;
+            while (true) {
+                const block = superblock.storage.grid_block(free_set_block_address).?;
+                assert(schema.header_from_block(block).checksum == free_set_block_checksum);
+
+                const encoded_words = schema.FreeSetNode.encoded_words(block);
+                free_set_cursor -= encoded_words.len;
+                stdx.copy_disjoint(
+                    .inexact,
+                    u8,
+                    checker.free_set_buffer[free_set_cursor..],
+                    encoded_words,
+                );
+
+                const previous = schema.FreeSetNode.previous(block) orelse break;
+                free_set_block_address = previous.address;
+                free_set_block_checksum = previous.checksum;
+            }
+            assert(free_set_cursor == 0);
+        }
+
+        checker.free_set.decode(checker.free_set_buffer[0..free_set_size]);
         defer checker.free_set.reset();
 
         var stream = vsr.ChecksumStream.init();