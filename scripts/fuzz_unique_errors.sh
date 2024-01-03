dfa07618356e4478e12ff00a2fe575349f748a95
Alex Kladov <aleksey.kladov@gmail.com>
2024-01-03 12:29:01 +0000

    ci: drop fuzz scripts
    I don't think we are using these. When we need it again, the relevant
    functionality can be implemented in Zig in `fuzz_tests.zig`.

--- scripts/fuzz_unique_errors.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-# After running ./fuzz_loop.sh use this script to produce a list of unique crashes.
-# As a heuristic, we look for the first line of the stacktrace that occurs inside tigerbeetle code.
-
-grep -m 1 'tigerbeetle/src' fuzz_* | sort -u -t':' -k2,2
