923dece6b4e1607ed57fc77374710825ee400dc4
Alex Kladov <aleksey.kladov@gmail.com>
2024-01-09 12:10:00 +0000

    lsm: remove slow redundant tests
    Segmented array was using the same code for both fuzzing and testing.
    Given that it takes 8 seconds to run, it seems important to reduce this
    duplciation. I can't fully make up my mind whether this should be a test
    or a fuzzer, but let's use the fuzzer for now, on the grounds that 8
    seconds is really to long for a test.
    
    Long term, I'd love us to maybe merge fuzzing and tests into a single
    entry point in build.zig, and instead make distinction between slow
    tests and fast tests, in which world this would clearly belong to slow
    tests.

--- src/lsm/segmented_array_fuzz.zig
+++ src/lsm/segmented_array_fuzz.zig
@@ -5,5 +5,5 @@ const segmented_array = @import("segmented_array.zig");
 
 pub fn main(fuzz_args: fuzz.FuzzArgs) !void {
     const allocator = fuzz.allocator;
-    try segmented_array.run_tests(allocator, fuzz_args.seed, .{ .verify = true });
+    try segmented_array.run_fuzz(allocator, fuzz_args.seed, .{ .verify = true });
 }
