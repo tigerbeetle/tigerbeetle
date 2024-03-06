37e99dbcf899a07710b93ef0213345a44e224122
batiati <rbatiati@gmail.com>
2024-03-06 09:20:19 +0200

    Reduce lsm_scans_max, since we don't use more than two scans.
    Let's rethink that during the Scan API work.

--- src/config.zig
+++ src/config.zig
@@ -143,10 +143,10 @@ const ConfigCluster = struct {
     lsm_snapshots_max: usize = 32,
     lsm_manifest_compact_extra_blocks: comptime_int = 1,
 
-    // Arbitrary value.
+    // Minimal value.
     // TODO(batiati): Maybe this constant should be derived from `grid_iops_read_max`,
     // since each scan can read from `lsm_levels` in parallel.
-    lsm_scans_max: comptime_int = 8,
+    lsm_scans_max: comptime_int = 2,
 
     /// The WAL requires at least two sectors of redundant headers — otherwise we could lose them all to
     /// a single torn write. A replica needs at least one valid redundant header to determine an
