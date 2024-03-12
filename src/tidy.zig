474925b5e54bf00d8c02d23e98279b91d8c09f81
batiati <rbatiati@gmail.com>
2024-03-12 10:54:53 -0300

    Tidy

--- src/tidy.zig
+++ src/tidy.zig
@@ -400,7 +400,6 @@ const naughty_list = [_][]const u8{
     "state_machine.zig",
     "state_machine/auditor.zig",
     "state_machine/workload.zig",
-    "testing/aof.zig",
     "testing/cluster/network.zig",
     "testing/cluster/state_checker.zig",
     "testing/hash_log.zig",
