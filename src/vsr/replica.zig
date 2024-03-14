755f0e7b52e7f0438c6f64e9a4467bd791933458
sentientwaffle <sentientwaffle@gmail.com>
2024-03-14 11:01:41 -0700

    VSR: Reset upgrade_release after finishing state sync
    If `upgrade_release` is non-null immediately after state sync, then:
    - The replica just synced a single checkpoint. (We do not assert this via
      sync_op_min/sync_op_max, since we may have synced a single checkpoint multiple
      times.)
    - An `operation=upgrade` was committed during the last bar of the checkpoint we
      just synced.
    - But at least one op (+1) of the last bar was *not* an `operation=upgrade`.
      (If all of the last bar was `operation=upgrade`, then the new superblock's
      release would have increased.
    - We were very close to reaching the checkpoint via WAL replay – close enough to
      have executed at least one (but not all) of the upgrades in that last bar.

--- src/vsr/replica.zig
+++ src/vsr/replica.zig
@@ -8471,6 +8471,7 @@ pub fn ReplicaType(
                 &stage.checkpoint_state,
             ));
 
+            const commit_min_previous = self.commit_min;
             self.commit_min = self.superblock.working.vsr_state.checkpoint.header.op;
 
             if (self.release < self.superblock.working.vsr_state.checkpoint.release) {
@@ -8479,7 +8480,26 @@ pub fn ReplicaType(
                 return;
             }
 
-            assert(self.upgrade_release == null);
+            if (self.upgrade_release) |_| {
+                // If `upgrade_release` is non-null, then:
+                // - The replica just synced a single checkpoint. (We do not assert this via
+                //   sync_op_min/sync_op_max, since we may have synced a single checkpoint multiple
+                //   times.)
+                // - An `operation=upgrade` was committed during the last bar of the checkpoint we
+                //   just synced.
+                // - But at least one op (+1) of the last bar was *not* an `operation=upgrade`.
+                //   (If all of the last bar was `operation=upgrade`, then the new superblock's
+                //   release would have increased.
+                // - We were very close to reaching the checkpoint via WAL replay – close enough to
+                //   have executed at least one (but not all) of the upgrades in that last bar.
+                // As we replay the bar immediately after this checkpoint, we will set
+                // `upgrade_release` "again", so we reset it now to keep the assertions simple.
+                assert(commit_min_previous > self.op_checkpoint() + 1);
+                assert(self.superblock.working.vsr_state.checkpoint.header.operation != .upgrade);
+
+                self.upgrade_release = null;
+            }
+
             assert(self.commit_min == self.op_checkpoint());
 
             // The head op must be in the Journal and there should not be a break between the
