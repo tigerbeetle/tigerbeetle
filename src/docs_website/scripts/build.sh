de1b8cd804615fb2bfaeebffb7be4fbc14b050e2
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-03-28 14:06:18 -0400

    docs: copy tiger style folder over during build process

--- src/docs_website/scripts/build.sh
+++ src/docs_website/scripts/build.sh
@@ -38,6 +38,9 @@ for page in $(ls pages/*.md); do
     fi
 done
 
+# The link validator doesn't like symlinks so we'll copy the file over instead.
+cp -f pages/TIGER_STYLE.md pages/about/tiger-style.md
+
 # Validate links
 npx remark --use remark-validate-links --frail pages
 
