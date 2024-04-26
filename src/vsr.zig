593bec2650bffde039fbc23c197c75421f83080a
sentientwaffle <sentientwaffle@gmail.com>
2024-04-26 09:12:46 -0700

    CLI: Print memory usage even more accurately
    This still does not perfectly reflect RSS, since it doesn't know about pages.

--- src/vsr.zig
+++ src/vsr.zig
@@ -58,6 +58,7 @@ pub const Header = @import("vsr/message_header.zig").Header;
 pub const FreeSet = @import("vsr/free_set.zig").FreeSet;
 pub const CheckpointTrailerType = @import("vsr/checkpoint_trailer.zig").CheckpointTrailerType;
 pub const GridScrubberType = @import("vsr/grid_scrubber.zig").GridScrubberType;
+pub const CountingAllocator = @import("counting_allocator.zig");
 
 /// The version of our Viewstamped Replication protocol in use, including customizations.
 /// For backwards compatibility through breaking changes (e.g. upgrading checksums/ciphers).
