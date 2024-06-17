8e154920a7619d3392da21a51573fc1816229c82
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-06-17 12:19:57 -0400

    treat other client output as script failures

--- tigerlings/tb_function.sh
+++ tigerlings/tb_function.sh
@@ -9,6 +9,11 @@ function tb() {
     output=$(./tigerbeetle repl --cluster=0 --addresses=3000 --command="$1" 2>&1 | sed 's/^/[Client] /')
 
     echo "$output"
-    ./tigerbeetle repl --cluster=0 --addresses=3000 --command="$1"
-}
+
+    # For efficiency, TigerBeetle only returns error responses.
+    # These will be printed to the console when we use the REPL.
+    # For the purposes of these exercises, we want to treat any error responses as script failures.
+    if [[ $output != "[Client] info(message_bus): connected to replica 0" ]]; then
+        exit 1
+    fi
 }
