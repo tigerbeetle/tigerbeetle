cf85f63a16cc1cdb28f180b92b9d061f9e048d3b
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-06-17 16:35:43 -0400

    revert patches and rm data file when test script exits

--- tigerlings/test.sh
+++ tigerlings/test.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+function remove_data_file() {
+    if [ -f "0_0.tigerbeetle" ]; then
+        rm "0_0.tigerbeetle"
+    fi
+}
+
+
+function revert_patches() {
+    for patch_file in ./patches/*.patch; do
+        # Revert the patch using git
+        git apply -R "$patch_file"
+    done
+}
+
+remove_data_file
+
+trap 'revert_patches; remove_data_file' EXIT SIGHUP SIGINT SIGQUIT SIGTERM SIGKILL
+
 # Iterate over each patch file in the directory
 for patch_file in ./patches/*.patch; do
     # Apply the patch using git
@@ -14,8 +32,3 @@ if [ $? -ne 0 ]; then
     echo "Uh oh, there is a problem in one of the exercises!"
     exit 1
 fi
-
-for patch_file in ./patches/*.patch; do
-    # Revert the patch using git
-    git apply -R "$patch_file"
-done
