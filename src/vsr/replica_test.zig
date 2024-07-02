6832e4b0c1ea104797b8da396f33d02a334d2dc8
Alex Kladov <aleksey.kladov@gmail.com>
2024-07-02 16:01:29 +0100

    test: fix flaky replica tests
    In this test, the replica can either state-sync or fast-forward the log.
    Right now, the choice between the two depends on timing:
    `on_repair_sync` timeout might force the replica to state sync.
    
    Fix this by completely stopping the replica, instead of just
    partitioning it.
    
    One alternative I've tried is preventing the replica from learning about
    the new checkpoint by dropping commits and pings, but that _also_
    prevents log forwariding from working.

--- src/vsr/replica_test.zig
+++ src/vsr/replica_test.zig
@@ -443,7 +443,7 @@ test "Cluster: repair: partition 2-1, then backup fast-forward 1 checkpoint" {
     try expectEqual(t.replica(.R_).commit(), 3);
 
     var r_lag = t.replica(.B2);
-    r_lag.drop_all(.__, .bidirectional);
+    r_lag.stop();
 
     // Commit enough ops to checkpoint once, and then nearly wrap around, leaving enough slack
     // that the lagging backup can repair (without state sync).
@@ -452,11 +452,11 @@ test "Cluster: repair: partition 2-1, then backup fast-forward 1 checkpoint" {
     try expectEqual(t.replica(.A0).op_checkpoint(), checkpoint_1);
     try expectEqual(t.replica(.B1).op_checkpoint(), checkpoint_1);
 
+    try r_lag.open();
     try expectEqual(r_lag.status(), .normal);
     try expectEqual(r_lag.op_checkpoint(), 0);
 
     // Allow repair, but ensure that state sync doesn't run.
-    r_lag.pass_all(.__, .bidirectional);
     r_lag.drop(.__, .bidirectional, .sync_checkpoint);
     t.run();
 
