93d97b7b8f685f285b03b4a3d031692ff14f41e9
Brian Anderson <andersrb@gmail.com>
2024-07-09 22:40:53 +0000

    tidy: level_data_iterator.zig

--- src/tidy.zig
+++ src/tidy.zig
@@ -383,7 +383,6 @@ fn parse_multiline_string(line: []const u8) ?[]const u8 {
 const naughty_list = [_][]const u8{
     "lsm/forest_fuzz.zig",
     "lsm/groove.zig",
-    "lsm/level_data_iterator.zig",
     "lsm/manifest_level.zig",
     "lsm/segmented_array_benchmark.zig",
     "lsm/segmented_array.zig",
