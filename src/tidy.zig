9bbb3591140ce1bde63049a2acd738e21172569e
Federico Lorenzi <florenzi@gmail.com>
2024-07-09 11:59:39 +0200

    tidy: tidy {config,constants}.zig

--- src/tidy.zig
+++ src/tidy.zig
@@ -384,8 +384,6 @@ fn parse_multiline_string(line: []const u8) ?[]const u8 {
 const naughty_list = [_][]const u8{
     "clients/java/docs.zig", // Contains long XML.
     "java_bindings.zig", // Contains Javadoc links.
-    "config.zig",
-    "constants.zig",
     "lsm/binary_search.zig",
     "lsm/binary_search_benchmark.zig",
     "lsm/forest_fuzz.zig",
