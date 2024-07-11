tigerbeetle/tigerbeetle

commit 4424058f571f96a91f072e6c08190445271db4c6

LSM/ManifestLevel: Prefer compacting old tables
Break compaction selection ties via snapshot_min.

When selecting the "least overlap" table for compaction, if multiple level-A overlap with the same number of level-B tables, pick the oldest (least-recently-created) table as a tiebreaker.

Rationale: Old tables are an approximation for cold tables. (Keep hot tables in upper levels, move cold tables down.)

src/lsm/manifest_level.zig
@@ -545,8 +545,9 @@ pub fn ManifestLevelType(
         /// is invoked and B is the other level), finds a table in Level A that
         /// overlaps with the least number of tables in Level B.
         ///
-        /// * Exits early if it finds a table that doesn't overlap with any
-        ///   tables in the second level.
+        /// As a tiebreaker between multiple tables with the same overlap, prefer to compact the
+        /// oldest (least-recently-created) table. This is primarily for the benefit of trees with
+        /// long runs of strictly monotonic key updates.
         pub fn table_with_least_overlap(
             level_a: *const Self,
             level_b: *const Self,
@@ -576,7 +577,11 @@ pub fn ManifestLevelType(
                 ) orelse continue;
                 assert(range.tables.count() <= max_overlapping_tables);
 
-                if (optimal == null or range.tables.count() < optimal.?.range.tables.count()) {
+                if (optimal == null or
+                    range.tables.count() < optimal.?.range.tables.count() or
+                    (range.tables.count() == optimal.?.range.tables.count() and
+                    table.snapshot_min < optimal.?.table.table_info.snapshot_min))
+                {
                     optimal = LeastOverlapTable{
                         .table = TableInfoReference{
                             .table_info = table,
@@ -585,8 +590,6 @@ pub fn ManifestLevelType(
                         .range = range,
                     };
                 }
-                // If the table can be moved directly between levels then that is already optimal.
-                if (optimal.?.range.tables.empty()) break;
             }
             assert(iterations > 0);
             assert(iterations == level_a.table_count_visible or

commit dc86ff7fbf3586aab34d22c665719d287baa1092

LSM/ManifestLevel: Revert "prefer compacting old tables"
This reverts commit 4424058f571f96a91f072e6c08190445271db4c6.

The tree fuzzer found this failing seed, which taught me something interesting about space amplification for trees with random key updates!:

```
zig build -Drelease fuzz -- lsm_tree 4088940554726182876
```

The failure is introduced by https://github.com/tigerbeetle/tigerbeetle/pull/2070/commits/4424058f571f96a91f072e6c08190445271db4c6.
It panics on the `assert(manifest_level.table_count_visible <= table_count_visible_max);` at the end of a bar.
We run out of tables -- level 6 is totally full of tables. (And notably, this isn't a case that even the greediest level-0-coalescing helps with!)
(The issue isn't actually introduced by that commit, it just happens to make it easier to hit.)

What happens is:

- The fuzzer inserts keys randomly across the entire key range:
    - 50% of the inserts the key is less than 8096.
    - 50% of the inserts the key is greater than 8096.
    - (Effectively this means that there is a very long tail of updates).
- Compacting immutable into level-0 produces at most `growth_factor` tables. (4 for the fuzzer).
- All but the last output table are full. Often, the last table will be small, since it just has the values that didn't fit in the other tables.
- On level 0, that last output table is also the highest (coldest) keys.
- When the table is small and holds cold keys (>8096), the chances are good that its key range is quite narrow -- and therefore that it will be selected for compaction due to least overlap (often zero overlap, triggering move-table to level 1).
- This cycle repeats -- since it is a small table, its key range is often quite narrow, so it has no overlap, so it makes its way down the tree via moves, not merges.
    - And thus, tiny tables accumulate on L6.
- This was exacerbated by the commit linked above.

src/lsm/manifest_level.zig
@@ -545,9 +545,8 @@ pub fn ManifestLevelType(
         /// is invoked and B is the other level), finds a table in Level A that
         /// overlaps with the least number of tables in Level B.
         ///
-        /// As a tiebreaker between multiple tables with the same overlap, prefer to compact the
-        /// oldest (least-recently-created) table. This is primarily for the benefit of trees with
-        /// long runs of strictly monotonic key updates.
+        /// * Exits early if it finds a table that doesn't overlap with any
+        ///   tables in the second level.
         pub fn table_with_least_overlap(
             level_a: *const Self,
             level_b: *const Self,
@@ -577,11 +576,7 @@ pub fn ManifestLevelType(
                 ) orelse continue;
                 assert(range.tables.count() <= max_overlapping_tables);
 
-                if (optimal == null or
-                    range.tables.count() < optimal.?.range.tables.count() or
-                    (range.tables.count() == optimal.?.range.tables.count() and
-                    table.snapshot_min < optimal.?.table.table_info.snapshot_min))
-                {
+                if (optimal == null or range.tables.count() < optimal.?.range.tables.count()) {
                     optimal = LeastOverlapTable{
                         .table = TableInfoReference{
                             .table_info = table,
@@ -590,6 +585,8 @@ pub fn ManifestLevelType(
                         .range = range,
                     };
                 }
+                // If the table can be moved directly between levels then that is already optimal.
+                if (optimal.?.range.tables.empty()) break;
             }
             assert(iterations > 0);
             assert(iterations == level_a.table_count_visible or