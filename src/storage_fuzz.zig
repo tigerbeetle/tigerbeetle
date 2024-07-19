8c71011f4653ba36bfa132e0aaf29d0e05a97c3e
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2024-07-19 10:57:39 +0200

    Make storage fuzzer more effective

--- src/storage_fuzz.zig
+++ src/storage_fuzz.zig
@@ -17,80 +17,83 @@ pub fn main(args: fuzz.FuzzArgs) !void {
         .client_replies,
     };
 
+    const sector_size = constants.sector_size;
     const sector_count = 64;
-    const storage_size = sector_count * constants.sector_size;
+    const storage_size = sector_count * sector_size;
     const iterations = args.events_max orelse 10_000;
 
-    const failed_sector_cluster_count = 7;
-    const failed_sector_cluster_minimum_length = 1;
-    const failed_sector_cluster_maximum_length = 4;
-
-    var data_prng = std.rand.DefaultPrng.init(args.seed);
-
-    var fault_map = std.bit_set.ArrayBitSet(u8, sector_count).initEmpty();
-
-    for (0..failed_sector_cluster_count) |_| {
-        const start = data_prng.random().intRangeLessThan(
-            usize,
-            0,
-            sector_count - failed_sector_cluster_maximum_length,
-        );
-        const end = start + data_prng.random().intRangeAtMost(
-            usize,
-            failed_sector_cluster_minimum_length,
-            failed_sector_cluster_maximum_length,
-        );
-
-        fault_map.setRangeValue(.{ .start = start, .end = end }, true);
-    }
-
-    var storage_data_written: [storage_size]u8 align(constants.sector_size) = undefined;
-    @memset(&storage_data_written, 0);
-
-    for (0..sector_count) |sector| {
-        if (!fault_map.isSet(sector)) {
-            data_prng.fill(
-                storage_data_written[sector * constants.sector_size ..][0..constants.sector_size],
-            );
-        }
-    }
-
-    var storage_data_stored: [storage_size]u8 align(constants.sector_size) = undefined;
-    @memset(&storage_data_stored, 0);
-    var storage_data_read: [storage_size]u8 align(constants.sector_size) = undefined;
-    @memset(&storage_data_read, 0);
-
-    var io = IO.init(&.{
-        .{
-            .buffer = &storage_data_stored,
-            .fault_map = &fault_map.masks,
-        },
-    }, .{
-        .seed = args.seed,
-        .larger_than_logical_sector_read_fault_probability = 10,
-    });
-
-    var storage = try Storage.init(&io, 0);
-
-    var write_completion: Storage.Write = undefined;
-
-    for (zones) |zone| {
-        storage.write_sectors(
-            struct {
-                fn callback(completion: *Storage.Write) void {
-                    _ = completion;
-                }
-            }.callback,
-            &write_completion,
-            storage_data_written[zone.start()..][0..zone.size().?],
-            zone,
-            0,
-        );
-
-        storage.tick();
-    }
+    var prng = std.rand.DefaultPrng.init(args.seed);
+    const random = prng.random();
 
     for (0..iterations) |_| {
+        var fault_map = std.bit_set.ArrayBitSet(u8, sector_count).initEmpty();
+
+        const failed_sector_cluster_count = random.intRangeAtMost(usize, 1, 10);
+        const failed_sector_cluster_minimum_length = random.intRangeAtMost(usize, 1, 3);
+        const failed_sector_cluster_maximum_length =
+            failed_sector_cluster_minimum_length + random.intRangeAtMost(usize, 1, 3);
+
+        for (0..failed_sector_cluster_count) |_| {
+            const start = random.intRangeLessThan(
+                usize,
+                0,
+                sector_count - failed_sector_cluster_maximum_length,
+            );
+            const end = start + random.intRangeAtMost(
+                usize,
+                failed_sector_cluster_minimum_length,
+                failed_sector_cluster_maximum_length,
+            );
+
+            fault_map.setRangeValue(.{ .start = start, .end = @min(end, sector_count) }, true);
+        }
+
+        var storage_data_written: [storage_size]u8 align(sector_size) = undefined;
+        @memset(&storage_data_written, 0);
+
+        for (0..sector_count) |sector| {
+            if (!fault_map.isSet(sector)) {
+                prng.fill(
+                    storage_data_written[sector * sector_size ..][0..sector_size],
+                );
+            }
+        }
+
+        var storage_data_stored: [storage_size]u8 align(sector_size) = undefined;
+        @memset(&storage_data_stored, 0);
+        var storage_data_read: [storage_size]u8 align(sector_size) = undefined;
+        @memset(&storage_data_read, 0);
+
+        var io = IO.init(&.{
+            .{
+                .buffer = &storage_data_stored,
+                .fault_map = &fault_map.masks,
+            },
+        }, .{
+            .seed = args.seed,
+            .larger_than_logical_sector_read_fault_probability = 10,
+        });
+
+        var storage = try Storage.init(&io, 0);
+
+        var write_completion: Storage.Write = undefined;
+
+        for (zones) |zone| {
+            storage.write_sectors(
+                struct {
+                    fn callback(completion: *Storage.Write) void {
+                        _ = completion;
+                    }
+                }.callback,
+                &write_completion,
+                storage_data_written[zone.start()..][0..zone.size().?],
+                zone,
+                0,
+            );
+
+            storage.tick();
+        }
+
         for (zones) |zone| {
             const ReadDetail = struct {
                 offset_in_zone: u64,
@@ -99,28 +102,28 @@ pub fn main(args: fuzz.FuzzArgs) !void {
 
             var read_details: [32]ReadDetail = undefined;
 
-            const zone_sector_count: u64 = @divExact(zone.size().?, constants.sector_size);
+            const zone_sector_count: u64 = @divExact(zone.size().?, sector_size);
             assert(zone_sector_count <= read_details.len);
 
             var index: u64 = 0;
             var read_detail_length: usize = 0;
 
             while (index < zone_sector_count) : (read_detail_length += 1) {
-                const n_sectors = data_prng.random().intRangeAtMost(
+                const n_sectors = random.intRangeAtMost(
                     u64,
                     1,
                     @min(4, zone_sector_count - index),
                 );
 
                 read_details[read_detail_length] = .{
-                    .offset_in_zone = index * constants.sector_size,
-                    .read_length = n_sectors * constants.sector_size,
+                    .offset_in_zone = index * sector_size,
+                    .read_length = n_sectors * sector_size,
                 };
 
                 index += n_sectors;
             }
 
-            data_prng.random().shuffle(ReadDetail, read_details[0..read_detail_length]);
+            random.shuffle(ReadDetail, read_details[0..read_detail_length]);
 
             for (read_details[0..read_detail_length]) |read_detail| {
                 const sector_offset = read_detail.offset_in_zone;
@@ -144,21 +147,21 @@ pub fn main(args: fuzz.FuzzArgs) !void {
                 storage.tick();
             }
         }
-    }
 
-    for (zones) |zone| {
-        const start = zone.start();
-        const end = start + zone.size().?;
+        for (zones) |zone| {
+            const start = zone.start();
+            const end = start + zone.size().?;
 
-        try std.testing.expectEqualSlices(
-            u8,
-            storage_data_written[start..end],
-            storage_data_stored[start..end],
-        );
-        try std.testing.expectEqualSlices(
-            u8,
-            storage_data_stored[start..end],
-            storage_data_read[start..end],
-        );
+            try std.testing.expectEqualSlices(
+                u8,
+                storage_data_written[start..end],
+                storage_data_stored[start..end],
+            );
+            try std.testing.expectEqualSlices(
+                u8,
+                storage_data_stored[start..end],
+                storage_data_read[start..end],
+            );
+        }
     }
 }
