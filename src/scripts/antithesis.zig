69feeccfc15df674a3a417c8094a2277a36ffec7
Oskar Wickström <oskar@wickstrom.tech>
2024-09-11 22:33:48 +0200

    test: push images from script

--- src/scripts/antithesis.zig
+++ src/scripts/antithesis.zig
@@ -14,12 +14,13 @@ pub const CLIArgs = struct {
     tag: []const u8,
 };
 
-pub fn main(shell: *Shell, gpa: std.mem.Allocator, cli_args: CLIArgs) !void {
+const Image = enum { config, workload, replica };
+
+pub fn main(shell: *Shell, cli_args: CLIArgs) !void {
     // if (builtin.os.tag == .windows) {
     //     return error.NotSupported;
     // }
     //
-    _ = gpa;
 
     assert(try shell.exec_status_ok("docker --version", .{}));
 
@@ -45,70 +46,71 @@ pub fn main(shell: *Shell, gpa: std.mem.Allocator, cli_args: CLIArgs) !void {
         try shell.exec("mvn clean package --batch-mode --quiet", .{});
     }
 
-    try build_images(shell, tag);
+    for (std.enums.values(Image)) |image| {
+        try build_image(shell, image, tag);
+        try push_image(shell, image, tag);
+    }
 }
 
-fn build_images(shell: *Shell, tag: []const u8) !void {
-    try shell.exec_options(.{
-        .echo = true,
-        .stdin_slice =
-        \\FROM debian:stable-slim
-        \\
-        \\ARG TAG
-        \\
-        \\RUN mkdir -p /volumes/database
-        \\RUN echo "TAG=${TAG}" > /.env
-        \\
-        \\FROM scratch
-        \\
-        \\COPY src/testing/systest/config/docker-compose.yaml docker-compose.yaml
-        \\COPY --from=0 /.env .env
-        \\COPY --from=0 /volumes/database /volumes/database
-        ,
-    },
+fn build_image(shell: *Shell, image: Image, tag: []const u8) !void {
+    try shell.exec_options(.{ .echo = true, .stdin_slice = @field(dockerfiles, @tagName(.workload)) },
         \\docker build 
         \\  --file - .
         \\  --build-arg TAG={tag}
-        \\  --tag=config:{tag}
-    , .{ .tag = tag });
-
-    try shell.exec_options(.{
-        .echo = true,
-        .stdin_slice =
-        \\FROM debian:stable-slim
-        \\WORKDIR /opt/tigerbeetle
-        \\
-        \\COPY zig-out/bin/tigerbeetle ./tigerbeetle
-        \\COPY src/testing/systest/scripts/run-tigerbeetle.sh ./run-tigerbeetle.sh
-        \\
-        \\ENTRYPOINT ["./run-tigerbeetle.sh"]
-        ,
-    },
-        \\docker build 
-        \\  --file - .
-        \\  --tag=replica:{tag}
-    , .{ .tag = tag });
-
-    try shell.exec_options(.{
-        .echo = true,
-        .stdin_slice =
-        \\FROM debian:stable-slim
-        \\WORKDIR /opt/tigerbeetle
-        \\
-        \\ENV DEBIAN_FRONTEND=noninteractive
-        \\
-        \\RUN apt update && apt install -y --no-install-recommends wget ca-certificates
-        \\
-        \\RUN wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb && dpkg -i jdk-21_linux-x64_bin.deb && rm jdk-21_linux-x64_bin.deb
-        \\
-        \\COPY src/clients/java/target/tigerbeetle-java-0.0.1-SNAPSHOT.jar ./tigerbeetle-java-0.0.1-SNAPSHOT.jar
-        \\COPY src/testing/systest/workload/target/workload-0.0.1-SNAPSHOT.jar ./workload-0.0.1-SNAPSHOT.jar
-        \\
-        \\ENTRYPOINT ["java", "-ea", "-cp", "workload-0.0.1-SNAPSHOT.jar:tigerbeetle-java-0.0.1-SNAPSHOT.jar", "Main"]
-        ,
-    },
-        \\docker build 
-        \\  --file - .
-        \\  --tag=workload:{tag}
-    , .{ .tag = tag });
+        \\  --tag={image}:{tag}
+    , .{ .image = @tagName(image), .tag = tag });
 }
+
+fn push_image(shell: *Shell, image: Image, tag: []const u8) !void {
+    const url_prefix = "us-central1-docker.pkg.dev/molten-verve-216720/tigerbeetle-repository";
+    try shell.exec(
+        "docker tag {image}:{tag} {url_prefix}/{image}:{tag}",
+        .{ .image = @tagName(image), .tag = tag, .url_prefix = url_prefix },
+    );
+    try shell.exec(
+        "docker push {url_prefix}/{image}:{tag}",
+        .{ .image = @tagName(image), .tag = tag, .url_prefix = url_prefix },
+    );
+}
+
+const dockerfiles = .{
+    .config =
+    \\FROM debian:stable-slim
+    \\
+    \\ARG TAG
+    \\
+    \\RUN mkdir -p /volumes/database
+    \\RUN echo "TAG=${TAG}" > /.env
+    \\
+    \\FROM scratch
+    \\
+    \\COPY src/testing/systest/config/docker-compose.yaml docker-compose.yaml
+    \\COPY --from=0 /.env .env
+    \\COPY --from=0 /volumes/database /volumes/database
+    ,
+    .workload =
+    \\FROM debian:stable-slim
+    \\WORKDIR /opt/tigerbeetle
+    \\
+    \\ENV DEBIAN_FRONTEND=noninteractive
+    \\
+    \\RUN apt update && apt install -y --no-install-recommends wget ca-certificates
+    \\
+    \\RUN wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb && dpkg -i jdk-21_linux-x64_bin.deb && rm jdk-21_linux-x64_bin.deb
+    \\
+    \\COPY src/clients/java/target/tigerbeetle-java-0.0.1-SNAPSHOT.jar ./tigerbeetle-java-0.0.1-SNAPSHOT.jar
+    \\COPY src/testing/systest/workload/target/workload-0.0.1-SNAPSHOT.jar ./workload-0.0.1-SNAPSHOT.jar
+    \\
+    \\ENTRYPOINT ["java", "-ea", "-cp", "workload-0.0.1-SNAPSHOT.jar:tigerbeetle-java-0.0.1-SNAPSHOT.jar", "Main"]
+    ,
+
+    .replica =
+    \\FROM debian:stable-slim
+    \\WORKDIR /opt/tigerbeetle
+    \\
+    \\COPY zig-out/bin/tigerbeetle ./tigerbeetle
+    \\COPY src/testing/systest/scripts/run-tigerbeetle.sh ./run-tigerbeetle.sh
+    \\
+    \\ENTRYPOINT ["./run-tigerbeetle.sh"]
+    ,
+};
