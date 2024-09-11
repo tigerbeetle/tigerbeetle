4d2f3f801db2db131cff990294ad3ed170ea7fef
Oskar Wickström <oskar@wickstrom.tech>
2024-09-11 15:41:28 +0200

    test: embed dockerfiles in script

--- src/scripts/antithesis.zig
+++ src/scripts/antithesis.zig
@@ -45,22 +45,70 @@ pub fn main(shell: *Shell, gpa: std.mem.Allocator, cli_args: CLIArgs) !void {
         try shell.exec("mvn clean package --batch-mode --quiet", .{});
     }
 
-    try shell.exec(
-        \\docker build --file=./src/testing/systest/configuration.Dockerfile
+    try build_images(shell, tag);
+}
+
+fn build_images(shell: *Shell, tag: []const u8) !void {
+    try shell.exec_options(.{
+        .echo = true,
+        .stdin_slice =
+        \\FROM debian:stable-slim
+        \\
+        \\ARG TAG
+        \\
+        \\RUN mkdir -p /volumes/database
+        \\RUN echo "TAG=${TAG}" > /.env
+        \\
+        \\FROM scratch
+        \\
+        \\COPY src/testing/systest/config/docker-compose.yaml docker-compose.yaml
+        \\COPY --from=0 /.env .env
+        \\COPY --from=0 /volumes/database /volumes/database
+        ,
+    },
+        \\docker build 
+        \\  --file - .
         \\  --build-arg TAG={tag}
-        \\ --tag=config:{tag}
-        \\ .
+        \\  --tag=config:{tag}
     , .{ .tag = tag });
 
-    try shell.exec(
-        \\docker build --file=./src/testing/systest/replica.Dockerfile
-        \\ --tag=config:{tag}
-        \\ .
+    try shell.exec_options(.{
+        .echo = true,
+        .stdin_slice =
+        \\FROM debian:stable-slim
+        \\WORKDIR /opt/tigerbeetle
+        \\
+        \\COPY zig-out/bin/tigerbeetle ./tigerbeetle
+        \\COPY src/testing/systest/scripts/run-tigerbeetle.sh ./run-tigerbeetle.sh
+        \\
+        \\ENTRYPOINT ["./run-tigerbeetle.sh"]
+        ,
+    },
+        \\docker build 
+        \\  --file - .
+        \\  --tag=replica:{tag}
     , .{ .tag = tag });
 
-    try shell.exec(
-        \\docker build --file=./src/testing/systest/workload.Dockerfile
-        \\ --tag=config:{tag}
-        \\ .
+    try shell.exec_options(.{
+        .echo = true,
+        .stdin_slice =
+        \\FROM debian:stable-slim
+        \\WORKDIR /opt/tigerbeetle
+        \\
+        \\ENV DEBIAN_FRONTEND=noninteractive
+        \\
+        \\RUN apt update && apt install -y --no-install-recommends wget ca-certificates
+        \\
+        \\RUN wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb && dpkg -i jdk-21_linux-x64_bin.deb && rm jdk-21_linux-x64_bin.deb
+        \\
+        \\COPY src/clients/java/target/tigerbeetle-java-0.0.1-SNAPSHOT.jar ./tigerbeetle-java-0.0.1-SNAPSHOT.jar
+        \\COPY src/testing/systest/workload/target/workload-0.0.1-SNAPSHOT.jar ./workload-0.0.1-SNAPSHOT.jar
+        \\
+        \\ENTRYPOINT ["java", "-ea", "-cp", "workload-0.0.1-SNAPSHOT.jar:tigerbeetle-java-0.0.1-SNAPSHOT.jar", "Main"]
+        ,
+    },
+        \\docker build 
+        \\  --file - .
+        \\  --tag=workload:{tag}
     , .{ .tag = tag });
 }
