dbd2b84e3cb4f49dfec09be60bfba7ab171dd8ee
Oskar Wickström <oskar@wickstrom.tech>
2024-09-25 14:47:52 +0200

    test: run systest as integration smoke test

--- src/testing/systest/supervisor/logged_process.zig
+++ src/testing/systest/supervisor/logged_process.zig
@@ -1,89 +1,58 @@
+//! Runs a subprocess inheriting its stderr output. The processes can terminate by its own or be
+//! actively terminated with a SIGKILL.
+//!
+//! We use this to run a cluster of replicas and the workload.
+//!
+//! NOTE: some duplication exists between this and `tmp_tigerbeetle.zig` that could perhaps be
+//! unified.
 const std = @import("std");
 const builtin = @import("builtin");
-const log = std.log.default;
+
+const log = std.log.scoped(.logged_process);
 
 const assert = std.debug.assert;
 
 const Self = @This();
-pub const State = enum(u8) { initial, running, terminated, completed };
+pub const State = enum(u8) { running, terminated, completed };
 const AtomicState = std.atomic.Value(State);
-const Options = struct { env: ?*const std.process.EnvMap = null };
-
-// Passed in to init
-allocator: std.mem.Allocator,
-name: []const u8,
-argv: []const []const u8,
-options: Options,
 
 // Allocated by init
-arena: std.heap.ArenaAllocator,
-cwd: []const u8,
+child: std.process.Child,
+stdin_thread: std.Thread,
+stderr_thread: std.Thread,
 
 // Lifecycle state
-child: ?std.process.Child = null,
-stdin_thread: ?std.Thread = null,
-stderr_thread: ?std.Thread = null,
 current_state: AtomicState,
 
-pub fn state(self: *Self) State {
-    return self.current_state.load(.seq_cst);
-}
-
-pub fn init(
+pub fn spawn(
     allocator: std.mem.Allocator,
-    name: []const u8,
     argv: []const []const u8,
-    options: Options,
 ) !*Self {
-    var arena = std.heap.ArenaAllocator.init(allocator);
+    const self = try allocator.create(Self);
+    errdefer allocator.destroy(self);
 
-    const cwd = try std.process.getCwdAlloc(arena.allocator());
-
-    const process = try allocator.create(Self);
-    process.* = .{
-        .allocator = allocator,
-        .arena = arena,
-        .name = name,
-        .cwd = cwd,
-        .argv = argv,
-        .options = options,
-        .current_state = AtomicState.init(.initial),
+    self.* = .{
+        .current_state = AtomicState.init(.running),
+        .child = std.process.Child.init(argv, allocator),
+        .stdin_thread = undefined,
+        .stderr_thread = undefined,
     };
-    return process;
-}
 
-pub fn deinit(self: *Self) void {
-    const allocator = self.allocator;
-    self.arena.deinit();
-    allocator.destroy(self);
-}
+    self.child.stdin_behavior = .Pipe;
+    self.child.stdout_behavior = .Ignore;
+    self.child.stderr_behavior = .Inherit;
 
-pub fn start(
-    self: *Self,
-) !void {
-    self.expect_state_in(.{ .initial, .terminated, .completed });
-    defer self.expect_state_in(.{.running});
+    try self.child.spawn();
 
-    var child = std.process.Child.init(self.argv, self.allocator);
-
-    child.cwd = self.cwd;
-    child.env_map = self.options.env;
-    child.stdin_behavior = .Pipe;
-    child.stdout_behavior = .Ignore;
-    child.stderr_behavior = .Pipe;
-
-    try child.spawn();
-
-    std.debug.print(
-        "{s}: {s}\n",
-        .{ self.name, try format_argv(self.arena.allocator(), self.argv) },
-    );
+    errdefer {
+        _ = self.child.kill() catch {};
+    }
 
     // Zig doesn't have non-blocking version of child.wait, so we use `BrokenPipe`
     // on writing to child's stdin to detect if a child is dead in a non-blocking
-    // manner. Checks once a second second in a separate thread.
+    // manner. Checks once a second in a separate thread.
     _ = try std.posix.fcntl(
-        child.stdin.?.handle,
+        self.child.stdin.?.handle,
         std.posix.F.SETFL,
         @as(u32, @bitCast(std.posix.O{ .NONBLOCK = true })),
     );
@@ -91,7 +60,7 @@ pub fn start(
         .{},
         struct {
             fn poll_broken_pipe(stdin: std.fs.File, process: *Self) void {
-                while (true) {
+                while (process.state() == .running) {
                     std.time.sleep(1 * std.time.ns_per_s);
                     _ = stdin.write(&.{1}) catch |err| {
                         switch (err) {
@@ -99,7 +68,15 @@ pub fn start(
                             error.BrokenPipe,
                             error.NotOpenForWriting,
                             => {
-                                process.current_state.store(.completed, .seq_cst);
+                                // Only write the state variable in case the process is still
+                                // considered running. If it was actively terminated, we don't
+                                // want to overwrite that state.
+                                _ = process.current_state.cmpxchgStrong(
+                                    .running,
+                                    .completed,
+                                    .seq_cst,
+                                    .seq_cst,
+                                );
                                 break;
                             },
                             else => @panic(@errorName(err)),
@@ -108,36 +85,19 @@ pub fn start(
                 }
             }
         }.poll_broken_pipe,
-        .{ child.stdin.?, self },
+        .{ self.child.stdin.?, self },
     );
 
-    // The child process' stderr is echoed to stderr with a name prefix
-    self.stderr_thread = try std.Thread.spawn(
-        .{},
-        struct {
-            fn log_stderr(stderr: std.fs.File, process: *Self) void {
-                while (true) {
-                    var buf: [1024]u8 = undefined;
-                    const line_opt = stderr.reader().readUntilDelimiterOrEof(
-                        &buf,
-                        '\n',
-                    ) catch |err| {
-                        log.info("{s}: failed reading stderr: {any}", .{ process.name, err });
-                        break;
-                    };
-                    if (line_opt) |line| {
-                        log.info("{s}: {s}", .{ process.name, line });
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }.log_stderr,
-        .{ child.stderr.?, self },
-    );
+    return self;
+}
 
-    self.child = child;
-    self.current_state.store(.running, .seq_cst);
+pub fn destroy(self: *Self, allocator: std.mem.Allocator) void {
+    assert(self.state() == .terminated or self.state() == .completed);
+    allocator.destroy(self);
+}
+
+pub fn state(self: *Self) State {
+    return self.current_state.load(.seq_cst);
 }
 
 pub fn terminate(
@@ -146,37 +106,30 @@ pub fn terminate(
     self.expect_state_in(.{.running});
     defer self.expect_state_in(.{.terminated});
 
-    var child = self.child.?;
-    const stdin_thread = self.stdin_thread.?;
-    const stderr_thread = self.stderr_thread.?;
-
-    // Terminate the process
+    // Terminate the process.
     //
     // Uses the same method as `src/testing/tmp_tigerbeetle.zig`.
     // See: https://github.com/ziglang/zig/issues/16820
     _ = kill: {
         if (builtin.os.tag == .windows) {
             const exit_code = 1;
-            break :kill std.os.windows.TerminateProcess(child.id, exit_code);
+            break :kill std.os.windows.TerminateProcess(self.child.id, exit_code);
         } else {
-            break :kill std.posix.kill(child.id, std.posix.SIG.TERM);
+            break :kill std.posix.kill(self.child.id, std.posix.SIG.KILL);
         }
     } catch |err| {
-        std.debug.print(
-            "{s}: failed to kill process: {any}\n",
-            .{ self.name, err },
+        log.err(
+            "failed to kill process {d}: {any}\n",
+            .{ self.child.id, err },
         );
     };
 
-    // Await threads
-    stdin_thread.join();
-    stderr_thread.join();
+    // Await thread.
+    self.stdin_thread.join();
 
-    // Await the terminated process
-    const term = child.wait() catch unreachable;
+    // Await the terminated process.
+    const term = self.child.wait() catch unreachable;
 
-    self.child = null;
-    self.stderr_thread = null;
     self.current_state.store(.terminated, .seq_cst);
 
     return term;
@@ -185,23 +138,16 @@ pub fn terminate(
 pub fn wait(
     self: *Self,
 ) !std.process.Child.Term {
-    self.expect_state_in(.{ .running, .completed });
-    defer self.expect_state_in(.{.completed});
+    self.expect_state_in(.{ .running, .terminated, .completed });
+    defer self.expect_state_in(.{ .terminated, .completed });
 
-    var child = self.child.?;
-    const stdin_thread = self.stdin_thread.?;
-    const stderr_thread = self.stderr_thread.?;
+    // Wait until the process runs to completion.
+    const term = self.child.wait();
 
-    // Wait until the process runs to completion
-    const term = child.wait();
-
-    // Await threads
-    stdin_thread.join();
-    stderr_thread.join();
-
-    self.child = null;
-    self.stderr_thread = null;
-    self.current_state.store(.completed, .seq_cst);
+    if (self.state() == .running) {
+        // Await thread in case this process is still running.
+        self.stdin_thread.join();
+    }
 
     return term;
 }
@@ -213,60 +159,100 @@ fn expect_state_in(self: *Self, comptime valid_states: anytype) void {
         if (actual_state == valid) return;
     }
 
-    log.err("{s}: expected state in {any} but actual state is {s}", .{
-        self.name,
+    log.err("expected state in {any} but actual state is {s}", .{
         valid_states,
         @tagName(actual_state),
     });
     unreachable;
 }
 
-fn format_argv(allocator: std.mem.Allocator, argv: []const []const u8) ![]const u8 {
-    assert(argv.len > 0);
-
-    var out = std.ArrayList(u8).init(allocator);
-    const writer = out.writer();
-
-    try writer.writeAll("$");
-    for (argv) |arg| {
-        try writer.writeByte(' ');
-        try writer.writeAll(arg);
+// The test for LoggedProcess needs an executable that runs forever which is solved (without
+// depending on system executables) by using the Zig compiler to build this very file as an
+// executable in a temporary directory.
+pub usingnamespace if (@import("root") != @This()) struct {
+    // For production builds, don't include the main function.
+    // This is `if __name__ == "__main__":` at comptime!
+} else struct {
+    pub fn main() !void {
+        while (true) {
+            _ = try std.io.getStdErr().write("yep\n");
+        }
     }
-
-    return try out.toOwnedSlice();
-}
+};
 
 test "LoggedProcess: starts and stops" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer assert(gpa.deinit() == .ok);
 
-    const argv: []const []const u8 = &.{
-        "awk",
-        \\ BEGIN { for (i = 0; i < 10; i++) { print i > "/dev/fd/2"; } }
-        ,
-    };
+    const zig_exe = try std.process.getEnvVarOwned(allocator, "ZIG_EXE"); // Set by build.zig
+    defer allocator.free(zig_exe);
 
-    const name = "test program";
-    var replica = try Self.init(allocator, name, argv, .{});
-    defer replica.deinit();
+    var tmp_dir = std.testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
 
-    // start & stop
-    try replica.start();
+    const tmp_dir_path = try std.fs.path.join(allocator, &.{
+        ".zig-cache",
+        "tmp",
+        &tmp_dir.sub_path,
+    });
+    defer allocator.free(tmp_dir_path);
+
+    const test_exe_buf = try allocator.create([std.fs.max_path_bytes]u8);
+    defer allocator.destroy(test_exe_buf);
+
+    { // Compile this file as an executable!
+        const this_file = try std.fs.cwd().realpath(@src().file, test_exe_buf);
+        const argv = [_][]const u8{ zig_exe, "build-exe", this_file };
+        const exec_result = try std.process.Child.run(.{
+            .allocator = allocator,
+            .argv = &argv,
+            .cwd = tmp_dir_path,
+        });
+        defer allocator.free(exec_result.stdout);
+        defer allocator.free(exec_result.stderr);
+
+        if (exec_result.term.Exited != 0) {
+            std.debug.print("{s}{s}", .{ exec_result.stdout, exec_result.stderr });
+            return error.FailedToCompile;
+        }
+    }
+
+    const test_exe = try tmp_dir.dir.realpath(
+        "logged_process" ++ comptime builtin.target.exeFileExt(),
+        test_exe_buf,
+    );
+
+    const argv: []const []const u8 = &.{test_exe};
+
+    var process = try Self.spawn(allocator, argv);
+    defer process.destroy(allocator);
+
     std.time.sleep(10 * std.time.ns_per_ms);
-    _ = try replica.terminate();
-
-    std.time.sleep(10 * std.time.ns_per_ms);
-
-    // restart & stop
-    try replica.start();
-    std.time.sleep(10 * std.time.ns_per_ms);
-    _ = try replica.terminate();
+    _ = try process.terminate();
 }
 
-test format_argv {
-    const formatted = try format_argv(std.testing.allocator, &.{ "foo", "bar", "baz" });
+/// Formats the ports as comma-separated. Caller owns slice after successful return.
+pub fn comma_separate_ports(allocator: std.mem.Allocator, ports: []const u16) ![]const u8 {
+    assert(ports.len > 0);
+
+    var out = std.ArrayList(u8).init(allocator);
+    errdefer out.deinit();
+
+    const writer = out.writer();
+
+    try std.fmt.format(writer, "{d}", .{ports[0]});
+    for (ports[1..]) |port| {
+        try writer.writeByte(',');
+        try std.fmt.format(writer, "{d}", .{port});
+    }
+
+    return out.toOwnedSlice();
+}
+
+test comma_separate_ports {
+    const formatted = try comma_separate_ports(std.testing.allocator, &.{ 3000, 3001, 3002 });
     defer std.testing.allocator.free(formatted);
 
-    try std.testing.expectEqualStrings("$ foo bar baz", formatted);
+    try std.testing.expectEqualStrings("3000,3001,3002", formatted);
 }
