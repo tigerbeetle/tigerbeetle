3d1b77968f53a2670e2ac4334536e1214b33048b
Oskar Wickström <oskar@wickstrom.tech>
2024-10-02 15:26:50 +0200

    test: run concurrent workloads with separate ledgers
    Restructures the systest workload (in Java) to run multiple workloads in
    separate threads, isolated by using unique ledgers. This let's us verify
    the thread safety claim of the Java client, which is shared across the
    threads.
    
    The verified properties after querying known accounts are similar to before, because we already
    grouped accounts by ledger and checked consistency for each group.

--- src/testing/systest/workload/src/main/java/Main.java
+++ src/testing/systest/workload/src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.security.SecureRandom;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
 import com.tigerbeetle.Client;
 import com.tigerbeetle.UInt128;
 
@@ -17,12 +19,34 @@ public final class Main {
           "REPLICAS must list at least one address (comma-separated)");
     }
 
+    int workloadCount = Integer.parseUnsignedInt(env.getOrDefault("WORKLOAD_COUNT", "5"));
+    if (replicaAddresses.length == 0) {
+      throw new IllegalArgumentException(
+          "REPLICAS must list at least one address (comma-separated)");
+    }
+
     Random random = new SecureRandom();
     byte[] clusterID = UInt128.asBytes(Long.parseLong(env.getOrDefault("CLUSTER", "1")));
 
     try (var client = new Client(clusterID, replicaAddresses)) {
-      System.err.println("starting workload");
-      new Workload(random, client).run();
+      System.err.println("starting %d workload(s)".formatted(workloadCount));
+
+      var executor = Executors.newFixedThreadPool(workloadCount);
+      var completionService = new ExecutorCompletionService<Void>(executor);
+
+      for (int i = 0; i < workloadCount; i++) {
+        final var ledger = i + 1;
+        completionService.submit(new Workload(random, client, ledger));
+      }
+
+      try {
+        for (int i = 0; i < workloadCount; i++) {
+          var result = completionService.take();
+          result.get();
+        }
+      } finally {
+        executor.shutdownNow();
+      }
     }
   }
 }
--- src/testing/systest/workload/src/main/java/Model.java
+++ src/testing/systest/workload/src/main/java/Model.java
@@ -1,46 +1,27 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
- * Tracks information about the accounts created by test, i.e. their ids and which ledgers they
- * belong to.
+ * Tracks information about the accounts created by test (e.g. their ids). The model should only be
+ * used for a single ledger.
  */
 public class Model {
   /**
    * Accounts by id.
    */
   HashMap<Long, CreatedAccount> accounts = new HashMap<>();
+  public final int ledger;
+
+  public Model(int ledger) {
+    this.ledger = ledger;
+  }
 
   ArrayList<CreatedAccount> allAccounts() {
     // We use a sorted collection of accounts for stable index-based lookups.
     var allAccounts =
         accounts.values().stream().sorted(Comparator.comparing(account -> account.id())).toList();
     return new ArrayList<CreatedAccount>(allAccounts);
-  }
-
-  ArrayList<CreatedAccount> ledgerAccounts(int ledger) {
-    // We use a sorted collection of accounts for stable index-based lookups.
-    var ledgerAccounts = accounts.values().stream().filter(account -> account.ledger() == ledger)
-        .sorted(Comparator.comparing(account -> account.id())).toList();
-
-    return new ArrayList<CreatedAccount>(ledgerAccounts);
-  }
-
-  Map<Integer, ArrayList<CreatedAccount>> accountsPerLedger() {
-    var ledgerAccounts =
-        accounts.values().stream().collect(Collectors.groupingBy(account -> account.ledger()));
-
-    var results = new HashMap<Integer, ArrayList<CreatedAccount>>();
-
-    for (var entry : ledgerAccounts.entrySet()) {
-      var values = new ArrayList<CreatedAccount>(entry.getValue());
-      results.put(entry.getKey(), values);
-    }
-
-    return results;
   }
 }
 
