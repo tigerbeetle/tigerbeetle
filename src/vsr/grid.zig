676a04b684a6be217cff33a59d6ade9d960eb153
chaitanyabhandari <chaitanya@tigerbeetle.com>
2024-11-02 11:35:17 +0530

    grid: divide request buffer between read_global_queue and faulty_blocks

--- src/vsr/grid.zig
+++ src/vsr/grid.zig
@@ -1177,10 +1177,16 @@ pub fn GridType(comptime Storage: type) type {
         pub fn next_batch_of_block_requests(grid: *Grid, requests: []vsr.BlockRequest) usize {
             assert(grid.callback != .cancel);
             assert(requests.len > 0);
+            assert(requests.len == constants.grid_repair_reads_max);
 
             // Prioritize requests for blocks with stalled Grid reads, so that commit/compaction can
-            // continue.
-            const request_faults_count = @min(grid.read_global_queue.count, requests.len);
+            // continue. We now divide the buffer up between `read_global_queue` and
+            // `blocks_missing.faulty_blocks` so that we always request blocks from both queues.
+            const request_faults_count = @min(
+                grid.read_global_queue.count,
+                // Surplus from `blocks_missing.faulty_blocks` may be used by `read_global_queue`.
+                requests.len / 2 + (requests.len / 2 -| grid.blocks_missing.faulty_blocks.count()),
+            );
             // (Note that many – but not all – of these blocks are also in the GridBlocksMissing.
             // The `read_global_queue` is a FIFO, whereas the GridBlocksMissing has a fixed
             // capacity.)
