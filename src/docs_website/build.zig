b3627b6302db4c04bddfdc62c142b71d60330028
Fabio Arnold <fabio@fabioarnold.de>
2024-11-14 20:33:09 +0100

    Re-use static website generator for docs

---
+++ src/docs_website/build.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const Website = @import("src/website.zig").Website;
+const assets = @import("src/assets.zig");
+const docs = @import("src/docs.zig");
+
+pub fn build(b: *std.Build) !void {
+    const url_prefix: []const u8 = b.option(
+        []const u8,
+        "url_prefix",
+        "Prefix links with this string",
+    ) orelse "";
+    const pandoc_bin = get_pandoc_bin(b) orelse return;
+    const website = Website.init(b, url_prefix, pandoc_bin);
+    const file_checker_exe = b.addExecutable(.{
+        .name = "file_checker",
+        .root_source_file = b.path("src/file_checker.zig"),
+        .target = b.graph.host,
+    });
+    const file_checker_run = b.addRunArtifact(file_checker_exe);
+
+    const install_assets = assets.install(b, .{ .source = "assets", .target = "." });
+    file_checker_run.step.dependOn(&install_assets.step);
+
+    const docs_dir = try docs.build(b, website, "../../docs");
+    const install_blog = b.addInstallDirectory(.{
+        .source_dir = docs_dir,
+        .install_dir = .prefix,
+        .install_subdir = ".",
+    });
+    file_checker_run.step.dependOn(&install_blog.step);
+
+    file_checker_run.addArg("zig-out");
+
+    b.getInstallStep().dependOn(&file_checker_run.step);
+}
+
+fn get_pandoc_bin(b: *std.Build) ?std.Build.LazyPath {
+    const host = b.graph.host.result;
+    const name = switch (host.os.tag) {
+        .linux => switch (host.cpu.arch) {
+            .x86_64 => "pandoc_linux_amd64",
+            else => @panic("unsupported cpu arch"),
+        },
+        .macos => switch (host.cpu.arch) {
+            .aarch64 => "pandoc_macos_arm64",
+            else => @panic("unsupported cpu arch"),
+        },
+        else => @panic("unsupported os"),
+    };
+    if (b.lazyDependency(name, .{})) |dep| {
+        return dep.path("bin/pandoc");
+    } else {
+        return null;
+    }
+}
---
+++ src/docs_website/src/website.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const LazyPath = std.Build.LazyPath;
+const Compile = std.Build.Step.Compile;
+const log = std.log.scoped(.website);
+
+pub const Website = @This();
+
+pub const file_size_max = 1 << 20; // 1 MiB
+
+url_prefix: []const u8,
+pandoc_bin: LazyPath,
+
+page_writer_exe: *Compile,
+
+pub fn init(b: *std.Build, url_prefix: []const u8, pandoc_bin: LazyPath) Website {
+    const page_writer_exe = b.addExecutable(.{
+        .name = "page_writer",
+        .root_source_file = b.path("src/page_writer.zig"),
+        .target = b.graph.host,
+    });
+
+    return .{
+        .url_prefix = url_prefix,
+        .pandoc_bin = pandoc_bin,
+        .page_writer_exe = page_writer_exe,
+    };
+}
+
+pub fn write_page(self: Website, options: struct {
+    title: []const u8 = "TigerBeetle",
+    author: []const u8 = "TigerBeetle Team",
+    content: LazyPath,
+}) LazyPath {
+    const b = self.page_writer_exe.step.owner;
+    const page_writer_run = b.addRunArtifact(self.page_writer_exe);
+    page_writer_run.addArg(options.title);
+    page_writer_run.addArg(options.author);
+    page_writer_run.addArg(self.url_prefix);
+    page_writer_run.addFileArg(options.content);
+    return page_writer_run.addOutputFileArg("page.html");
+}
