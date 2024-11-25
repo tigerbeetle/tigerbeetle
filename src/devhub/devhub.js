56ab4c09010f15326f7a644fbef2d935bf86bc86
Fabio Arnold <fabio@fabioarnold.de>
2024-11-25 11:49:09 +0100

    Display the number of untriaged issues

--- src/devhub/devhub.js
+++ src/devhub/devhub.js
@@ -52,14 +52,18 @@ async function mainSeeds() {
   const dataUrl =
     "https://raw.githubusercontent.com/tigerbeetle/devhubdb/main/fuzzing/data.json";
   const pullsURL = "https://api.github.com/repos/tigerbeetle/tigerbeetle/pulls";
+  const issuesURL = "https://api.github.com/repos/tigerbeetle/tigerbeetle/issues?per_page=200";
 
-  const [records, pulls] = await Promise.all([
+  const [records, pulls, issues] = await Promise.all([
     (async () => await (await fetch(dataUrl)).json())(),
     (async () => await (await fetch(pullsURL)).json())(),
+    (async () => await (await fetch(issuesURL)).json())(),
   ]);
 
   const pullsByURL = new Map(pulls.map((pull) => [pull.html_url, pull]));
   const openPullRequests = new Set(pulls.map((it) => it.number));
+  const untriagedIssues = issues.filter(issue => !issue.pull_request && !issue.labels.map(label => label.name).includes("triaged"));
+  document.querySelector("#untriaged-issues-count").innerText = untriagedIssues.length;
 
   // Filtering:
   // - By default, show one seed per fuzzer per commit; exclude successes for the main branch and
