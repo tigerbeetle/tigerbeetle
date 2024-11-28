42d34004da913fb3b13a3b3ecf0e95520c77fa7a
Fabio Arnold <fabio@fabioarnold.de>
2024-11-28 16:55:14 +0100

    Fix links to client documentation

--- src/docs_website/pandoc/markdown-links.lua
+++ src/docs_website/pandoc/markdown-links.lua
@@ -1,9 +1,6 @@
 is_readme = PANDOC_STATE.input_files[1]:sub(-9) == "README.md"
 
 function Link (link)
-  link.target = link.target:gsub("README.md", "")
-  link.target = link.target:gsub(".md", "")
-
   if not is_readme then
     -- We have to adjust relative links to go up one more level.
     if link.target:sub(1, 2) == "./"  then
@@ -12,6 +9,20 @@ function Link (link)
       link.target = "../" .. link.target
     end
   end
+
+  -- Links to client documentation
+  if link.target:sub(1, 12) == "/src/clients" then
+    if link.target:find("README.md") then
+      -- Coming from 'reference/requests/page'
+      link.target = "../../.." .. link.target:sub(5)
+    else
+      -- Make GitHub link
+      link.target = "https://github.com/tigerbeetle/tigerbeetle/blob/main" .. link.target
+    end
+  end
+
+  link.target = link.target:gsub("README.md", "")
+  link.target = link.target:gsub(".md", "")
 
   return link
 end
