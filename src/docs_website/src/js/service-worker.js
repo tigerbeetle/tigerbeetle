26302467adbc5a813dfe1a9f1a4ef2129dd7fd31
Fabio Arnold <fabio@fabioarnold.de>
2024-12-04 11:22:42 +0100

    ServiceWorker: don’t wait to activate after install

--- src/docs_website/src/js/service-worker.js
+++ src/docs_website/src/js/service-worker.js
@@ -4,6 +4,8 @@ const FILES_TO_CACHE =
 ;
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
