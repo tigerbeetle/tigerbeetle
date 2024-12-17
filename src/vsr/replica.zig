67ac4a4090821e99775340f0d65cffb01aca8c94
batiati <rbatiati@gmail.com>
2024-12-17 10:11:49 -0300

    [StateMachine] Make alignment checks operation-specific. Accept non-aligned memory and check the alignment required by the specific state machine operation.
    Previously, we enforced the state machine signatures to receive aligned slices as `[]align(16) u8` and `*align(16) [message_body_size_max]u8`.
    
    This approach would cause friction with custom state machines by requiring all events/results to be aligned by a constant. It's also already inconsistent with results such as `CreateAccountsResult` and `CreateTransfersResult`, which are 8 bytes long. For example, when replying 3 results, we don't add any padding to keep it `align(16)`,  we would return 24 bytes instead of 32 bytes.
    
    However, the main motivation for this change is protocol batching. When passing chunks of memory to the state machine for reading/writing, those chunks are aligned to the corresponding Event and Result, but not always aligned to 16 (especially when writing results to a buffer of `CreateAccountsResult` and `CreateTransfersResult`).

--- src/vsr/replica.zig
+++ src/vsr/replica.zig
@@ -4816,6 +4816,7 @@ pub fn ReplicaType(
                 .timestamp = prepare.header.timestamp,
                 .commit = prepare.header.op,
                 .size = @sizeOf(Header) + @as(u32, @intCast(reply_body_size)),
+                .batch_count = 0,
             };
             assert(reply.header.epoch == 0);
 
@@ -6587,6 +6588,7 @@ pub fn ReplicaType(
                 },
                 .request = request_header.request,
                 .operation = request_header.operation,
+                .batch_count = 0,
             };
             message.header.set_checksum_body(message.body_used());
             message.header.set_checksum();
@@ -10303,6 +10305,7 @@ pub fn ReplicaType(
                 .parent = 0,
                 .client = 0,
                 .session = 0,
+                .batch_count = 0,
             };
 
             stdx.copy_disjoint(.exact, u8, request.body_used(), body);
