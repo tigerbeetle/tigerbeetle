8aa6e7a3a1e2fbff6fbd180f985b768b049b7f29
Fabio Arnold <fabio@fabioarnold.de>
2025-01-29 15:54:00 +0100

    docs: add link checker

--- src/docs_website/src/file_checker.zig
+++ src/docs_website/src/file_checker.zig
@@ -100,4 +100,65 @@ fn validate_text_file(arena: std.mem.Allocator, dir: std.fs.Dir, path: []const u
         log.err("file '{s}' doesn't end with a newline", .{try dir.realpathAlloc(arena, path)});
         return error.MissingNewline;
     }
+
+    if (std.mem.endsWith(u8, path, ".html")) {
+        try check_links(arena, dir, path);
+    }
+}
+
+// These links don't work with https.
+const https_exceptions = std.StaticStringMap(void).initComptime(.{
+    .{"http://www.bailis.org/blog/linearizability-versus-serializability/"},
+});
+
+fn check_links(arena: std.mem.Allocator, dir: std.fs.Dir, html_path: []const u8) !void {
+    const html = try dir.readFileAlloc(arena, html_path, file_size_max);
+
+    var line_it = std.mem.tokenizeScalar(u8, html, '\n');
+    var line_number: usize = 1;
+    while (line_it.next()) |line| {
+        defer line_number += 1;
+        errdefer log.err("[link checker] error in {s}:{}", .{ html_path, line_number });
+
+        var link_it = std.mem.tokenizeSequence(u8, line, "href=\"");
+        if (!std.mem.startsWith(u8, line, "href=\"")) _ = link_it.next(); // Skip prefix
+        while (link_it.next()) |chunk| {
+            var url = std.mem.sliceTo(chunk, '"');
+
+            // Strip anchor
+            if (std.mem.lastIndexOfScalar(u8, url, '#')) |i| url = url[0..i];
+            if (url.len == 0) continue;
+
+            if (std.mem.startsWith(u8, url, "http://")) {
+                if (https_exceptions.has(url)) continue;
+                log.err("Found insecure link: {s}", .{url});
+                return error.InsecureLink;
+            }
+
+            // Skip external link
+            if (std.mem.startsWith(u8, url, "https://")) continue;
+            if (std.mem.startsWith(u8, url, "mailto:")) continue;
+
+            const target = if (url[0] == '/')
+                url[1..]
+            else if (std.fs.path.dirname(html_path)) |dirname|
+                try std.fs.path.join(arena, &.{ dirname, url })
+            else
+                url;
+            if (target.len == 0) continue;
+
+            if (!try path_exists(dir, target)) {
+                log.err("Link (\"{s}\") target not found: {s}", .{ url, target });
+                return error.TargetNotFound;
+            }
+        }
+    }
+}
+
+fn path_exists(dir: std.fs.Dir, path: []const u8) !bool {
+    dir.access(path, .{}) catch |err| switch (err) {
+        error.FileNotFound => return false,
+        else => return err,
+    };
+    return true;
 }
