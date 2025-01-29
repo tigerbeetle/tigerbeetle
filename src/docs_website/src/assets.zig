bbd9e25dafe6f13df5d55958fcbe27c79158c1e8
Alex Kladov <aleksey.kladov@gmail.com>
2025-01-29 15:04:38 +0000

    docs: refactor file checker
    - split into functions to separate walk control flow from validation proper
    - remove dependency on assets.zig

--- src/docs_website/src/assets.zig
+++ src/docs_website/src/assets.zig
@@ -1,25 +1,6 @@
 const std = @import("std");
 const Build = std.Build;
 
-const extensions_text = .{ ".css", ".html", ".js", ".json", ".svg", ".xml" };
-const extensions_binary = .{ ".avif", ".gif", ".jpg", ".png", ".ttf", ".webp", ".woff2" };
-
-const FileType = struct {
-    extension: []const u8,
-    is_binary: bool,
-};
-
-pub const supported_file_types: []const FileType = blk: {
-    var result: []const FileType = &.{};
-    for (extensions_text) |ext| {
-        result = result ++ .{.{ .extension = ext, .is_binary = false }};
-    }
-    for (extensions_binary) |ext| {
-        result = result ++ .{.{ .extension = ext, .is_binary = true }};
-    }
-    break :blk result;
-};
-
 pub const exclude_extensions = [_][]const u8{
     ".DS_Store",
 };
