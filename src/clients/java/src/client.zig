37da4355616242716917e8f947ce9d55f149825e
batiati <rbatiati@gmail.com>
2025-02-17 19:55:32 -0300

    [Java] Fix Windows JNI Tests Split all JNI tests so they no longer depend on the `JNIClient` or the `VSR` module.
    The `VSR` module can only be built in `Debug` or `ReleaseSafe`, but on Windows, JNI tests must run in `ReleaseFast` due to `JNI_CreateJavaVM` triggering Zig's panic handler when probing the stack size.

--- src/clients/java/src/client.zig
+++ src/clients/java/src/client.zig
@@ -11,6 +11,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const jni = @import("jni.zig");
+const JNIThreadCleaner = @import("jni_thread_cleaner.zig").JNIThreadCleaner;
 
 comptime {
     if (!builtin.link_libc) {
@@ -18,7 +19,6 @@ comptime {
     }
 }
 
-// When referenced from unit_test.zig, there is no vsr import module. So use relative path instead.
 pub const vsr = @import("vsr");
 
 const tb = vsr.tb_client;
@@ -138,7 +138,7 @@ const NativeClient = struct {
         const jvm: *jni.JavaVM = @ptrFromInt(context_ptr);
 
         const env = JNIHelper.try_get_env(jvm) orelse
-            JNIThreadHelper.attach_current_thread_with_cleanup(jvm);
+            JNIThreadCleaner.attach_current_thread_with_cleanup(jvm);
 
         // Retrieves the request instance, and drops the GC reference.
         assert(packet.user_data != null);
@@ -624,33 +624,6 @@ const JNIHelper = struct {
         };
     }
 
-    pub inline fn attach_current_thread(jvm: *jni.JavaVM) *jni.JNIEnv {
-        var env: *jni.JNIEnv = undefined;
-        const jni_result = jvm.attach_current_thread_as_daemon(&env, null);
-        if (jni_result != .ok) {
-            const message = "Unexpected result calling JavaVM.AttachCurrentThreadAsDaemon";
-            log.err(
-                message ++ "; Error = {} ({s})",
-                .{ @intFromEnum(jni_result), @tagName(jni_result) },
-            );
-            @panic("JNI: " ++ message);
-        }
-
-        return env;
-    }
-
-    pub inline fn detach_current_thread(jvm: *jni.JavaVM) void {
-        const jni_result = jvm.detach_current_thread();
-        if (jni_result != .ok) {
-            const message = "Unexpected result calling JavaVM.DetachCurrentThread";
-            log.err(
-                message ++ "; Error = {} ({s})",
-                .{ @intFromEnum(jni_result), @tagName(jni_result) },
-            );
-            @panic("JNI: " ++ message);
-        }
-    }
-
     pub inline fn get_java_vm(env: *jni.JNIEnv) *jni.JavaVM {
         var jvm: *jni.JavaVM = undefined;
         const jni_result = env.get_java_vm(&jvm);
@@ -773,187 +746,3 @@ const JNIHelper = struct {
         return @ptrCast(address[0..@as(usize, @intCast(length))]);
     }
 };
-
-/// Helper for managing the `AttachCurrentThread`/`DetachCurrentThread` lifecycle
-/// when the JNI layer is unaware of when the native thread exits.
-/// https://developer.android.com/training/articles/perf-jni#threads
-const JNIThreadHelper = struct {
-    var tls_key: ?tls.Key = null;
-    var create_key_once = std.once(create_key);
-
-    /// This function calls `AttachCurrentThreadAsDaemon` to attach the current native thread to
-    /// the JVM as a daemon thread. It also registers a callback to call `DetachCurrentThread`
-    /// when the thread exits (e.g., when the client is closed or evicted).
-    pub fn attach_current_thread_with_cleanup(jvm: *jni.JavaVM) *jni.JNIEnv {
-        // Create the tls key once per JVM.
-        create_key_once.call();
-
-        // Set the JVM handler to the thread-local storage slot for each time a native
-        // thread is started.
-        tls.set_key(tls_key.?, jvm);
-
-        return JNIHelper.attach_current_thread(jvm);
-    }
-
-    /// Create the thread-local storage key and the corresponding destructor callback.
-    /// Note: We don't need to delete the key because the JNI module cannot be unloaded,
-    /// so it will always be available for the duration of the JVM process.
-    fn create_key() void {
-        assert(tls_key == null);
-        tls_key = tls.create_key(&destructor_callback);
-    }
-
-    // Will be called by the OS with the JVM handler when the thread finalizes.
-    fn destructor_callback(jvm: *anyopaque) callconv(.C) void {
-        assert(tls_key != null);
-        JNIHelper.detach_current_thread(@ptrCast(jvm));
-    }
-
-    /// Thread-local storage abstraction,
-    /// based on `pthread_key_create` for Linux/MacOS and `FlsAlloc` for Windows.
-    const tls = switch (builtin.os.tag) {
-        .linux, .macos => struct {
-            /// TODO(zig) Should use `std.c` instead, redeclaring because of macos.
-            /// https://github.com/ziglang/zig/issues/13950.
-            const c = struct {
-                const pthread_key_t = c_uint;
-                extern "c" fn pthread_key_create(
-                    key: *pthread_key_t,
-                    destructor: ?*const fn (value: *anyopaque) callconv(.C) void,
-                ) std.c.E;
-                extern "c" fn pthread_setspecific(key: pthread_key_t, value: ?*anyopaque) c_int;
-            };
-
-            const Key = c.pthread_key_t;
-
-            fn create_key(destructor: ?*const fn (value: *anyopaque) callconv(.C) void) Key {
-                var key: Key = undefined;
-                const ret = c.pthread_key_create(&key, destructor);
-                if (ret != .SUCCESS) {
-                    const message = "Unexpected result calling pthread_key_create";
-                    log.err(message ++ "; Error = {} ({s})", .{
-                        @intFromEnum(ret),
-                        @tagName(ret),
-                    });
-                    @panic("JNI: " ++ message);
-                }
-
-                return key;
-            }
-
-            fn set_key(key: Key, value: *anyopaque) void {
-                const ret = c.pthread_setspecific(key, value);
-                if (ret != 0) {
-                    const message = "Unexpected result calling pthread_setspecific";
-                    log.err(message ++ "; Error = {}", .{ret});
-                    @panic("JNI: " ++ message);
-                }
-            }
-        },
-        .windows => struct {
-            const windows = struct {
-                const FLS_OUT_OF_INDEXES: std.os.windows.DWORD = 0xffffffff;
-
-                // Declaring the function with an alternative name because `CamelCase` functions are
-                // by convention, used for building generic types.
-                const fls_alloc = @extern(
-                    *const fn (
-                        ?*const fn (value: *anyopaque) callconv(.C) void,
-                    ) callconv(.C) std.os.windows.DWORD,
-                    .{
-                        .library_name = "kernel32",
-                        // https://learn.microsoft.com/en-us/windows/win32/api/fibersapi/nf-fibersapi-flsalloc
-                        .name = "FlsAlloc",
-                    },
-                );
-
-                const fls_set_value = @extern(
-                    *const fn (
-                        std.os.windows.DWORD,
-                        *anyopaque,
-                    ) callconv(.C) std.os.windows.BOOL,
-                    .{
-                        .library_name = "kernel32",
-                        // https://learn.microsoft.com/en-us/windows/win32/api/fibersapi/nf-fibersapi-flssetvalue
-                        .name = "FlsSetValue",
-                    },
-                );
-            };
-
-            const Key = std.os.windows.DWORD;
-
-            fn create_key(destructor: ?*const fn (value: *anyopaque) callconv(.C) void) Key {
-                const key = windows.fls_alloc(destructor);
-                if (key == windows.FLS_OUT_OF_INDEXES) {
-                    const message = "Unexpected result calling FlsAlloc";
-                    log.err(message ++ "; Error = {}", .{key});
-                    @panic("JNI: " ++ message);
-                }
-
-                return key;
-            }
-
-            fn set_key(key: Key, value: *anyopaque) void {
-                const ret = windows.fls_set_value(key, value);
-                if (ret == std.os.windows.FALSE) {
-                    const message = "Unexpected result calling FlsSetValue";
-                    log.err(message ++ "; Error = {}", .{ret});
-                    @panic("JNI: " ++ message);
-                }
-            }
-        },
-        else => unreachable,
-    };
-};
-
-test "JNIThreadHelper:tls" {
-    const tls = JNIThreadHelper.tls;
-    const TestContext = struct {
-        const TestContext = @This();
-
-        var tls_key: ?tls.Key = null;
-        var event: std.Thread.ResetEvent = .{};
-
-        counter: std.atomic.Value(u32),
-
-        fn init() TestContext {
-            if (tls_key == null) {
-                tls_key = tls.create_key(&destructor_callback);
-            }
-
-            return .{
-                .counter = std.atomic.Value(u32).init(0),
-            };
-        }
-
-        fn thread_main(self: *TestContext) void {
-            tls.set_key(tls_key.?, self);
-            event.wait();
-        }
-
-        fn destructor_callback(tls_value: *anyopaque) callconv(.C) void {
-            assert(tls_key != null);
-
-            const self: *TestContext = @alignCast(@ptrCast(tls_value));
-            _ = self.counter.fetchAdd(1, .monotonic);
-        }
-    };
-
-    var context = TestContext.init();
-    var threads: [10]std.Thread = undefined;
-    for (&threads) |*thread| {
-        thread.* = try std.Thread.spawn(.{}, TestContext.thread_main, .{&context});
-    }
-
-    // Assert that the callback only fires when the thread finishes.
-    try std.testing.expect(context.counter.load(.monotonic) == 0);
-
-    // Signal all threads to complete and wait for them.
-    TestContext.event.set();
-    for (&threads) |*thread| {
-        thread.join();
-    }
-
-    // Assert that all callbacks have fired.
-    try std.testing.expect(context.counter.load(.monotonic) == threads.len);
-}
