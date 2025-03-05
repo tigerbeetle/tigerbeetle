d1883380325d164796a5094d46af0dc5dadaefe0
batiati <rbatiati@gmail.com>
2025-03-05 14:14:23 -0300

    Fix: `@src.file` is relative to the module root path.

--- src/flags.zig
+++ src/flags.zig
@@ -712,9 +712,13 @@ test "flags" {
             errdefer gpa.destroy(flags_exe_buf);
 
             { // Compile this file as an executable!
-
+                const path_relative = try std.fs.path.join(gpa, &.{
+                    "src",
+                    @src().file,
+                });
+                defer gpa.free(path_relative);
                 const this_file = try std.fs.cwd().realpath(
-                    "src/" ++ @src().file,
+                    path_relative,
                     flags_exe_buf,
                 );
                 const argv = [_][]const u8{ zig_exe, "build-exe", this_file };
