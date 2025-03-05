39f7983de3e6d1f5fb467b80cafe88ff56d7c670
batiati <rbatiati@gmail.com>
2025-03-05 15:30:24 -0300

    Redeclare kernel32 functions removed from std

--- src/lsm/binary_search_benchmark.zig
+++ src/lsm/binary_search_benchmark.zig
@@ -168,7 +168,23 @@ const Benchmark = struct {
             var kernel_time: std.os.windows.FILETIME = undefined;
             var user_time: std.os.windows.FILETIME = undefined;
 
-            if (std.os.windows.kernel32.GetProcessTimes(
+            // Declaring the function with an alternative name because `CamelCase` functions are
+            // by convention, used for building generic types.
+            const get_process_times = @extern(
+                *const fn (
+                    in_hProcess: std.os.windows.HANDLE,
+                    out_lpCreationTime: *std.os.windows.FILETIME,
+                    out_lpExitTime: *std.os.windows.FILETIME,
+                    out_lpKernelTime: *std.os.windows.FILETIME,
+                    out_lpUserTime: *std.os.windows.FILETIME,
+                ) callconv(std.os.windows.WINAPI) std.os.windows.BOOL,
+                .{
+                    .library_name = "kernel32",
+                    .name = "GetProcessTimes",
+                },
+            );
+
+            if (get_process_times(
                 std.os.windows.kernel32.GetCurrentProcess(),
                 &creation_time,
                 &exit_time,
