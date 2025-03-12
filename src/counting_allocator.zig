1aa89d0b406a613eba2a5ef2031c7d426c6b7df0
batiati <rbatiati@gmail.com>
2025-03-12 09:31:38 -0300

    [Zig 0.14.0] Allocator interface.

--- src/counting_allocator.zig
+++ src/counting_allocator.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Alignment = std.mem.Alignment;
 
 const CountingAllocator = @This();
 
@@ -19,24 +20,34 @@ pub fn allocator(self: *CountingAllocator) std.mem.Allocator {
         .vtable = &.{
             .alloc = alloc,
             .resize = resize,
+            .remap = remap,
             .free = free,
         },
     };
 }
 
-fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
+fn alloc(ctx: *anyopaque, len: usize, ptr_align: Alignment, ret_addr: usize) ?[*]u8 {
     const self: *CountingAllocator = @alignCast(@ptrCast(ctx));
     self.size += len;
     return self.parent_allocator.rawAlloc(len, ptr_align, ret_addr);
 }
 
-fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+fn resize(ctx: *anyopaque, buf: []u8, buf_align: Alignment, new_len: usize, ret_addr: usize) bool {
     const self: *CountingAllocator = @alignCast(@ptrCast(ctx));
     self.size = (self.size - buf.len) + new_len;
     return self.parent_allocator.rawResize(buf, buf_align, new_len, ret_addr);
 }
 
-fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
+fn remap(ctx: *anyopaque, buf: []u8, buf_align: Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
+    const self: *CountingAllocator = @alignCast(@ptrCast(ctx));
+    if (self.parent_allocator.rawRemap(buf, buf_align, new_len, ret_addr)) |remapped| {
+        self.size = (self.size - buf.len) + new_len;
+        return remapped;
+    }
+    return null;
+}
+
+fn free(ctx: *anyopaque, buf: []u8, buf_align: Alignment, ret_addr: usize) void {
     const self: *CountingAllocator = @alignCast(@ptrCast(ctx));
     self.size -= buf.len;
     return self.parent_allocator.rawFree(buf, buf_align, ret_addr);
