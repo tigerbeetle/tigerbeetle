af78262b5bbabde70792842bba9bd1bf9c4c3f0b
Alex Kladov <aleksey.kladov@gmail.com>
2025-04-10 10:01:25 +0200

    stdx: simple fuzz test for the queue

--- src/queue.zig
+++ src/queue.zig
@@ -159,3 +159,124 @@ test "Queue: push/pop/peek/remove/empty" {
     try testing.expectEqual(@as(?*Foo, null), fifo.pop());
     try testing.expect(fifo.empty());
 }
+
+test "Queue: fuzz" {
+    const stdx = @import("./stdx.zig");
+    const fuzz = @import("./testing/fuzz.zig");
+
+    const Item = struct {
+        value: u64,
+        next: ?*@This(),
+    };
+    const Queue = QueueType(Item);
+    const Model = stdx.RingBufferType(u64, .slice);
+
+    const gpa = std.testing.allocator;
+    var prng = stdx.PRNG.from_seed(92);
+
+    for (0..100) |_| {
+        const N = 1000;
+
+        var queue: Queue = .{
+            .name = "fuzz",
+        };
+        var model = try Model.init(gpa, N);
+        defer model.deinit(gpa);
+
+        // Implement "remove" by copy.
+        var model_scratch = try Model.init(gpa, N);
+        defer model_scratch.deinit(gpa);
+
+        var items = try gpa.alloc(Item, N);
+        defer gpa.free(items);
+        for (items) |*item| {
+            item.* = .{ .value = prng.int(u64), .next = null };
+        }
+
+        var free: std.ArrayListUnmanaged(usize) = .{};
+        defer free.deinit(gpa);
+
+        try free.ensureTotalCapacity(gpa, N);
+        for (items, 0..) |_, index| {
+            free.appendAssumeCapacity(index);
+        }
+
+        const weights = fuzz.random_enum_weights(&prng, std.meta.DeclEnum(Queue));
+
+        for (0..N) |_| {
+            switch (prng.enum_weighted(std.meta.DeclEnum(Queue), weights)) {
+                .push => {
+                    if (free.items.len > 0) {
+                        const index = free.swapRemove(prng.index(free.items));
+                        model.push_assume_capacity(items[index].value);
+                        queue.push(&items[index]);
+                    }
+                },
+                .pop => {
+                    if (model.empty()) {
+                        assert(queue.pop() == null);
+                    } else {
+                        const item = queue.pop().?;
+                        assert(item.value == model.pop().?);
+
+                        const index = @divExact(
+                            (@intFromPtr(item) - @intFromPtr(items.ptr)),
+                            @sizeOf(Item),
+                        );
+                        free.appendAssumeCapacity(index);
+                    }
+                },
+                .peek => {
+                    if (model.head()) |head| {
+                        assert(queue.peek().?.value == head);
+                    } else {
+                        assert(queue.peek() == null);
+                    }
+                },
+                .peek_last => {
+                    if (model.tail()) |tail| {
+                        assert(queue.peek_last().?.value == tail);
+                    } else {
+                        assert(queue.peek_last() == null);
+                    }
+                },
+                .empty => {
+                    assert(model.empty() == queue.empty());
+                },
+                .contains => {
+                    const item = &items[prng.index(items)];
+
+                    const model_contains = model_contains: {
+                        var it = model.iterator();
+                        while (it.next()) |v| {
+                            if (v == item.value) {
+                                break :model_contains true;
+                            }
+                        }
+                        break :model_contains false;
+                    };
+                    assert(model_contains == queue.contains(item));
+                },
+                .remove => {
+                    const item = &items[prng.index(items)];
+                    if (queue.contains(item)) {
+                        queue.remove(item);
+
+                        assert(model_scratch.empty());
+                        while (model.pop()) |value| {
+                            if (value != item.value) {
+                                model_scratch.push_assume_capacity(value);
+                            }
+                        }
+                        std.mem.swap(Model, &model, &model_scratch);
+                    }
+                },
+                .reset => {
+                    model.clear();
+                    queue.reset();
+                },
+            }
+            assert(queue.count == model.count);
+        }
+    }
+}
