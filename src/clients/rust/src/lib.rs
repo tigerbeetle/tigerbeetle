ef731f43c6a6764df66b8e683bf5af97c1cc7daa
Brian Anderson <andersrb@gmail.com>
2025-07-14 13:21:00 -0600

    rust client: Fix new clippy lint

--- src/clients/rust/src/lib.rs
+++ src/clients/rust/src/lib.rs
@@ -1112,7 +1112,7 @@ impl core::fmt::Display for CreateAccountResult {
             Self::ImportedEventTimestampMustNotRegress => {
                 f.write_str("imported event timestamp must not regress")
             }
-            Self::Unknown(code) => f.write_fmt(format_args!("unknown {0}", code)),
+            Self::Unknown(code) => f.write_fmt(format_args!("unknown {code}")),
         }
     }
 }
@@ -1317,7 +1317,7 @@ impl core::fmt::Display for CreateTransferResult {
             Self::OverflowsTimeout => f.write_str("overflows timeout"),
             Self::ExceedsCredits => f.write_str("exceeds credits"),
             Self::ExceedsDebits => f.write_str("exceeds debits"),
-            Self::Unknown(code) => f.write_fmt(format_args!("unknown {0}", code)),
+            Self::Unknown(code) => f.write_fmt(format_args!("unknown {code}")),
         }
     }
 }
@@ -1357,7 +1357,7 @@ impl core::fmt::Display for InitStatus {
             Self::AddressLimitExceeded => f.write_str("address limit exceeded"),
             Self::SystemResources => f.write_str("system resources"),
             Self::NetworkSubsystem => f.write_str("network subsystem"),
-            Self::Unknown(code) => f.write_fmt(format_args!("unknown {0}", code)),
+            Self::Unknown(code) => f.write_fmt(format_args!("unknown {code}")),
         }
     }
 }
@@ -1407,7 +1407,7 @@ impl core::fmt::Display for PacketStatus {
             Self::ClientShutdown => f.write_str("client shutdown"),
             Self::InvalidOperation => f.write_str("invalid operation"),
             Self::InvalidDataSize => f.write_str("invalid data size"),
-            Self::Unknown(code) => f.write_fmt(format_args!("unknown {0}", code)),
+            Self::Unknown(code) => f.write_fmt(format_args!("unknown {code}")),
         }
     }
 }
