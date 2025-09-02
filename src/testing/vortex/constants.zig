26209483d63c55d5c8fc5a91585bd6bffa32dbf2
batiati <rbatiati@gmail.com>
2025-09-01 23:12:42 -0300

    Vortex: Remove `usingnamespace`.

--- src/testing/vortex/constants.zig
+++ src/testing/vortex/constants.zig
@@ -1,30 +1,35 @@
 const std = @import("std");
-const constants = @import("../../constants.zig");
+const constants = @This();
 
-pub usingnamespace constants;
+pub const vsr = @import("../../constants.zig");
 
-pub const cluster_id = 1;
-pub const connections_count_max = @divFloor(constants.clients_max, constants.replicas_max);
+pub const vortex = struct {
+    pub const cluster_id = 1;
+    pub const connections_count_max = @divFloor(
+        constants.vsr.clients_max,
+        constants.vsr.replicas_max,
+    );
 
-// We allow the cluster to not make progress processing requests for this amount of time. After
-// that it's considered a test failure.
-pub const liveness_requirement_seconds = 120;
-pub const liveness_requirement_micros = liveness_requirement_seconds * std.time.us_per_s;
+    // We allow the cluster to not make progress processing requests for this amount of time.
+    // After that it's considered a test failure.
+    pub const liveness_requirement_seconds = 120;
+    pub const liveness_requirement_micros = liveness_requirement_seconds * std.time.us_per_s;
 
-pub const replica_ports_actual = brk: {
-    var ports: [constants.replicas_max]u16 = undefined;
-    var replica_num: u16 = 0;
-    while (replica_num < constants.replicas_max) : (replica_num += 1) {
-        ports[replica_num] = 4000 + replica_num;
-    }
-    break :brk ports;
-};
+    pub const replica_ports_actual = brk: {
+        var ports: [constants.vsr.replicas_max]u16 = undefined;
+        var replica_num: u16 = 0;
+        while (replica_num < constants.vsr.replicas_max) : (replica_num += 1) {
+            ports[replica_num] = 4000 + replica_num;
+        }
+        break :brk ports;
+    };
 
-pub const replica_ports_proxied = brk: {
-    var ports: [constants.replicas_max]u16 = undefined;
-    var replica_num: u16 = 0;
-    while (replica_num < constants.replicas_max) : (replica_num += 1) {
-        ports[replica_num] = 3000 + replica_num;
-    }
-    break :brk ports;
+    pub const replica_ports_proxied = brk: {
+        var ports: [constants.vsr.replicas_max]u16 = undefined;
+        var replica_num: u16 = 0;
+        while (replica_num < constants.vsr.replicas_max) : (replica_num += 1) {
+            ports[replica_num] = 3000 + replica_num;
+        }
+        break :brk ports;
+    };
 };
