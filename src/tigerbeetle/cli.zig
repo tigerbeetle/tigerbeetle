f58e54384e3700a60d8c1799c0657496a795186d
Federico Lorenzi <florenzi@gmail.com>
2025-09-09 10:10:36 +0200

    cli: require --log-debug with --log-trace

--- src/tigerbeetle/cli.zig
+++ src/tigerbeetle/cli.zig
@@ -964,6 +964,10 @@ fn parse_args_start(start: CLIArgs.Start) Command.Start {
         break :blk aof_file;
     } else null;
 
+    if (start.log_trace and !start.log_debug) {
+        vsr.fatal(.cli, "--log-debug must be provided when using --log-trace", .{});
+    }
+
     return .{
         .addresses = addresses,
         .addresses_zero = std.mem.eql(u8, start.addresses, "0"),
