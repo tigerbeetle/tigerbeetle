67e0990560f5e5b35df864978bf712002eb1fd23
batiati <rbatiati@gmail.com>
2025-09-15 10:22:07 -0300

    TODO: Dotnet client

--- src/clients/dotnet/TigerBeetle/Client.cs
+++ src/clients/dotnet/TigerBeetle/Client.cs
@@ -29,21 +29,9 @@ public sealed class Client : IDisposable
 
     public UInt128 ClusterID => clusterID;
 
-    public CreateAccountResult CreateAccount(Account account)
-    {
-        var ret = nativeClient.CallRequest<CreateAccountsResult, Account>(TBOperation.CreateAccounts, new[] { account });
-        return ret.Length == 0 ? CreateAccountResult.Ok : ret[0].Result;
-    }
-
     public CreateAccountsResult[] CreateAccounts(ReadOnlySpan<Account> batch)
     {
         return nativeClient.CallRequest<CreateAccountsResult, Account>(TBOperation.CreateAccounts, batch);
-    }
-
-    public Task<CreateAccountResult> CreateAccountAsync(Account account)
-    {
-        return nativeClient.CallRequestAsync<CreateAccountsResult, Account>(TBOperation.CreateAccounts, new[] { account })
-        .ContinueWith(x => x.Result.Length == 0 ? CreateAccountResult.Ok : x.Result[0].Result);
     }
 
     public Task<CreateAccountsResult[]> CreateAccountsAsync(ReadOnlyMemory<Account> batch)
@@ -51,21 +39,9 @@ public sealed class Client : IDisposable
         return nativeClient.CallRequestAsync<CreateAccountsResult, Account>(TBOperation.CreateAccounts, batch);
     }
 
-    public CreateTransferResult CreateTransfer(Transfer transfer)
-    {
-        var ret = nativeClient.CallRequest<CreateTransfersResult, Transfer>(TBOperation.CreateTransfers, new[] { transfer });
-        return ret.Length == 0 ? CreateTransferResult.Ok : ret[0].Result;
-    }
-
     public CreateTransfersResult[] CreateTransfers(ReadOnlySpan<Transfer> batch)
     {
         return nativeClient.CallRequest<CreateTransfersResult, Transfer>(TBOperation.CreateTransfers, batch);
-    }
-
-    public Task<CreateTransferResult> CreateTransferAsync(Transfer transfer)
-    {
-        return nativeClient.CallRequestAsync<CreateTransfersResult, Transfer>(TBOperation.CreateTransfers, new[] { transfer })
-        .ContinueWith(x => x.Result.Length == 0 ? CreateTransferResult.Ok : x.Result[0].Result);
     }
 
     public Task<CreateTransfersResult[]> CreateTransfersAsync(ReadOnlyMemory<Transfer> batch)
@@ -73,21 +49,9 @@ public sealed class Client : IDisposable
         return nativeClient.CallRequestAsync<CreateTransfersResult, Transfer>(TBOperation.CreateTransfers, batch);
     }
 
-    public Account? LookupAccount(UInt128 id)
-    {
-        var ret = nativeClient.CallRequest<Account, UInt128>(TBOperation.LookupAccounts, new[] { id });
-        return ret.Length == 0 ? null : ret[0];
-    }
-
     public Account[] LookupAccounts(ReadOnlySpan<UInt128> batch)
     {
         return nativeClient.CallRequest<Account, UInt128>(TBOperation.LookupAccounts, batch);
-    }
-
-    public Task<Account?> LookupAccountAsync(UInt128 id)
-    {
-        return nativeClient.CallRequestAsync<Account, UInt128>(TBOperation.LookupAccounts, new[] { id })
-        .ContinueWith(x => x.Result.Length == 0 ? (Account?)null : x.Result[0]);
     }
 
     public Task<Account[]> LookupAccountsAsync(ReadOnlyMemory<UInt128> batch)
@@ -95,21 +59,9 @@ public sealed class Client : IDisposable
         return nativeClient.CallRequestAsync<Account, UInt128>(TBOperation.LookupAccounts, batch);
     }
 
-    public Transfer? LookupTransfer(UInt128 id)
-    {
-        var ret = nativeClient.CallRequest<Transfer, UInt128>(TBOperation.LookupTransfers, new[] { id });
-        return ret.Length == 0 ? null : ret[0];
-    }
-
     public Transfer[] LookupTransfers(ReadOnlySpan<UInt128> batch)
     {
         return nativeClient.CallRequest<Transfer, UInt128>(TBOperation.LookupTransfers, batch);
-    }
-
-    public Task<Transfer?> LookupTransferAsync(UInt128 id)
-    {
-        return nativeClient.CallRequestAsync<Transfer, UInt128>(TBOperation.LookupTransfers, new[] { id })
-        .ContinueWith(x => x.Result.Length == 0 ? (Transfer?)null : x.Result[0]);
     }
 
     public Task<Transfer[]> LookupTransfersAsync(ReadOnlyMemory<UInt128> batch)
