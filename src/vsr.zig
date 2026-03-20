8437e0a97c009959a33ae8c1f802a3a4297f56c3
Georg Kreuzmayr <georg@tigerbeetle.com>
2026-03-20 16:29:51 +0100

    fix vsr export

--- src/vsr.zig
+++ src/vsr.zig
@@ -76,7 +76,6 @@ pub const GridScrubberType = @import("vsr/grid_scrubber.zig").GridScrubberType;
 pub const Routing = @import("vsr/routing.zig");
 pub const FaultDetector = @import("vsr/fault_detector.zig");
 pub const CountingAllocator = @import("counting_allocator.zig");
-pub const IOPSType = @import("iops.zig").IOPSType;
 pub const StackType = @import("stack.zig").StackType;
 
 /// The version of our Viewstamped Replication protocol in use, including customizations.
