9b06b4ca3fe6e15e4faca7dde3c6850c20938524
Alex Kladov <aleksey.kladov@gmail.com>
2026-03-27 16:09:59 +0000

    ci: cache Zig

--- zig/download.win.ps1
+++ zig/download.win.ps1
@@ -20,27 +20,36 @@ $ZIG_EXTENSION = ".zip"
 
 # Build URL:
 $ZIG_URL = "$ZIG_MIRROR/$ZIG_RELEASE/zig-$ZIG_ARCH-$ZIG_OS-$ZIG_RELEASE$ZIG_EXTENSION"
-$ZIG_ARCHIVE = [System.IO.Path]::GetFileName("$ZIG_URL")
-$ZIG_DIRECTORY = "$ZIG_ARCHIVE" -replace [regex]::Escape($ZIG_EXTENSION), ""
+$ZIG_ARCHIVE = "./zig/cache/" + [System.IO.Path]::GetFileName("$ZIG_URL")
+$ZIG_DIRECTORY = "./" + ([System.IO.Path]::GetFileName("$ZIG_ARCHIVE") -replace [regex]::Escape($ZIG_EXTENSION), "")
 
 # Find expected checksum from list:
 $ZIG_CHECKSUM_EXPECTED = ($ZIG_CHECKSUMS -split "`n" | Where-Object { $_ -like "*$ZIG_URL*" }) -split ' ' | Select-Object -Last 1
 
-Write-Output "Downloading Zig $ZIG_RELEASE for Windows..."
-Invoke-WebRequest -Uri "$ZIG_URL" -OutFile "$ZIG_ARCHIVE"
-
-# Verify the checksum.
-$ZIG_CHECKSUM_ACTUAL=(Get-FileHash "${ZIG_ARCHIVE}").Hash
-
-if ($ZIG_CHECKSUM_ACTUAL -ne $ZIG_CHECKSUM_EXPECTED) {
-    Write-Error "Checksum mismatch. Expected '$ZIG_CHECKSUM_EXPECTED' but got '$ZIG_CHECKSUM_ACTUAL'."
-    exit 1
+# Returns $true if the given file exists and its SHA-256 checksum matches the expected value.
+function checksum_valid($file, $expected) {
+    if (-not (Test-Path "$file")) { return $false }
+    $actual = (Get-FileHash "$file").Hash
+    return $actual -eq $expected
 }
 
-# Extract and then remove the downloaded archive:
-Write-Output "Extracting $ZIG_ARCHIVE..."
+if (checksum_valid "$ZIG_ARCHIVE" "$ZIG_CHECKSUM_EXPECTED") { # Caching for CI.
+    Write-Output "Skip downloading Zig $ZIG_RELEASE."
+} else {
+    Write-Output "Downloading Zig $ZIG_RELEASE ..."
+    New-Item -ItemType Directory -Path ./zig/cache -Force | Out-Null
+    Invoke-WebRequest -Uri "$ZIG_URL" -OutFile "$ZIG_ARCHIVE"
+
+    # Verify the checksum.
+    if (-not (checksum_valid "$ZIG_ARCHIVE" "$ZIG_CHECKSUM_EXPECTED")) {
+        Write-Error "Checksum mismatch."
+        exit 1
+    }
+}
+
+Write-Output "Extracting $ZIG_ARCHIVE ..."
 Expand-Archive -Path "$ZIG_ARCHIVE" -DestinationPath .
-Remove-Item "$ZIG_ARCHIVE"
+# NB: Keep archive for caching.
 
 # Replace these existing directories and files so that we can install or upgrade:
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue zig/doc, zig/lib
