c16c51e66dfb1d227a21ae7605c3eca7bc84c823
Michael Kohl <me@citizen428.net>
2026-05-22 21:23:25 +0700

    clients/ruby: Improve platform detection
    Also fix Windows DLL loading

--- src/clients/ruby/src/ext/tigerbeetle/extconf.rb
+++ src/clients/ruby/src/ext/tigerbeetle/extconf.rb
@@ -2,10 +2,44 @@ require "mkmf"
 require "rbconfig"
 
 arch = RbConfig::CONFIG["target_cpu"]
-os = RbConfig::CONFIG["target_os"]
+target_os = RbConfig::CONFIG["target_os"]
 
-platform_dir = Dir.glob(File.join(__dir__, "lib", "#{arch}-#{os.split("-").first}*")).first
-raise "No prebuilt libtb_client found for #{arch}-#{os}" unless platform_dir
+platform_arch =
+  case arch
+  when "x64", "x86_64"
+    "x86_64"
+  when "arm64", "aarch64"
+    "aarch64"
+  else
+    raise "Unsupported architecture #{arch.inspect}"
+  end
+
+platform_os =
+  case target_os
+  when /linux/
+    host_os = RbConfig::CONFIG["host_os"]
+
+    if host_os.include?("musl") || target_os.include?("musl")
+      "linux-musl"
+    else
+      "linux-gnu*"
+    end
+  when /darwin/
+    "macos"
+  when /mingw|mswin/
+    raise "Unsupported Windows architecture #{arch.inspect}" unless platform_arch == "x86_64"
+
+    "windows"
+  else
+    raise "Unsupported operating system #{target_os.inspect}"
+  end
+
+platform = "#{platform_arch}-#{platform_os}"
+
+platform_dir = Dir.glob(File.join(__dir__, "lib", platform)).first
+raise "No prebuilt libtb_client found for #{arch}-#{target_os}" unless platform_dir
+
+$DLDFLAGS << " -Wl,-rpath,#{platform_dir}" if target_os.match?(/darwin/)
 
 dir_config("tb_client", __dir__, platform_dir)
 have_library("tb_client") or raise("libtb_client not found")
--- src/clients/ruby/src/tigerbeetle.rb
+++ src/clients/ruby/src/tigerbeetle.rb
@@ -3,7 +3,28 @@ require_relative "tigerbeetle/bindings"
 require_relative "tigerbeetle/id"
 require_relative "tigerbeetle/completion_dispatcher"
 require_relative "tigerbeetle/client"
-require "tigerbeetle/tigerbeetle"
+
+native_extension = "tigerbeetle/tigerbeetle"
+
+# Since we ship a fat binary gem, enable the bundled DLL path before loading the extension.
+if Gem.win_platform?
+  dll_path = File.expand_path("ext/tigerbeetle/lib/x86_64-windows", __dir__)
+
+  begin
+    require "ruby_installer/runtime"
+    RubyInstaller::Runtime.add_dll_directory(dll_path) do
+      require native_extension
+    end
+  rescue LoadError
+    old_path = ENV["PATH"]
+    ENV["PATH"] = "#{dll_path};#{old_path}"
+    require native_extension
+  ensure
+    ENV["PATH"] = old_path if defined?(old_path)
+  end
+else
+  require native_extension
+end
 
 module TigerBeetle
   private_constant :NativeClient
