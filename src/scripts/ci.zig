9f57bbeefc590e570f8ec2e40128790990b155c8
sentientwaffle <sentientwaffle@gmail.com>
2026-05-28 14:15:35 -0600

    Release: Validate release from main
    Execute release-validate from `main` so that fixes are deployed promptly.

--- src/scripts/ci.zig
+++ src/scripts/ci.zig
@@ -103,6 +103,13 @@ fn validate_release(shell: *Shell, gpa: std.mem.Allocator, language_requested: ?
     const tag, _ = stdx.cut(release_info, "\t").?;
     log.info("validating release {s}", .{tag});
 
+    // Run release validation against the latest tag.
+    // (The release branch is ahead of release tag over the weekend.)
+    try shell.exec(
+        "git clone --branch={tag} https://github.com/tigerbeetle/tigerbeetle .",
+        .{ .tag = tag },
+    );
+    try shell.exec("./zig/download.sh", .{});
     try shell.exec(
         "gh release --repo tigerbeetle/tigerbeetle download {tag}",
         .{ .tag = tag },
@@ -131,24 +138,21 @@ fn validate_release(shell: *Shell, gpa: std.mem.Allocator, language_requested: ?
     }
 
     const git_sha = try shell.exec_stdout("git rev-parse HEAD", .{});
-    try shell.exec_zig_options(.{ .timeout = .minutes(20) }, "build scripts -- release --build " ++
-        "--sha={git_sha} --language=zig", .{
-        .git_sha = git_sha,
-    });
+    try shell.exec_options(
+        .{ .timeout = .minutes(20) },
+        "./zig/zig build scripts -- release --build --sha={git_sha} --language=zig",
+        .{ .git_sha = git_sha },
+    );
     for (artifacts) |artifact| {
         // Zig only guarantees release builds to be deterministic.
         if (std.mem.indexOf(u8, artifact, "-debug.zip") != null) continue;
 
         const checksum_downloaded = try shell.sha256sum(artifact);
 
-        shell.popd();
         const checksum_built = try shell.sha256sum(try shell.fmt(
             "zig-out/dist/tigerbeetle/{s}",
-            .{
-                artifact,
-            },
+            .{artifact},
         ));
-        try shell.pushd_dir(tmp_dir.dir);
 
         if (checksum_downloaded != checksum_built) {
             std.debug.panic("checksum mismatch - {s}: downloaded {x}, built {x}", .{
@@ -175,6 +179,7 @@ fn validate_release(shell: *Shell, gpa: std.mem.Allocator, language_requested: ?
         },
         else => std.debug.panic("unsupported os {}", .{builtin.os.tag}),
     };
+    try shell.cwd.deleteFile("tigerbeetle");
     try shell.unzip_executable(artifact_host, "tigerbeetle");
 
     const version = try shell.exec_stdout("./tigerbeetle version --verbose", .{});
