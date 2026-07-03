6420fb3a91788da62d51d718b061e4efa79d4c94
sentientwaffle <sentientwaffle@gmail.com>
2026-07-03 09:50:14 -0600

    VSR: Fix scrub-write-available calculation
    The old scrub-write-available calculation in `on_grid_scrub_timeout()` could be incorrect due to the timing mismatch between:
    - `GridBlocksMissing.sync_jump_commence()`, and
    - the `grid_repair_write` actually being released
    
    Instead count it explicitly.

--- src/vsr/replica.zig
+++ src/vsr/replica.zig
@@ -3513,6 +3513,7 @@ pub fn ReplicaType(
                 self.grid.blocks_missing.repair_block(
                     message.header.address,
                     message.header.checksum,
+                    .{ .scrub = false },
                 );
             }
 
@@ -3864,9 +3865,8 @@ pub fn ReplicaType(
             );
 
             const grid_scrubber_writes_count =
-                self.grid.blocks_missing.repair_blocks_available() -
-                self.grid_repair_writes.available();
-            assert(grid_scrubber_writes_count <= constants.grid_scrubber_writes_max);
+                constants.grid_scrubber_writes_max -
+                self.grid.blocks_missing.enqueued_blocks_repair_scrub;
             for (0..grid_scrubber_writes_count) |_| {
                 const fault = blk: {
                     while (self.grid_scrubber.read_result_next()) |result| {
@@ -3888,6 +3888,7 @@ pub fn ReplicaType(
                 self.grid.blocks_missing.repair_block(
                     fault.block_address,
                     fault.block_checksum,
+                    .{ .scrub = true },
                 );
             }
 
