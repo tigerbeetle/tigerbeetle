5a54a1140fee3392d7f62ea706ab4f0371ef7012
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2026-08-24 10:55:18 +0200

    perf: don't debug print in expected test case

--- src/stdx/benchmark/perf.zig
+++ src/stdx/benchmark/perf.zig
@@ -34,13 +34,16 @@ test "perf: usage example" {
 
     var perf = PerfCounters.init() catch |err| switch (err) {
         error.PermissionDenied => {
-            std.debug.print(
-                \\ Insufficient permmissions for opening linux performance counters. Try running
-                \\    sudo sysctl -w kernel.perf_event_paranoid=-1
-                \\ This may not be possible when running in a virtualized environment. 
-                \\
-            , .{});
-            return;
+            // we expect this to fail on CI machines and don't want to pollute the test output
+            if (!builtin.is_test) {
+                std.debug.print(
+                    \\Insufficient permmissions for opening linux performance counters. Try running
+                    \\   sudo sysctl -w kernel.perf_event_paranoid=-1
+                    \\This may not be possible when running in a virtualized environment. 
+                    \\
+                , .{});
+            }
+            return error.SkipZigTest;
         },
         else => return err,
     };
